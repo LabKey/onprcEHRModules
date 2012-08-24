03aac1a2c6fe173d35a1cf6bfdc5644844a7170d
Adam Rauch <adam@labkey.com>
2012-08-24 00:03:03 +0000

    Update licenses
    SVN r21724 |2012-08-24 00:03:03 +0000

--- onprc_ehr/tools/qcStates.sql
+++ onprc_ehr/tools/qcStates.sql
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 INSERT INTO study.qcstate (label,description,publicdata,container) VALUES ('Abnormal', 'Value is abnormal', TRUE, (SELECT entityid FROM core.containers WHERE name='EHR' AND parent = (SELECT entityid FROM core.containers WHERE name='ONPRC')));
 INSERT INTO study.qcstate (label,description,publicdata,container) VALUES ('Completed', 'Record has been completed and is public', FALSE, (SELECT entityid FROM core.containers WHERE name='EHR' AND parent = (SELECT entityid FROM core.containers WHERE name='ONPRC')));
 INSERT INTO study.qcstate (label,description,publicdata,container) VALUES ('Delete Requested', 'Records are requested to be deleted', FALSE, (SELECT entityid FROM core.containers WHERE name='EHR' AND parent = (SELECT entityid FROM core.containers WHERE name='ONPRC')));
