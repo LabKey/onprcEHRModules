10a50d9ca34a733ff2a7ce8ce5873a7b05e4320b
Ben Bimber <bbimber@labkey.com>
2012-11-21 05:15:35 +0000

    elispot / electrochemiluminescence assay tests
    SVN r23203 |2012-11-21 05:15:35 +0000

--- onprc_ehr/resources/etl/study/Treatment Orders.sql
+++ onprc_ehr/resources/etl/study/Treatment Orders.sql
@@ -62,4 +62,4 @@ FROM Cln_Medications cln
      left join Sys_parameters s6 on (s6.Field = 'DepartmentCode' and s6.Flag = rt.DeptCode)
      left join ref_snomed121311 sno on (sno.SnomedCode = cln.Medication)
 
-where cln.ts > ?
+where cln.ts > ? and code is not null
