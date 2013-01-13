30e003ba3e00f52a5a6510325301ea2bdacecb36
Adam Rauch <adam@labkey.com>
2013-01-13 03:54:45 +0000

    Update licenses
    SVN r24012 |2013-01-13 03:54:45 +0000

--- onprc_ehr/resources/web/onprc_ehr/panel/IconPanel.js
+++ onprc_ehr/resources/web/onprc_ehr/panel/IconPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 /**
  * Used to display the results of ONPRC.Utils.getNavItems using an IconPanel.
  * @cfg category The category of icons to display
