a57507fd687b45e91a0a35a8ab8886dab90a441f
Adam Rauch <adam@labkey.com>
2013-12-20 17:58:05 +0000

    Update licenses
    SVN r30037 |2013-12-20 17:58:05 +0000

--- onprc_ehr/resources/web/onprc_ehr/panel/ChargesInstructionPanel.js
+++ onprc_ehr/resources/web/onprc_ehr/panel/ChargesInstructionPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext4.define('ONPRC_EHR.panel.ChargesInstructionPanel', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.onprcehr-chargesinstructionpanel',
