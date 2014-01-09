62bc24c767fb4c406e70aedcea511d47b83bb8e8
Adam Rauch <adam@labkey.com>
2014-01-09 16:10:33 +0000

    Update licenses
    SVN r30211 |2014-01-09 16:10:33 +0000

--- onprc_ehr/resources/web/onprc_ehr/buttons/financeButtons.js
+++ onprc_ehr/resources/web/onprc_ehr/buttons/financeButtons.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013-2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 /**
  * A button specific to the charges form
  */
