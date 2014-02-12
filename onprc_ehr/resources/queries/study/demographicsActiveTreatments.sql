c72406dfe9d5bc4c3b0b8bc48b8c4e9a03348c3b
Ben Bimber <bbimber@labkey.com>
2014-02-12 05:04:09 +0000

    merge onprc13.3 r30893-30904
    SVN r30905 |2014-02-12 05:04:09 +0000

--- onprc_ehr/resources/queries/study/demographicsActiveTreatments.sql
+++ onprc_ehr/resources/queries/study/demographicsActiveTreatments.sql
@@ -4,6 +4,11 @@ SELECT
   COUNT(CASE WHEN t.category = 'Clinical' THEN 1 ELSE null END) as totalClinicalTreatments,
   COUNT(CASE WHEN t.category = 'Surgical' THEN 1 ELSE null END) as totalSurgicalTreatments,
   COUNT(CASE WHEN t.category = 'Research' THEN 1 ELSE null END) as totalResearchTreatments,
+
+  GROUP_CONCAT(CASE WHEN t.category = 'Clinical' THEN t.code.meaning ELSE null END, chr(10)) as clinicalTreatments,
+  GROUP_CONCAT(CASE WHEN t.category = 'Surgical' THEN t.code.meaning ELSE null END, chr(10)) as surgicalTreatments,
+  GROUP_CONCAT(CASE WHEN t.category = 'Research' THEN t.code.meaning ELSE null END, chr(10)) as researchTreatments,
+
   max(t.modified) as lastModification,
 
 FROM study.treatment_order t
