6f6ae73c98fe1c0cc193d1dd000a28988e29a9eb
Adam Rauch <adam@labkey.com>
2014-04-04 04:15:25 +0000

    Update licenses
    SVN r31842 |2014-04-04 04:15:25 +0000

--- onprc_ehr/resources/web/onprc_ehr/panel/LockAnimalsPanel.js
+++ onprc_ehr/resources/web/onprc_ehr/panel/LockAnimalsPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013-2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext4.define('ONPRC_EHR.panel.LockAnimalsPanel', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.onprc-lockanimalspanel',
