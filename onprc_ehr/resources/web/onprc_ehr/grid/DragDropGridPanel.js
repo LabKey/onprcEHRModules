516e6ce5a9136e02b6c14874449d9e03833f4f7c
Adam Rauch <adam@labkey.com>
2014-11-03 21:35:28 +0000

    Update licenses
    SVN r35181 |2014-11-03 21:35:28 +0000

--- onprc_ehr/resources/web/onprc_ehr/grid/DragDropGridPanel.js
+++ onprc_ehr/resources/web/onprc_ehr/grid/DragDropGridPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext4.define('ONPRC_EHR.grid.DragDropGridPanel', {
     extend: 'EHR.grid.Panel',
     alias: 'widget.onprc_ehr-dragdropgridpanel',
