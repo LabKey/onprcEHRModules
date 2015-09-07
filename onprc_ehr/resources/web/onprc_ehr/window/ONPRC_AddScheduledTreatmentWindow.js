4223084fd0ff557773cc4ca98bdc65a593c97433
Adam Rauch <adam@labkey.com>
2015-09-07 18:37:59 +0000

    Update licenses
    SVN r39524 |2015-09-07 18:37:59 +0000

--- onprc_ehr/resources/web/onprc_ehr/window/ONPRC_AddScheduledTreatmentWindow.js
+++ onprc_ehr/resources/web/onprc_ehr/window/ONPRC_AddScheduledTreatmentWindow.js
@@ -2,7 +2,7 @@
  * Created by jonesga on 4/30/2015.
  */
 /*
- * Copyright (c) 2013-2014 LabKey Corporation
+ * Copyright (c) 2015 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
