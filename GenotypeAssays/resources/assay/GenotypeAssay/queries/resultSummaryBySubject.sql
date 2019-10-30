f87b7b9864f4e1f31d354e809521226abdb393b1
labkey-tchad <tchad@labkey.com>
2019-10-30 12:02:41 -0700

    Update module locations for `laboratory` and `GenotypeAssays`

--- GenotypeAssays/resources/assay/GenotypeAssay/queries/resultSummaryBySubject.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  d.subjectId,
-  d.run.assayType,
-  count(*) as totalResults,
-  count(distinct d.run) as totalRuns,
-
-FROM Data d
-
-GROUP BY d.subjectId, d.run.assayType
--- GenotypeAssays/resources/assay/GenotypeAssay/queries/resultSummaryBySubjectAndMethod.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  d.subjectId,
-  d.run.method as method,
-  count(*) as totalResults,
-  count(distinct d.run) as totalRuns,
-
-FROM Data d
-
-GROUP BY d.subjectId, d.run.method
