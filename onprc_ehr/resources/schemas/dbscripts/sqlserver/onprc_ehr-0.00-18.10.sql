6b1b755bead13b4aba4163dc88ffa02b31d1211b
Binal <binalp@labkey.com>
2020-10-14 22:50:47 -0700

    Handle observation_types deletion for dbs on different versions.

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-0.00-18.10.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-0.00-18.10.sql
@@ -1033,6 +1033,7 @@ Err_Proc:    Return 1
 
 
 END
+GO
 
 CREATE TABLE [onprc_ehr].[AvailableBloodVolume](
 	[datecreated] [datetime] NULL,
@@ -1080,3 +1081,49 @@ CREATE TABLE onprc_ehr.Frequency_DayofWeek(
     CONSTRAINT pk_FreqWeek PRIMARY KEY (RowId)
 
 );
+
+--Add container column
+ALTER TABLE onprc_ehr.observation_types ADD container entityid;
+GO
+
+--Add container ids to onprc_ehr.observation_types:
+UPDATE onprc_ehr.observation_types
+SET container = (SELECT c.entityid FROM core.containers c
+                                            LEFT JOIN core.Containers c2 ON c.Parent = c2.EntityId
+                 WHERE c.name = 'EHR' and c2.name = 'ONPRC')
+WHERE container IS NULL;
+GO
+
+--copy data into ehr table
+INSERT INTO ehr.observation_types
+(value,
+ category,
+ editorconfig,
+ schemaName,
+ queryName,
+ valueColumn,
+ createdby,
+ created,
+ modifiedby,
+ modified,
+ container
+)
+SELECT
+    value,
+    category,
+    editorconfig,
+    schemaName,
+    queryName,
+    valueColumn,
+    createdby,
+    created,
+    modifiedby,
+    modified,
+    container
+FROM onprc_ehr.observation_types obs
+WHERE obs.container IS NOT NULL;
+GO
+
+--drop table
+DROP TABLE onprc_ehr.observation_types
+GO
