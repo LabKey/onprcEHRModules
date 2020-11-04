6866e1fe7badc61f37258cda328d1c3430a1c852
Binal <binalp@labkey.com>
2020-11-04 13:13:42 -0800

    Use core.fn_dropifexists to drop COLUMN to keep it backward compatible with MS SQL Server 2012

--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-20.104-20.105.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-20.104-20.105.sql
@@ -1,39 +1,68 @@
 -- Adding additional Fields for Alias insert from OGA Synch
 --Rerunning and it does not appear in Build
 --2020-03-4 Revision to add this to UAT
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [ApplicationType];
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'ApplicationType';
+GO
 ALTER TABLE onprc_billing.aliases ADD [ApplicationType] VarChar(255) Null;
 
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [ApplicationTypeDescription];
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'ApplicationTypeDescription';
+GO
 ALTER TABLE onprc_billing.aliases ADD [ApplicationTypeDescription]  VarChar(255) Null;
 
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [AwardStatus];
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'AwardStatus';
+GO
 ALTER TABLE onprc_billing.aliases ADD [AwardStatus] VARCHAR(100) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [AwardID];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'AwardID';
+GO
 ALTER TABLE onprc_billing.aliases ADD [AwardID] VARCHAR(100) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [ApplicationType];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'ApplicationType';
+GO
 ALTER TABLE onprc_billing.aliases ADD [ApplicationType] VARCHAR(255) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [ProjectID];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'ProjectID';
+GO
 ALTER TABLE onprc_billing.aliases ADD [ProjectID] VARCHAR(100) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [ActivityType];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'ActivityType';
+GO
 ALTER TABLE onprc_billing.aliases ADD [ActivityType] VARCHAR(255) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [AwardNumber];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'AwardNumber';
+GO
 ALTER TABLE onprc_billing.aliases ADD [AwardNumber] VARCHAR(255) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [AwardSuffix];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'AwardSuffix';
+GO
 ALTER TABLE onprc_billing.aliases ADD [AwardSuffix] VARCHAR(255) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [Org];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'Org';
+GO
 ALTER TABLE onprc_billing.aliases ADD [Org] VARCHAR(255) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [ADFMEmpNum];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'ADFMEmpNum';
+GO
 ALTER TABLE onprc_billing.aliases ADD [ADFMEmpNum] VARCHAR(255) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [ADFMFullName];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'ADFMFullName';
+GO
 ALTER TABLE onprc_billing.aliases ADD [ADFMFullName] VARCHAR(255) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [ActivityTypeDescription];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'ActivityTypeDescription';
+GO
 ALTER TABLE onprc_billing.aliases ADD [ActivityTypeDescription] VARCHAR(255) NUll;
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [FundingSourceNumber];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'FundingSourceNumber';
+GO
 ALTER TABLE onprc_billing.aliases ADD [FUndingSourceNumber] VARCHAR(255) NUll
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [FundingSourceName];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'FundingSourceName';
+GO
 ALTER TABLE onprc_billing.aliases ADD [FUndingSourceName] VARCHAR(255) NUll
-ALTER TABLE onprc_billing.aliases DROP COLUMN IF EXISTS [Org];
+
+EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'Org';
+GO
 ALTER TABLE onprc_billing.aliases ADD [Org] VARCHAR(255) NUll
 
 --Adding additional fields to OGA Synch
