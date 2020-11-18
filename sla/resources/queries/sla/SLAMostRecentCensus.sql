ac756ba4126568dc2489efd08132a92164c7e4a9
Binal <binalp@labkey.com>
2020-11-18 14:26:00 -0800

    Fix query

--- sla/resources/queries/sla/SLAMostRecentCensus.sql
+++ sla/resources/queries/sla/SLAMostRecentCensus.sql
@@ -7,13 +7,17 @@
   /*   Created  Blasa    2-6-2015  Provide most SLA Census   Modified 8-28-2015 */
 
 select
-  a.project [project],  a.Investigatorid [Investigator],  rtrim(a.room) [room],
-  a.species [Species], a.cagetype [Cage_Type], a.cageSize [Cage_Size], a.date [Date] ,
-  Case a.cagecount
-      when  0 then 0
-      else a.animalcount
-      end [Animal_Count],
-  a.cagecount [Cage_Count] , now() [recentdate] from  sla.census a
+    a.project AS project,
+    a.Investigatorid AS Investigator,
+    rtrim(a.room) AS room,
+    a.species AS Species,
+    a.cagetype AS Cage_Type,
+    a.cageSize AS Cage_Size,
+    a.date AS Date,
+    CASE WHEN a.cagecount = 0 THEN 0 ELSE a.animalcount END AS Animal_Count,
+    a.cagecount AS Cage_Count,
+    now() AS recentdate
+from  sla.census a
 
 Where a.date in (select max(b.date) from  sla.census b where  a.project = b.project
 And a.species = b.species and a.cageSize = b.cageSize and a.Cagetype = b.cageType and a.room = b.room and b.cagecount >0 AND timestampdiff('SQL_TSI_DAY', b.date, now())  <  6)
@@ -23,7 +27,3 @@ And  timestampdiff('SQL_TSI_DAY', a.date, now())  <  6
 
 group by a.project, a.Investigatorid, a.species, a.cagetype, a.cageSize, a.room,a.date,a.animalcount, a.cagecount
 order by   a.room  , a.date desc
-
-
-
-
