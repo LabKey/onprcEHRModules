19f629d3b8afe659765e28b8d5a0fb8727bfa579
jonesgaohsu <Cora1970627>
2021-01-27 12:59:31 -0800

    Update of source files for vet_assignment Process

---
+++ onprc_ehr/resources/queries/study/vet_AssignedResearch_project.sql
@@ -0,0 +1,19 @@
+Select a.Id,
+       a.project,
+       a.project.project as ProjectID,
+       v.project.project as VetAssignedProject,
+       a.project.protocol.investigatorID.lastName as PI,
+       a.project.use_category,
+       v.userid.userid,
+       a.date,
+       a.projectedRelease,
+       a.enddate,
+       a.assignCondition,
+       CASE
+           when v.project is not null then 'Project Research Assigned'
+           End as ProjectType
+
+
+from onprc_ehr.vet_assignment v left outer join  study.assignment a on a.project.project = v.project.project
+
+where (v.project is not null and (a.date <= Now() and a.enddate is null) and a.project.use_category = 'Research')
