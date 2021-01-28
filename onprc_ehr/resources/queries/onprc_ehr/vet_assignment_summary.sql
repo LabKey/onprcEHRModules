fe2dd2351f9ada75e2a2b5079ca29ed8e68f161f
Gary Jones <jonesga@ohsu.edu>
2021-01-28 09:11:07 -0800

    Update of Files and queries for Vet Assignment (#70)
    Co-authored-by: jonesgaohsu <Cora1970627>

--- onprc_ehr/resources/queries/onprc_ehr/vet_assignment_summary.sql
+++ onprc_ehr/resources/queries/onprc_ehr/vet_assignment_summary.sql
@@ -14,15 +14,18 @@
  * limitations under the License.
  */
 SELECT
-  v.userId,
+    v.userId,
+    group_concat(DISTINCT ( CASE WHEN (v.project is not null and v.room is not null)  THEN ((v.project.name || ' (' || v.project.investigatorid.lastname || ')' || '-' || v.room)) ELSE '' END), chr(10)) as Project_Room,
+    group_concat(DISTINCT ( CASE WHEN (v.project is not null and v.area is not null)  THEN (v.project.name || '-' || v.area) ELSE '' END), chr(10)) as Project_Area,
+    group_concat(DISTINCT ( CASE WHEN (v.project is not null and v.room is  null and v.area is null ) then (v.project.name || ' (' || v.project.investigatorid.lastname || ')') END ), chr(10)) as projects,
+    group_concat(DISTINCT ( CASE WHEN (v.protocol is not null and v.room is not null)  THEN (v.protocol.displayname || '-' || v.room) ELSE '' END), chr(10)) as Protocol_Room,
+    group_concat(DISTINCT ( CASE WHEN (v.protocol is not null and v.area is not null)  THEN (v.protocol.displayname || '-' || v.area) ELSE '' END), chr(10)) as Protocol_Area,
+    group_concat(DISTINCT (v.protocol.displayName || ' (' || v.project.investigatorid.lastname || ')'), chr(10)) as protocols,
+    group_concat(DISTINCT ( CASE WHEN (v.room is not null and v.area is null and v.project is null and v.protocol is null)  THEN (v.room || '-' || v.area) ELSE '' END), chr(10)) as rooms,
+    -- group_concat(DISTINCT (v.room || CASE WHEN v.priority = true THEN '*' ELSE '' END), chr(10)) as rooms,
+    group_concat(DISTINCT ( CASE WHEN (v.area is not null and v.room is null and v.project is null and v.protocol is null)  THEN (v.area) ELSE '' END), chr(10)) as areas
 
-  group_concat(DISTINCT ( CASE WHEN (v.protocol is not null and v.room is not null)  THEN (v.protocol.displayname || '-' || v.room) ELSE '' END), chr(10)) as Protocol_Room,
-  group_concat(DISTINCT ( CASE WHEN (v.protocol is not null and v.area is not null)  THEN (v.protocol.displayname || '-' || v.area) ELSE '' END), chr(10)) as Protocol_Area,
-  group_concat(DISTINCT (v.protocol.displayName || ' (' || v.protocol.investigatorid.lastname || ')'), chr(10)) as protocols,
-  group_concat(DISTINCT (v.room || CASE WHEN v.priority = true THEN '*' ELSE '' END), chr(10)) as rooms,
-  group_concat(DISTINCT (v.area || CASE WHEN v.priority = true THEN '*' ELSE '' END), chr(10)) as areas
-
-
+--  group_concat(DISTINCT (v.area || CASE WHEN v.priority = true THEN '*' ELSE '' END), chr(10)) as areas
 
 FROM onprc_ehr.vet_assignment v
 GROUP BY v.userId
