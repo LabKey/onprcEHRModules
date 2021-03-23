bcbc4f2faf49104eab04c64f8f5dc6fc75f43737
Lakshmi Kolli <kollil@ohsu.edu>
2021-03-23 14:35:11 -0700

    Removed the containerid from the queries

--- extscheduler/resources/queries/extscheduler/PMIC_Scheduler_Daily.sql
+++ extscheduler/resources/queries/extscheduler/PMIC_Scheduler_Daily.sql
@@ -1,6 +1,6 @@
 /*
 Created by: Kolli on 10/24/2019
-This query filters all the PMIC events
+This query filters all the PMIC daily events
 */
 SELECT
     r.name as resourceid,
@@ -18,6 +18,6 @@ SELECT
     e.modifiedby,
     e.modified
 FROM Events e, Resources r
---Where r.id = e.resourceid and e.container = 'E1CFA5B8-FAD2-1034-A87D-5107380A72B9' And CAST(e.startDate AS DATE) = curdate() --for testing
 Where r.id = e.resourceid
-  and e.container = '783D2EA5-C6AC-1036-A33C-BD25D0574070' And CAST(e.startDate AS DATE) = curdate() --Show only PMIC events
+  --And e.container = '783D2EA5-C6AC-1036-A33C-BD25D0574070'
+  And CAST(e.startDate AS DATE) = curdate() --Show only PMIC events
