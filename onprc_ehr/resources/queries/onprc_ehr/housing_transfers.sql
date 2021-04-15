9e21a5e32ad08027fd4a5918bd77b51ac40ae04a
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2021-04-15 16:34:42 -0700

    Color coding the cells in query.xml (#140)
    * Color coding the cells in query.xml
    
    * Changed the date filter in the query.
    
    * Renamed a PMIC file.
    
    * Updated the housing color coding for diets

--- onprc_ehr/resources/queries/onprc_ehr/housing_transfers.sql
+++ onprc_ehr/resources/queries/onprc_ehr/housing_transfers.sql
@@ -1,9 +1,13 @@
 Select
     h.Id,
     CASE
-        WHEN (Select totalClinicalTreatments from study.demographicsActiveTreatments t where h.id = t.id ) > 0 THEN 'Yes'
+        WHEN (Select totalClinicalTreatments from study.demographicsActiveTreatments t where h.id = t.id) > 0 THEN 'Yes'
         ELSE null
         END as ActiveClinicalTreatment,
+    CASE
+        WHEN (Select totalClinicalTreatments FROM study.demographicsActiveTreatments t WHERE h.id = t.id and activeTreatments LIKE '%diet%') > 0 THEN 'Yes'
+        ELSE null
+        END as ActiveDiets,
     h.date as InDate,
     h.room.building as Building,
     h.room.area as Area,
@@ -16,4 +20,5 @@ Select
     r.totalAnimals as TotalAnimals
 From study.housing h, roomUtilization_temp r
 Where h.room = r.room
-And h.date >= timestampadd(SQL_TSI_DAY, -1, now()) --'03-08-2021'
+--   And h.date >= '03-01-2021'
+And h.date >= timestampadd(SQL_TSI_DAY, -1, now())
