d13b9f02a489f1f4736d36129e7b2ea956a7c3af
Lakshmi Kolli <kollil@ohsu.edu>
2021-04-21 21:36:15 -0700

    Added groups info to SLA purchase data and changed the billing container in PMIC

--- onprc_ehr/resources/queries/study/PMIC_ChargeTypes.sql
+++ onprc_ehr/resources/queries/study/PMIC_ChargeTypes.sql
@@ -1,3 +1,3 @@
 SELECT chargetype
-From Site.{substitutePath moduleProperty('onprc_billing','BillingContainer')}.onprc_billing.chargeUnits
+From Site.{substitutePath moduleProperty('ONPRC_Billing','BillingContainer_Public'}.onprc_billing.chargeUnits
 Where servicecenter is null or servicecenter like 'PMIC'
