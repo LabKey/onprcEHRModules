a28267fd3cd2eb5fef15d32b0b43951678fa1b6e
Lakshmi Kolli <kollil@ohsu.edu>
2021-05-11 13:25:01 -0700

    Covid db scripts

--- extscheduler/src/org/labkey/extscheduler/ExtSchedulerModule.java
+++ extscheduler/src/org/labkey/extscheduler/ExtSchedulerModule.java
@@ -27,7 +27,7 @@ public class ExtSchedulerModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.503;
+        return 22.002;
     }
 
     @Override
