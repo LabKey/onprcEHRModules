25ac83ef13d90bdd6ce8b18a7e0e357924a57146
jonesga <jonesga@ohsu.edu>
2021-05-26 10:24:10 -0700

    Updating scheduler Schema for Covid Issues

---
+++ extscheduler/resources/schemas/dbscripts/sqlserver/extscheduler-22.006-22.007.sql
@@ -0,0 +1,18 @@
+--2021-05-25 Modified view  to Address modification of Processing to Events DB
+--Created by jonesga
+EXEC core.fn_dropifexists 'vw_Covid19DCMDaily','extscheduler','VIEW';
+/****** Object:  View [extscheduler].[vw_Covid19DCMDaily]    Script Date: 5/25/2021 5:34:40 AM ******/
+SET ANSI_NULLS ON
+    GO
+
+SET QUOTED_IDENTIFIER ON
+    GO
+
+CREATE OR ALTER VIEW [extscheduler].[vw_Covid19DCMDaily]
+AS
+SELECT        Id, StartDate, Container, Comments, ResourceId, Quantity
+FROM            extscheduler.Events AS e
+WHERE        (Quantity > 1) AND (Comments NOT LIKE '%-%') AND (DateDisabled IS NULL) AND (Container IN
+                    (SELECT        Container  FROM            extscheduler.Events
+                      WHERE        (ResourceId = 67)))
+
