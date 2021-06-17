6f254530a74b949043d7e2f55389e3a4ac4ec6ff
Josh Eckels <jeckels@labkey.com>
2021-06-17 16:08:19 -0700

    Support pipeline job to aggregate MHC data prior to ETL (#188)
    * Support pipeline job to aggregate MHC data prior to ETL
    
    * Add MHC pipeline job itself
    
    * Use objectId as sole PK
    
    * Better logging
    
    Co-authored-by: bbimber <bbimber@gmail.com>

--- GeneticsCore/resources/queries/geneticscore/mhc_data_source.sql
+++ GeneticsCore/resources/queries/geneticscore/mhc_data_source.sql
@@ -1,41 +1,49 @@
-SELECT
-  s.subjectId as Id,
-  s.allele,
-  --s.primerPairs,
-  s.shortName,
-  s.totalRecords as totalTests,
-  s.status as result,
-  cast('SSP' as varchar) as type
+-- This is the source query for aggregating MHC data into geneticscore.mhc_data
+SELECT t.subjectId, t.allele as marker, t.result, GROUP_CONCAT(distinct t.assaytype) as assaytype, sum(totalTests) as totalTests
 
-FROM assay.SSP_assay.SSP.SSP_Summary s
-WHERE s.subjectId is not null
+FROM (
+  SELECT
+    s.subjectId,
+    s.allele,
+    --s.primerPairs,
+    s.shortName,
+    s.totalRecords as totalTests,
+    s.status as result,
+    cast('SSP' as varchar) as assaytype
 
-UNION ALL
+  FROM assay.SSP_assay.SSP.SSP_Summary s
+  WHERE s.subjectId is not null
 
-SELECT
-  a.subjectId as Id,
-  a.marker as allele,
-  null as shortName,
-  count(*) as totalTests,
-  cast('POS' as varchar) as result,
-  cast('SBT' as varchar) as type
+  UNION ALL
 
-FROM assay.GenotypeAssay.Genotype.Data a
-WHERE a.run.assayType = 'SBT'
-GROUP BY a.subjectid, a.marker
+  SELECT
+    a.subjectId,
+    a.marker as allele,
+    null as shortName,
+    count(*) as totalTests,
+    cast('POS' as varchar) as result,
+    cast('SBT' as varchar) as assaytype
 
-UNION ALL
+  FROM assay.GenotypeAssay.Genotype.Data a
+  WHERE a.run.assayType = 'SBT'
+  GROUP BY a.subjectid, a.marker
 
-SELECT
-  DISTINCT s.subjectId as Id,
-           p.ref_nt_name as allele,
-           null as shortName,
-           1 as totalTests,
-           cast('NEG' as varchar) as result,
-           cast('SBT' as varchar) as type
+  UNION ALL
 
- --we want any IDs with SBT data, but lacking data for these special-cased markers
-FROM genotypeassays.primer_pairs p
-       FULL JOIN (SELECT DISTINCT subjectId FROM assay.GenotypeAssay.Genotype.Data s WHERE s.run.assayType = 'SBT') s ON (1=1)
-       LEFT JOIN assay.GenotypeAssay.Genotype.Data a ON (a.run.assayType = 'SBT' AND p.ref_nt_name = a.marker AND a.subjectid = s.subjectid)
-WHERE a.rowid IS NULL AND (p.ref_nt_name LIKE 'Mamu-A%' OR p.ref_nt_name LIKE 'Mamu-B%')
+  SELECT
+    DISTINCT s.subjectId,
+             p.ref_nt_name as allele,
+             null as shortName,
+             1 as totalTests,
+             cast('NEG' as varchar) as result,
+             cast('SBT' as varchar) as assaytype
+
+   --we want any IDs with SBT data, but lacking data for these special-cased markers
+  FROM genotypeassays.primer_pairs p
+         FULL JOIN (SELECT DISTINCT subjectId FROM assay.GenotypeAssay.Genotype.Data s WHERE s.run.assayType = 'SBT') s ON (1=1)
+         LEFT JOIN assay.GenotypeAssay.Genotype.Data a ON (a.run.assayType = 'SBT' AND p.ref_nt_name = a.marker AND a.subjectid = s.subjectid)
+  WHERE a.rowid IS NULL AND (p.ref_nt_name LIKE 'Mamu-A%' OR p.ref_nt_name LIKE 'Mamu-B%')
+
+  ) t
+
+GROUP BY t.subjectId, t.allele, t.result
---
+++ GeneticsCore/resources/schemas/dbscripts/sqlserver/geneticscore-17.11-17.12.sql
@@ -0,0 +1,2 @@
+ALTER TABLE geneticscore.mhc_data ADD objectid ENTITYID;
+ALTER TABLE geneticscore.mhc_data ADD totalTests int;
