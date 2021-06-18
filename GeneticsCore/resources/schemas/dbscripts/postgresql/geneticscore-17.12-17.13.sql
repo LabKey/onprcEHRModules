0024f5523dd0f8938af123ac32ce6dc67c9db4f2
labkey-jeckels <Josh Eckels>
2021-06-18 09:00:34 -0700

    Fix PG syntax making column non-nullable

--- GeneticsCore/resources/schemas/dbscripts/postgresql/geneticscore-17.12-17.13.sql
+++ GeneticsCore/resources/schemas/dbscripts/postgresql/geneticscore-17.12-17.13.sql
@@ -1,3 +1,3 @@
 ALTER TABLE geneticscore.mhc_data DROP CONSTRAINT PK_mhc_data;
-ALTER TABLE geneticscore.mhc_data ALTER COLUMN objectid NOT NULL;
+ALTER TABLE geneticscore.mhc_data ALTER COLUMN objectid SET NOT NULL;
 ALTER TABLE geneticscore.mhc_data ADD CONSTRAINT PK_mhc_data PRIMARY KEY (objectid);
