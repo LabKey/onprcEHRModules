d0255e745a1d4c158a3920edab13cb373f78f00b
jonesga <jonesga@ohsu.edu>
2021-06-21 08:24:08 -0700

    SQL queries and xml for Weekly and Daily SCheduler View

---
+++ extscheduler/resources/queries/extscheduler/Covid19WeeklyEvents.sql
@@ -0,0 +1,23 @@
+SELECT e.Id,
+
+e.ResourceID,
+--e.startDate,
+Cast(e.startDate as date) EventDate,
+Cast(e.startDate as time ) EventTime,
+(Select LastName+ ', ' + FirstName from core.users where userID = e.userID) as PartipantName,
+e.Name,
+e.UserID,
+e.EmployeeID,
+e.Created,
+e.CreatedBy,
+e.Modified,
+e.ModifiedBy,
+e.Recorded,
+e.RecordedBy,
+e.ComplianceUpdated,
+e.DateCompleted,
+e.dateDisabled
+
+FROM events e
+where (week(e.StartDate) = week(Now())
+and e.datedisabled is null and e.ResourceID not in (69,77))
