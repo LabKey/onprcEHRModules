e3f769214a50611d92645dd9d21cedc03d4a2d07
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-07-07 12:15:08 -0700

    Modified the column header to show Orchard instead of Merge.  Modified lab request message.

--- onprc_ehr/resources/web/onprc_ehr/panel/LabworkRequestDataEntryPanel.js
+++ onprc_ehr/resources/web/onprc_ehr/panel/LabworkRequestDataEntryPanel.js
@@ -16,7 +16,7 @@ Ext4.define('ONPRC_EHR.panel.LabworkRequestDataEntryPanel', {
             Ext4.Msg.hide();
 
         if (extraContext && extraContext.successURL){
-                Ext4.Msg.alert('Message', 'Your Lab Request have been synced into Orchard. Please print your labels by accessing the Orchard app located on your Windows desktop. If you need an Orchard Login, please email the ONPRC Clin Path Lab at ONPRCClinPathLab@ohsu.edu.' +
+                Ext4.Msg.alert('Message', 'The Lab Requests have been synced into Orchard. Please print your labels by accessing the Orchard app located on your Windows desktop. If you need an Orchard Login, please email the ONPRC Clin Path Lab at ONPRCClinPathLab@ohsu.edu.' +
                         (!Ext4.isIE ? '  Note: There may be a delay of about ten minutes before you are able to print your labels through the Orchard application.' : ''), function(val){
                     window.onbeforeunload = Ext4.emptyFn;
                     window.location = extraContext.successURL;
