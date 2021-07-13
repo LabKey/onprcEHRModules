538fdca3f02d72db3d6b394eacec1851e679db89
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-07-12 19:47:36 -0700

    Instead of using Core.users, modified code to use onprc_ehr.usersActiveNames.

--- onprc_ehr/resources/queries/onprc_ehr/Labwork_Requestor_Vets.sql
+++ onprc_ehr/resources/queries/onprc_ehr/Labwork_Requestor_Vets.sql
@@ -1,4 +1,4 @@
 
-select a.*, b.UserId, Left(b.Email,CHARINDEX('@',b.Email)-1) as emailName from Reference_StaffNames a, core.users b
+select a.*, b.UserId, Left(b.Email,CHARINDEX('@',b.Email)-1) as emailName from Reference_StaffNames a, onprc_ehr.usersActiveNames b
 where a.username = Left(b.Email,CHARINDEX('@',b.Email)-1)
   and b.active = 'true'
