d188f50cc40dfebc183b9152dbd4719a59d6fce6
Lakshmi Kolli <kollil@ohsu.edu>
2021-07-30 10:59:00 -0700

    Update Prima ETL process - Final take2

--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -120,7 +120,7 @@ public class ONPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.003;
+        return 21.004;
     }
 
     @Override
