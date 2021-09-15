da2c27337f1cece799b8de7e4b0a7920bbf51b9f
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2021-09-15 13:56:01 -0700

    Updated ASB spe ins code: 9/15/21

--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -120,7 +120,7 @@ public class ONPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.002;
+        return 21.101;
     }
 
     @Override
