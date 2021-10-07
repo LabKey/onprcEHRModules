bf7c53647f8e48d52cf30879c27654bf906ec5ed
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-10-07 09:57:33 -0700

    Added two new input screens and added one new tables.

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceRecentTests.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceRecentTests.sql
@@ -19,13 +19,13 @@ select b.requirementname,
 
        (select max(zz.date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid  ) as completiondate,
 
+    CAST(
      (select
-        CAST(
          CASE
             WHEN max(pq.date) is null then 0
             WHEN Tr.expireperiod = 0 then null
             ELSE ( Tr.expireperiod - ( age_in_months(max(pq.date), curdate()))  )
-        END AS double)  from completiondates pq, requirements Tr where pq.requirementname = b.requirementname And Tr.requirementname = pq.requirementname group by tr.expireperiod)
+        END  from completiondates pq, requirements Tr where pq.requirementname = b.requirementname And Tr.requirementname = pq.requirementname group by tr.expireperiod) AS double)
          AS MonthsUntilRenewal
 
 
