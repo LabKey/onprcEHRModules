c640927b67acfbab36290a30375b79d14c2c8049
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2021-10-27 14:16:46 -0700

    updated query

--- onprc_billing/resources/queries/onprc_billing/ARTCoreBIllingCharges.sql
+++ onprc_billing/resources/queries/onprc_billing/ARTCoreBIllingCharges.sql
@@ -1,5 +1,5 @@
 -- Query to get ART core chargeable items
 SELECT rowid, category, activeRate.unitCost, itemCode + ' - ' + name as chargeName
 FROM "/onprc/admin/finance/public".onprc_billing_public.chargeableItems
-WHERE category = 'ART'
+WHERE category = 'ART Core'
   AND Active = 'true'
