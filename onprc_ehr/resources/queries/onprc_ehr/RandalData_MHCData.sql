cd2726fa2cbb24234d5ec09180075f584d7822b4
jonesga <jonesga@ohsu.edu>
2021-10-28 12:43:44 -0700

    Update of QUery to assign correct value for type

---
+++ onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
@@ -0,0 +1,11 @@
+SELECT d.rowid,
+       d.subjectId,
+       d.datatype,
+       d.marker,
+       d.result,
+       d.score,
+       d.assaytype,
+       d.container,
+       d.totalTests
+FROM  StudyDetails_RandalData s, "/ONPRC/Core Facilities/Genetics Core/MHC_Typing".geneticscore.mhc_data d
+where (active = 'y' and Cast(s.rh as varchar(25)) = Cast(d.subjectid as varchar(25)))
