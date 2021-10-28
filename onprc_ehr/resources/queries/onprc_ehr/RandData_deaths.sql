a6958c6b1d34c7a47522189e0c63a1c8b190637e
Gary Jones <jonesga@ohsu.edu>
2021-10-28 12:28:25 -0700

    21.7 fb hansen queryto file xml (#270)
    * values is now a keyword (#212)
    
    * Item 9192: Initial modifications to support sample status  (#245)
    
    * Hansen Study Animal Queries xml to Files
    
    * Added File Structure for Details Table for Randal
    
    * Update to add StudyDetails_RandalData to xml for onprc_ehr
    
    * Update to add additional Field for Protocol for Annual Update Last Approved
    
    * Inserting needed queries for Hansen Project
    
    * Up0date to ETL for Hansen to File
    
    * Update RandData_AnimalGroups.sql
    
    Change of Source in Query
    
    * Update of SOurce Statment to use Suggestion from Josh
    
    * Update of SOurce Statment to use Suggestion from Josh
    
    * Removed unwanted " from query and added etl script
    
    * Add columns to match production dataset definitions
    
    * Updates per identified test failures on GIt Hub and Team City
    
    * Update of QUery to assign correct value for type
    
    * More onprc_ehr.xml fixes
    
    Co-authored-by: github-actions <teamcity@labkey.com>
    Co-authored-by: labkey-matthewb <matthewb@labkey.com>
    Co-authored-by: Susan Hert <susanh@labkey.com>
    Co-authored-by: labkey-jeckels <Josh Eckels>

---
+++ onprc_ehr/resources/queries/onprc_ehr/RandData_deaths.sql
@@ -0,0 +1,23 @@
+SELECT
+d.Id,
+d.date,
+d.cause,
+d.necropsy,
+d.remark,
+d.cageattime,
+d.dam,
+d.performedby,
+d.roomattime,
+d.notAtCenter,
+d.description,
+d.taskid,
+d.project,
+d.requestid,
+d.Container,
+d.history,
+d.isAssignedAtTime,
+d.isAssignedToProtocolAtTime,
+d.enteredSinceVetReview,
+d.QCState
+ FROM  StudyDetails_RandalData s, Site.{substitutePath moduleProperty('EHR','EHRStudyContainer')}.study.deaths d
+    where (active = 'y' and s.rh = d.id)
