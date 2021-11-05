88cab69d4283b247dd812f6d4027579408db1216
jonesga <jonesga@ohsu.edu>
2021-11-05 09:48:30 -0700

    Update to MHC Pull to render from correct folder using sustitute path method

--- onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
+++ onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
@@ -9,5 +9,5 @@ SELECT d.rowid,
        d.assaytype,
        d.container,
        d.totalTests
-FROM  StudyDetails_RandalData s, "/ONPRC/Core Facilities/Genetics Core/MHC_Typing".geneticscore.mhc_data d
+FROM  StudyDetails_RandalData s, Site.{substitutePath moduleProperty('ONPRC_EHR','MHC_COntainer')}.geneticscore.mhc_data d
 where (active = 'y' and Cast(s.rh as varchar(25)) = Cast(d.subjectid as varchar(25)))
