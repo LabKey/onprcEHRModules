45df4becd6d569d11e71c8202b43cacec29f0b2a
bbimber <bbimber@gmail.com>
2021-11-09 15:32:58 -0800

    Correct field name in MHC query (#286)
    * Correct field name in MHC query and update percent calculation

--- GeneticsCore/resources/queries/geneticscore/mhc_data_source.sql
+++ GeneticsCore/resources/queries/geneticscore/mhc_data_source.sql
@@ -2,7 +2,7 @@
 
 SELECT
     s.subjectId,
-    s.allele,
+    s.allele as marker,
     --s.primerPairs,
     s.shortName,
     s.totalRecords as totalTests,
@@ -17,12 +17,12 @@ UNION ALL
 
 SELECT
     a.subjectId,
-    a.marker as allele,
+    a.marker,
     null as shortName,
     count(*) as totalTests,
     cast('POS' as varchar) as result,
     cast('SBT' as varchar) as assaytype,
-    sum(a.result) / (SELECT count(DISTINCT a2.analysisId) as total FROM assay.GenotypeAssay.Genotype.Data a2 WHERE a.subjectId = a.subjectId ) AS score,
+    sum(a.result) / (SELECT count(DISTINCT a2.analysisId) as total FROM assay.GenotypeAssay.Genotype.Data a2 WHERE a2.subjectId = a.subjectId AND a2.run.assayType = 'SBT' ) AS score,
 
 FROM assay.GenotypeAssay.Genotype.Data a
 WHERE a.run.assayType = 'SBT'
@@ -32,7 +32,7 @@ UNION ALL
 
 SELECT
 DISTINCT s.subjectId,
-         p.ref_nt_name as allele,
+         p.ref_nt_name as marker,
          null as shortName,
          1 as totalTests,
          cast('NEG' as varchar) as result,
