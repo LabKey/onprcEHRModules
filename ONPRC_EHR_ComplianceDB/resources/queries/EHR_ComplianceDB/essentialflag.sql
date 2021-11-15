1f67b596934b761d4a804b2486428dfde1fdc4eb
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-11-15 11:28:49 -0800

    Fixing Lab work Vet Names Display Issues

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/essentialflag.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/essentialflag.sql
@@ -1,3 +1,3 @@
-select value from "/ONPRC/EHR".sla.Reference_Data
+select value from Site.{substitutePath moduleProperty('EHR','EHRStudyContainer')}.sla.Reference_Data
 where columnName = 'Necropsydist'
   And enddate is null
