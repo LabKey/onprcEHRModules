021c1882d63f08e8ec1ec056aa1893dc7a0c2d48
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-11-19 16:29:24 -0800

    Modified a Pop up Window message after submitting a Lab Request entry into Prime. First feature branch included extraneous files.

--- onprc_ehr/resources/web/onprc_ehr/panel/LabworkRequestDataEntryPanel.js
+++ onprc_ehr/resources/web/onprc_ehr/panel/LabworkRequestDataEntryPanel.js
@@ -16,10 +16,14 @@ Ext4.define('ONPRC_EHR.panel.LabworkRequestDataEntryPanel', {
             Ext4.Msg.hide();
 
         if (extraContext && extraContext.successURL){
-                Ext4.Msg.alert('Message', 'The Lab Requests have been synced into Orchard. Please print your labels by accessing the Orchard app located on your Windows desktop. If you need an Orchard Login, please email the ONPRC Clin Path Lab at ONPRCClinPathLab@ohsu.edu.' +
-                        (!Ext4.isIE ? '  Note: There may be a delay of about ten minutes before you are able to print your labels through the Orchard application.' : ''), function(val){
-                    window.onbeforeunload = Ext4.emptyFn;
-                    window.location = extraContext.successURL;
+            Ext4.Msg.alert('Message', 'The Lab Requests have been synced into Orchard.  ' +
+                    '<b>Clin Path orders in Prime can only be done ahead of time and no later than 4pm on the business day prior to when the samples are submitted to Clin Path. Labels will ' +
+                    'not be available to print from Orchard until the next business day after the orders are placed. If same day orders are needed, please place these orders in Orchard. </b>' +
+                    'If you have questions, please contact the Clin Path Lab at 503-346-5297. Please print your labels by accessing the Orchard app located' +
+                    ' on your Windows desktop. ' +
+                    (!Ext4.isIE ? 'If you need an Orchard Login, please email the ONPRC Clin Path Lab at ONPRCClinPathLab@ohsu.edu.  Thank you.' : ''), function(val){
+                window.onbeforeunload = Ext4.emptyFn;
+                window.location = extraContext.successURL;
 
                 }, this);
                 return;
