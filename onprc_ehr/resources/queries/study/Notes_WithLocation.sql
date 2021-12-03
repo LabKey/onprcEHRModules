91a600bfb6c586d45e3f4bf4d19c65342d1461a7
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2021-12-02 18:26:05 -0800

    21.7 fb bsu alerts (#309)
    * Added location the two alerts: Flags and Notes
    
    * Fix tests by adding "category" field to study.flags dataset
    
    * Another missing field
    
    * Updated flags query
    
    * Updated date format for the Notes alert
    
    * Updated the date filter to actiondate
    
    * Updated the method to extract animal location in the query
    
    Co-authored-by: labkey-jeckels <Josh Eckels>

--- onprc_ehr/resources/queries/study/Notes_WithLocation.sql
+++ onprc_ehr/resources/queries/study/Notes_WithLocation.sql
@@ -1,6 +1,11 @@
 Select
     Id,
-    housingattime.roomattime + ' ' + housingattime.cageattime as location,
+--     housingattime.roomattime + ' ' + housingattime.cageattime as location,
+--     id.curLocation.room + ' ' +  id.curLocation.cage as location,
+    CASE
+        WHEN id.curLocation.cage is null then id.curLocation.room
+        ELSE (id.curLocation.room || '-' || id.curLocation.cage)
+        END AS Location,
     date,
     actiondate,
     enddate,
