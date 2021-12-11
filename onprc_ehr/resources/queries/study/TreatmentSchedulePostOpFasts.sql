6124f440d5cd833cb76f5192d7fd345606882fec
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-12-10 16:13:55 -0800

    Modified the date range to report Fast Alert records. For reports between 4Pm from previous day to 8am of the current day.

--- onprc_ehr/resources/queries/study/TreatmentSchedulePostOpFasts.sql
+++ onprc_ehr/resources/queries/study/TreatmentSchedulePostOpFasts.sql
@@ -29,8 +29,5 @@ Select
     curdate() */
 From encounters
 Where procedureid in (1804,1807,2440) -- get these procedures only
-  And year(date) = year(now())  -- get today
-  And month(date) = month(now())
-  And dayofmonth(date) = dayofmonth(now())
-  And ((hour(date) >= 17 and hour(date) <= 24 ) OR (hour(date) >= 1 and hour(date) <= 8)) -- 4pm to 8am
---And CAST(date AS TIME) between '16:00' And '7:30'
+ And (date > cast(TimeStampAdd('SQL_TSI_DAY',-1,cast(cast(now() as date) as varchar(30)) + ' ' + '16:00') as TIMESTAMP)
+  And date < cast(cast(cast(now() as date) as varchar(30)) + ' ' + '08:00' as TIMESTAMP)  )  ---Between 4pm thru 8am
