e3783d40f436e6854179a8f7bfdf42e9aad352cf
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-12-14 09:07:53 -0800

    Modified the date range to report Fast Alert records. For reports between 4Pm from previous day to 8am of the current day.

--- onprc_ehr/resources/queries/study/TreatmentSchedulePostOpFasts.sql
+++ onprc_ehr/resources/queries/study/TreatmentSchedulePostOpFasts.sql
@@ -3,11 +3,12 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
 
  Created by Kolli on Nov 1st, 2021
- Create an alert to show the following fasting procedures prescribed on the current day between 16:00 - 7:30Am (4pm to 8am)
+ Create an alert to show the following fasting procedures prescribed on the current day between 16:00 thru 8am following day
  1. Complete NPO - 2440
  2. Overnight Fast - 1807
  3. AM Fast - 1804
 */
+
 Select
     Id,
     id.curLocation.room + ' ' +  id.curLocation.cage as location,
@@ -21,12 +22,7 @@ Select
     performedby,
     taskid,
     QCState.label as status
-    /*hour(date),
-    minute(date),
-    year(date),year(now()),
-    month(date),month(now()),
-    dayofmonth(date),dayofmonth(now()),
-    curdate() */
+
 From encounters
 Where procedureid in (1804,1807,2440) -- get these procedures only
  And (date < cast(TimeStampAdd('SQL_TSI_DAY',1,cast(cast(now() as date) as varchar(30)) + ' ' + '08:00') as TIMESTAMP)
