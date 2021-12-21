fb5f21f0b422a98776ec887ec059353a9f1bbdaf
Gary Jones <jonesga@ohsu.edu>
2021-12-21 06:21:47 -0700

    update for MHC Pull to correct format and comment insert

--- onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
+++ onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
---Update to use corredct method to cross container
---jonesga 11/5/2021
-=======
-
->>>>>>> origin/release21.7-SNAPSHOT
 SELECT d.rowid,
        d.subjectId,
        d.datatype,
@@ -13,10 +7,6 @@ SELECT d.rowid,
        d.assaytype,
        d.container,
        d.totalTests
-<<<<<<< HEAD
-FROM  StudyDetails_RandalData s, "/ONPRC/Core Facilities/Genetics Core/MHC_Typing".geneticscore.mhc_data d
-=======
-
 FROM  StudyDetails_RandalData s, Site.{substitutePath moduleProperty('ONPRC_EHR','MHC_Container')}.geneticscore.mhc_data d
->>>>>>> origin/release21.7-SNAPSHOT
+
 where (active = 'y' and Cast(s.rh as varchar(25)) = Cast(d.subjectid as varchar(25)))
