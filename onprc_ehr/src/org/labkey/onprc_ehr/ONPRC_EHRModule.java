d7efb59ef4dfac8aa9bd3a6c0bd4c277897c1f25
Adam Rauch <adam@labkey.com>
2022-01-17 15:43:22 -0800

    Drop unused stored procedure that was created in "dbo" instead of "onprc_ehr"

--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -121,7 +121,7 @@ public class ONPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.202;
+        return 22.001;
     }
 
     @Override
