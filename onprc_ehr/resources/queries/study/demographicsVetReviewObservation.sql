86335469aa364083d9619bae87fce2896fe36bcb
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-02-02 14:06:50 -0800

    Provide an enhanced version of the Vet review that incldes entries made to Clinical Observation records since the last most recent Vet review.

--- onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
+++ onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
@@ -1,5 +1,5 @@
 select dem.*,
-       ( select group_concat(DISTINCT j.remark, chr(10))  from "Clinical Observations" j where j.id = dem.id  and j.remark is not null and j.date >= (select max(k.date)
+       ( select j.observation + ':   ' +  j.remark  from "Clinical Observations" j where j.id = dem.id  and j.category = 'Observations' and j.remark is not null and j.date >= (select max(k.date)
                                                                                                                                                       from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) ) as observationremarks,
        ( select group_concat(DISTINCT s.remark, chr(10))  from "Clinical Remarks" s where s.id = dem.id and s.remark is not null  and s.date >= (select max(k.date)
                                                                                                                                                  from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.id) ) as clinicalremarks
@@ -7,7 +7,7 @@ select dem.*,
 
 from demographics dem
 Where
-        dem.id in (Select cln.id from "Clinical Observations" cln where cln.category = 'Observation' and cln.remark is not null
+        dem.id in (Select cln.id from "Clinical Observations" cln where cln.category = 'Observations' and cln.remark is not null
                                                                     and cln.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.qcstate = 18
                                                                                                                                          and r.id = cln.id)  )
    Or dem.id in (Select rem.id from "Clinical Remarks" rem where  rem.remark is not null
