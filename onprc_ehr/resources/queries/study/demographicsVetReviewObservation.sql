90308c858c41045a7aea225d63793057a26c7d1f
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-02-04 18:22:53 -0800

    Provide an enhanced version of the Vet review that incldes entries made to Clinical Observation records since the last most recent Vet review.

--- onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
+++ onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
@@ -2,18 +2,17 @@ select dem.*,
        (select s.observations from study.mostRecentClinicalObservationsForAnimal s  where s.id = dem.id) as mostRecentClinicalObservations,
        (select s.date from study.mostRecentClinicalObservationsForAnimal s  where s.id = dem.id) as mostRecentClinicalObservationsdate,
        ( select cast(cast(j.date as date) as varchar(20)) + '-' + j.category + ':' + group_concat(DISTINCT j.remark, chr(10))  from "Clinical Observations" j where j.id = dem.id  and j.category = 'Observations' and j.remark is not null and j.date >= (select max(k.date)
-                                                                                                                                                      from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as observationremarks,
+                                                      from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as observationremarks,
        ( select group_concat(DISTINCT s.remark, chr(10))  from "Clinical Remarks" s where s.id = dem.id and s.remark is not null  and s.date >= (select max(k.date)
-                                                                                                                                                 from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.id) ) as clinicalremarks
+                                                      from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.id) ) as clinicalremarks
 
 
 from study.demographics dem
-Where
-        dem.id in (Select cln.id from "Clinical Observations" cln where cln.category = 'Observations' and cln.remark is not null
+  Where  dem.id in (Select cln.id from "Clinical Observations" cln where cln.category = 'Observations' and cln.remark is not null
                   and cln.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.qcstate = 18
                  and r.id = cln.id)  )
-   Or dem.id in (Select rem.id from "Clinical Remarks" rem where  (rem.remark is not null or rem.description is not null)
-    and rem.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.qcstate = 18
+   Or dem.id in (Select rem.id from "Clinical Remarks" rem where rem.category = 'Clinical'
+     and rem.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.qcstate = 18
     and rem.id = r.id)  )
 
 
