2a15a029f81ff65f0b3a7b057f905c0734a237dd
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-02-08 13:12:49 -0800

    Provide an optional weight's data when running a grid report that contains BCS scores.  Provide weight's data that corresponds to the nearest weight date to the most recent BCS Score date.

--- onprc_ehr/resources/queries/study/BCSScoreWeights.sql
+++ onprc_ehr/resources/queries/study/BCSScoreWeights.sql
@@ -29,9 +29,10 @@ FROM study.Clinical_Observations p, study.weight K
 WHERE p.qcstate.publicdata = true
   And (p.id = k.id)
   And p.category = 'BCS'
-  And p.date in (Select max(r.date) from study.Clinical_Observations r, study.Weight j
-                 Where r.category = 'BCS' And r.id = p.id And r.id = j.id
-                   And (TIMESTAMPDIFF('SQL_TSI_DAY', j.date, r.date) < 8) And r.qcstate = 18 And j.qcstate = 18)
+  And p.date in (Select max(r.date) from study.Clinical_Observations r
+                 Where r.category = 'BCS' And r.id = p.id
+                    And r.qcstate = 18 )
 
-  And (K.date in (select min(s.date) as date  from study.Weight s Where s.Id = k.Id And ( (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) < 0) And (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) > -8) ) And s.qcstate = 18)
-    or K.date in (select max(s.date) as date  from study.Weight s Where s.Id = k.Id And ( (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) < 8) And (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) >= 0) ) And s.qcstate = 18)  )
+  And (K.date in
+   (coalesce((select max(s.date) as date  from study.Weight s Where s.Id = k.Id And ( (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) < 8) And (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) >= 0) ) And s.qcstate = 18),
+      (select min(s.date) as date  from study.Weight s Where s.Id = k.Id And ( (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) < 0) And (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) > -8) ) And s.qcstate = 18)) ) )
