5ebd4f6978ce815ebdc6c310496b53e7f54b7ad7
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-02-11 15:28:25 -0800

    Modified to include an option to display the weight of a monkey closest to the most recent BCS Score date.

--- onprc_ehr/resources/queries/study/BCSScoreWeights.sql
+++ onprc_ehr/resources/queries/study/BCSScoreWeights.sql
@@ -29,10 +29,9 @@ FROM study.Clinical_Observations p, study.weight K
 WHERE p.qcstate.publicdata = true
   And (p.id = k.id)
   And p.category = 'BCS'
-  And p.date in (Select max(r.date) from study.Clinical_Observations r, study.Weight j
-                 Where r.category = 'BCS' And r.id = p.id And r.id = j.id And r.QCState.Label = 'Completed'
-                   And (TIMESTAMPDIFF('SQL_TSI_DAY', j.date, r.date) < 8) And r.qcstate = 18 And j.QCState.Label = 'Completed')
-
+  And p.date in (Select max(r.date) from study.Clinical_Observations r
+                 Where r.category = 'BCS' And r.id = p.id
+                    And r.QCState.Label = 'Completed' )
 
   And (K.date in
    (coalesce((select max(s.date) as date  from study.Weight s Where s.Id = k.Id And ( (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) < 8) And (TIMESTAMPDIFF('SQL_TSI_DAY', s.date, p.date) >= 0) ) And s.QCState.Label = 'Completed'),
