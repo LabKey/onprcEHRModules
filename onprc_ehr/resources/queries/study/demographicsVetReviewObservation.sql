05c9de06d6c5653e993c21b0283c79477de9a444
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-02-24 09:13:59 -0800

    Modified Vet Review program to include other requirements when generating a report.

--- onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
+++ onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
@@ -5,14 +5,22 @@ select dem.*,
                                                       from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as observationremarks,
        ( select group_concat(DISTINCT s.remark, chr(10))  from "Clinical Remarks" s where s.id = dem.id and s.description is not null  and s.date >= (select max(k.date)
                                                       from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.id) ) as clinicalremarks
+    ( select group_concat(DISTINCT s.remark, chr(10)) from "Clinical Encounters" rem where rem.type.value = 'Procedure' and rem.procedure.name
+                    in ('Ultrasound','Ultrasound - Ovaries','Ultrasound - Pregnancy', 'Ultrasound - Uterus','Dental prophylaxis') and rem.date >= (select max(k.date)
+                                                   from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.id) ) as clinicalremarks
 
 
 from study.demographics dem
-  Where  dem.id in (Select cln.id from "Clinical Observations" cln where cln.category = 'Observations' and cln.remark is not null
+  Where  dem.id in (Select cln.id from "Clinical Observations" cln where cln.category in ('Observations', 'BCS') and (cln.remark is not null or cln.observation is not null)
                   and cln.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.QCState.Label = 'Completed'
                                                                                        and r.id = cln.id)  )
    Or dem.id in (Select rem.id from "Clinical Remarks" rem where rem.category = 'Clinical' and rem.description is not null
      and rem.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.QCState.Label = 'Completed'
     and rem.id = r.id)  )
+
+     Or dem.id in (Select rem.id from "Clinical Encounters" rem where rem.type.value = 'Procedure' and rem.procedure.name
+                    in ('Ultrasound','Ultrasound - Ovaries','Ultrasound - Pregnancy', 'Ultrasound - Uterus','Dental prophylaxis')
+                                                               and rem.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.QCState.Label = 'Completed'
+                                                                                                                                    and rem.id = r.id)  )
 
 
