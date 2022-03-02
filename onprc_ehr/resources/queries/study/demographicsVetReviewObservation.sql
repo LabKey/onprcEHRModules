1b1e1580500d91dc375c5d025e3f0f4113ef381f
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-03-01 22:06:33 -0800

    Modified Vet Review reporting program to now include Clinical Procedures, and BCS Scores since last most recent Vet Review.

--- onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
+++ onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
@@ -1,6 +1,10 @@
 select dem.*,
        (select s.observations from study.mostRecentClinicalObservationsForAnimal s  where s.id = dem.id) as mostRecentClinicalObservations,
        (select s.date from study.mostRecentClinicalObservationsForAnimal s  where s.id = dem.id) as mostRecentClinicalObservationsdate,
+       ( select 'BCS Score: ' + j.value  from "Clinical Observations" j where j.id = dem.id  and j.category = 'BCS' and j.remark is not null and j.date >= (select max(k.date)
+                                                                                     from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as mostRecentBCSScore,
+       ( select 'Clinical Procedure: ' + j.value  from "Clinical Encounters" j where j.id = dem.id  and j.type.value = 'Procedure' and j.date >= (select max(k.date)
+                                                                                               from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as mostRecengProcedure,
        ( select cast(cast(j.date as date) as varchar(20)) + '-' + j.category + ':' + group_concat(DISTINCT j.remark, chr(10))  from "Clinical Observations" j where j.id = dem.id  and j.category = 'Observations' and j.remark is not null and j.date >= (select max(k.date)
                                                       from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as observationremarks,
        ( select group_concat(DISTINCT s.remark, chr(10))  from "Clinical Remarks" s where s.id = dem.id and s.description is not null  and s.date >= (select max(k.date)
