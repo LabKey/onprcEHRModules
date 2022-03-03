643ca478d6de5fa8b6d18e6d7283aab2fc958e53
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-03-02 17:23:12 -0800

    Modified Vet Review reporting program to include BCS Score and Clinical Procedures Information.

--- onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
+++ onprc_ehr/resources/queries/study/demographicsVetReviewObservation.sql
@@ -1,30 +1,33 @@
 select dem.*,
        (select s.observations from study.mostRecentClinicalObservationsForAnimal s  where s.id = dem.id) as mostRecentClinicalObservations,
+
        (select s.date from study.mostRecentClinicalObservationsForAnimal s  where s.id = dem.id) as mostRecentClinicalObservationsdate,
-       ( select 'BCS Score: ' + j.value  from "Clinical Observations" j where j.id = dem.id  and j.category = 'BCS' and j.remark is not null and j.date >= (select max(k.date)
-                                                                                     from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as mostRecentBCSScore,
-       ( select 'Clinical Procedure: ' + j.value  from "Clinical Encounters" j where j.id = dem.id  and j.type.value = 'Procedure' and j.date >= (select max(k.date)
-                                                                                               from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as mostRecentProcedure,
-       ( select cast(cast(j.date as date) as varchar(20)) + '-' + j.category + ':' + group_concat(DISTINCT j.remark, chr(10))  from "Clinical Observations" j where j.id = dem.id  and j.category = 'Observations' and j.remark is not null and j.date >= (select max(k.date)
-                                                      from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by j.date, j.category) as observationremarks,
-       ( select group_concat(DISTINCT s.remark, chr(10))  from "Clinical Remarks" s where s.id = dem.id and s.description is not null  and s.date >= (select max(k.date)
+
+       ( select 'BCS Score: ' + group_concat(DISTINCT j.observation, chr(500))  from "Clinical Observations" j where j.id = dem.id  and j.category = 'BCS'  and j.date >= (select max(k.date)
+                                                                                     from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) ) as mostRecentBCSScore,
+
+       ( select 'Clinical Procedure: ' + group_concat(DISTINCT q.procedureid.name, chr(500))  from "Clinical Encounters" q where q.id = dem.id  and q.type.value = 'Procedure' and q.date >= (select max(k.date)
+                                                                                               from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) ) as mostRecentProcedure,
+
+       ( select cast(cast(t.date as date) as varchar(20)) + '-' + t.category + ':' + group_concat(DISTINCT t.remark, chr(10))  from "Clinical Observations" t where t.id = dem.id  and t.category = 'Observations' and t.remark is not null and t.date >= (select max(k.date)
+                                                      from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.Id) group by t.date, t.category) as observationremarks,
+
+       ( select group_concat(DISTINCT s.remark, chr(500))  from "Clinical Remarks" s where s.id = dem.id and s.description is not null  and s.date >= (select max(k.date)
                                                       from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.id) ) as clinicalremarks
-    ( select group_concat(DISTINCT s.remark, chr(10)) from "Clinical Encounters" rem where rem.type.value = 'Procedure' and rem.procedure.name
-                    in ('Ultrasound','Ultrasound - Ovaries','Ultrasound - Pregnancy', 'Ultrasound - Uterus','Dental prophylaxis') and rem.date >= (select max(k.date)
-                                                   from  "Clinical Observations" k where k.category = 'Vet Review' and dem.id = k.id) ) as clinicalremarks
+
 
 
 from study.demographics dem
   Where  dem.id in (Select cln.id from "Clinical Observations" cln where cln.category in ('Observations', 'BCS') and (cln.remark is not null or cln.observation is not null)
                   and cln.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.QCState.Label = 'Completed'
-                                                                                       and r.id = cln.id)  )
+                                                                                       and r.Id = cln.Id)  )
    Or dem.id in (Select rem.id from "Clinical Remarks" rem where rem.category = 'Clinical' and rem.description is not null
      and rem.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.QCState.Label = 'Completed'
-    and rem.id = r.id)  )
+    and rem.id = r.Id)  )
 
-     Or dem.id in (Select rem.id from "Clinical Encounters" rem where rem.type.value = 'Procedure' and rem.procedure.name
-                    in ('Ultrasound','Ultrasound - Ovaries','Ultrasound - Pregnancy', 'Ultrasound - Uterus','Dental prophylaxis')
-                                                               and rem.date >= (select max(r.date) from "Clinical Observations" r where r.category = 'Vet Review' and r.QCState.Label = 'Completed'
-                                                                                                                                    and rem.id = r.id)  )
+     Or dem.id in (Select rems.id from "Clinical Encounters" rems where rems.type.value = 'Procedure' and rems.procedureid.name
+      in ('Ultrasound','Ultrasound - Ovaries','Ultrasound - Pregnancy', 'Ultrasound - Uterus','Dental prophylaxis')
+                                                                  and rems.date >= (select max(x.date) from "Clinical Observations" x where x.category = 'Vet Review' and x.QCState.Label = 'Completed'
+                                                                                                                                       and rems.Id = x.Id)  )
 
 
