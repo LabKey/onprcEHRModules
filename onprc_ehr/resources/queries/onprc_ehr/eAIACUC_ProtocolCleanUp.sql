64fd3eee49ea67952d7b8c4a9b7bd7b35248cb38
gary Jones <oregonjonesga@outlook.com>
2022-03-07 09:39:08 -0700

    New QUery to be basis for Protocol cleanup

---
+++ onprc_ehr/resources/queries/onprc_ehr/eAIACUC_ProtocolCleanUp.sql
@@ -0,0 +1,10 @@
+SELECT p.protocol,
+       p.external_id,
+       p.renewaldate,
+       p.last_modified,
+       p.displayName,
+       'Protocol End Date and State Updated per reivew with Integrity Team'  as Description,
+       'Expired' as Protocol_Stats
+
+FROM protocol p
+where p.renewaldate <= '3/1/2022' and p.enddate is null)()
