e6f769fa063e84a894ecd265a156132dd7c205ad
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-03-31 21:52:15 -0700

    Modified Necropsy Request form with additional features.

--- onprc_ehr/resources/queries/study/NecropsyRequestdata.sql
+++ onprc_ehr/resources/queries/study/NecropsyRequestdata.sql
@@ -1,7 +1,7 @@
-select * from study.encounters a, study.tissueDistributions,
-              Site.{substitutePath moduleProperty('EHR','EHRStudyContainer')}.onprc_billing.miscCharges e
+select * from study.encounters a, study.tissueDistributions b,
+              onprc_billing.miscCharges e
 where a.type = 'Tissues'
-And a.participantid = b.participantid
-And a.participantid = e.participantid
-And a.requestid = b.requestid
-and a.requestid = e.requestid
+  And a.participantid = b.participantid
+  And a.participantid = e.Id
+  And a.requestid = b.requestid
+  and a.requestid = e.requestid
