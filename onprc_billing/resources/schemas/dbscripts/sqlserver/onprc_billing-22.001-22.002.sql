f32a847e594c5b4dade3a5bbdb18ac5ac31c0036
gary Jones <oregonjonesga@outlook.com>
2022-04-04 13:25:47 -0700

    UPdate of Module number for Billing Module

--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-22.001-22.002.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-22.001-22.002.sql
@@ -1,3 +1,3 @@
 EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'Originating Agency Award Number';
 GO
-ALTER TABLE onprc_billing.aliases ADD [OriginatingAgencyAwareNum] VarChar(255) Null;
+ALTER TABLE onprc_billing.aliases ADD [OriginatingAgencyAwardNum] VarChar(255) Null;
