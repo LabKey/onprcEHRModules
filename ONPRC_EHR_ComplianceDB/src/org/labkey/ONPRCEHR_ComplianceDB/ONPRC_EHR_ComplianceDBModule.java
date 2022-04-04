78d8a863d13b82d1ee79a3bfbb652e7d1fb0ecf6
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-04-04 13:07:37 -0700

    Modified Compliance Editor page

--- ONPRC_EHR_ComplianceDB/src/org/labkey/ONPRCEHR_ComplianceDB/ONPRC_EHR_ComplianceDBModule.java
+++ ONPRC_EHR_ComplianceDB/src/org/labkey/ONPRCEHR_ComplianceDB/ONPRC_EHR_ComplianceDBModule.java
@@ -79,6 +79,9 @@ public class ONPRC_EHR_ComplianceDBModule extends ExtendedSimpleModule
         // Added: 7-6-2021 R. Blasa
         EHRService.get().registerClientDependency(ClientDependency.supplierFromPath("ehr_compliancedb/panel/EnterDataPanel.js"), this);
 
+//        Added: 4-4-2022  R.Blasa
+        EHRService.get().registerMoreActionsButton(new ShowEditUIButton(this, "ehr_compliancedb", "employees", ONPRC_ComplianceDBAdminPermission.class), "ehr_compliancedb", "employees");
+
 
     }
 
