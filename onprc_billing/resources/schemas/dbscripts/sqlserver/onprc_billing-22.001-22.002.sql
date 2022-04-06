cd087e28d0b9bc4f861c4a81d424e0d642f9f3b8
gary Jones <oregonjonesga@outlook.com>
2022-04-06 07:31:43 -0700

    Added field to OGA Synch for Originating Award

--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-22.001-22.002.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-22.001-22.002.sql
@@ -1,3 +1,5 @@
 EXEC core.fn_dropifexists 'aliases', 'onprc_billing', 'COLUMN', 'Originating Agency Award Number';
 GO
 ALTER TABLE onprc_billing.aliases ADD [OriginatingAgencyAwardNum] VarChar(255) Null;
+GO
+ALTER TABLE onprc_billing.ogaSynch ADD [OriginatingAgencyAwardNum] VarChar(255) Null;
--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-22.003-22.004.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-22.003-22.004.sql
@@ -17,7 +17,7 @@ INSERT INTO [onprc_billing].[aliases]
 ,[createdBy]
 ,[created]
 ,[category]
-,[faRate]
+,[faRate]bu
 ,[faSchedule]
 ,[budgetStartDate]
 ,[budgetEndDate]
@@ -80,7 +80,7 @@ SELECT
              ,[ADFM EMP NUM]
              ,[ADFM FULL NAME]
              ,[ORG]
-             ,[ORIGINATING_AGENCY_AWARD_NUM]
+             ,[OriginatingAgencyAwardNum]
         From [onprc_billing].[ogasynch] o
 		left outer join [onprc_ehr].investigators i on o.[PI EMP NUM] = i.employeeid and i.datedisabled is Null
 		left outer join onprc_billing.fiscalAuthorities f on f.employeeId = o.[PDFM EMP NUM] and f.active = 'true';
