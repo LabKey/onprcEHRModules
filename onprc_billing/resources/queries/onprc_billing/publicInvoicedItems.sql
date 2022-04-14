0870196d74787bf93ef01f88fdded34b0351e30d
gary Jones <oregonjonesga@outlook.com>
2022-04-14 07:05:47 -0700

    Update of SOurce to allow processing in 22.3 build

--- onprc_billing/resources/queries/onprc_billing/publicInvoicedItems.sql
+++ onprc_billing/resources/queries/onprc_billing/publicInvoicedItems.sql
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *Update 2022.04.14 change of source
  */
 SELECT
   i.rowId,
