e39406ad2378223354c170fd75a0036e22498284
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2022-04-21 10:47:40 -0700

    ehrsla template created.

--- sla/resources/queries/sla/PublicPurchaseDetails.sql
+++ sla/resources/queries/sla/PublicPurchaseDetails.sql
@@ -1,13 +1,28 @@
 SELECT pd.*
-FROM sla.purchase p, sla.purchasedetails pd
-WHERE p.objectid = pd.purchaseid
-AND (
-  (SELECT max(rowid) as expr FROM onprc_billing.dataAccess da
-    -- current logged in user is the dataAccess user
-    WHERE isMemberOf(da.userid)
-    -- has access to all data
-    AND (da.allData = true
-    -- has access to the specified investigatorId and the specified project (if applicable)
-      OR (da.investigatorId = p.project.investigatorId AND (da.project IS NULL OR da.project = p.project)))
-  ) IS NOT NULL
-)
+FROM sla.purchase p, sla.purchasedetails pd, publicehr.project pr, publicehr.protocol prc
+WHERE p.objectid = pd.purchaseid and p.project = pr.project
+  AND (
+    (SELECT max(rowid) as expr FROM financePublic.dataAccess da
+                                    -- current logged in user is the dataAccess user
+     WHERE isMemberOf(da.userid)
+       -- has access to all data
+       AND (da.allData = true
+         -- has access to the specified investigatorId and the specified project (if applicable)
+         OR (da.investigatorId = pr.investigatorId AND (da.project IS NULL OR da.project = p.project)))
+    ) IS NOT NULL
+    )
+
+
+-- SELECT pd.*
+-- FROM sla.purchase p, sla.purchasedetails pd
+-- WHERE p.objectid = pd.purchaseid
+-- AND (
+--   (SELECT max(rowid) as expr FROM financePublic.onprc_billing.dataAccess da
+--     -- current logged in user is the dataAccess user
+--     WHERE isMemberOf(da.userid)
+--     -- has access to all data
+--     AND (da.allData = true
+--     -- has access to the specified investigatorId and the specified project (if applicable)
+--       OR (da.investigatorId = p.project.investigatorId AND (da.project IS NULL OR da.project = p.project)))
+--   ) IS NOT NULL
+-- )
