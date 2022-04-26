2f83b9a560038132e5dc3ae692f7b47c85f0f40e
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-04-26 14:54:36 -0700

    Modified Procedure Overdue/Soon report was updated to include list of exemptions for an employeeid.

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceRecentTests.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceRecentTests.sql
@@ -19,6 +19,9 @@ select b.requirementname,
 
        (select max(zz.date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid  ) as MostRecentDate,
 
+       (Select group_concat(distinct yy.comment, chr(10))  from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid )
+                                                                                     And  yy.requirementname= b.requirementname and yy.employeeid= a.employeeid   ) as comment,
+
        CAST(
                CASE
 
@@ -52,6 +55,8 @@ select a.requirementname,
 
        (select max(zz.date) from ehr_compliancedb.completiondates zz where zz.requirementname= a.requirementname and zz.employeeid= a.employeeid  ) as MostRecentDate,
 
+       (Select group_concat(distinct yy.comment, chr(10))  from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= a.requirementname and zz.employeeid= a.employeeid )
+                                                                                     And  yy.requirementname= a.requirementname and yy.employeeid= a.employeeid   ) as comment,
 
        CAST(
 
@@ -65,14 +70,41 @@ select a.requirementname,
                    END  AS double)  AS MonthsUntilRenewal
 
 
-
-
 from  ehr_compliancedb.completiondates a
 where a.requirementname not in (select distinct h.requirementname from ehr_compliancedb.employeeperunit k, ehr_compliancedb.requirementspercategory h Where (k.unit = h.unit
     or k.category = h.category) And a.employeeid = k.employeeid )
-And a.requirementname not in (select distinct t.requirementname from ehr_compliancedb.employeerequirementexemptions t Where a.employeeid = t.employeeid
-                                         And a.requirementname = t.requirementname)
-
+  And a.requirementname not in (select distinct t.requirementname from ehr_compliancedb.employeerequirementexemptions t Where a.employeeid = t.employeeid
+                                                                                                                          And a.requirementname = t.requirementname)
 
 group by a.requirementname,a.employeeid
+
+
+union
+
+select j.requirementname,
+       j.employeeid,
+       null,
+       null,
+       'No' as trackingflag,
+       (select count(zz.date) from completiondates zz where zz.requirementname= j.requirementname and zz.employeeid= j.employeeid  ) as timesCompleted,
+       (select k.expireperiod from ehr_compliancedb.Requirements k where k.requirementname = j.requirementname) as ExpiredPeriod,
+       (select max(zz.date) from ehr_compliancedb.completiondates zz where zz.requirementname= j.requirementname and zz.employeeid= j.employeeid  ) as MostRecentDate,
+       (Select group_concat(distinct yy.comment, chr(10))  from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= j.requirementname and zz.employeeid= j.employeeid )
+                                                                                     And  yy.requirementname= j.requirementname and yy.employeeid= j.employeeid   ) as comment,
+       CAST(
+
+               CASE
+
+                   WHEN (select max(st.date) from completiondates st where st.requirementname = j.requirementname and st.employeeid = j.employeeid ) IS NULL   then 0
+                   WHEN ( select  (tt.expireperiod)  from  ehr_compliancedb.requirements tt, ehr_compliancedb.completiondates pq where tt.requirementname = j.requirementname and pq.requirementname = tt.requirementname and pq.employeeid = j.employeeid group by tt.expireperiod  ) = 0 then Null
+
+
+                   ELSE ( select  (tt.expireperiod) - ( age_in_months(max(pq.date), curdate())) from  ehr_compliancedb.requirements tt, ehr_compliancedb.completiondates pq where tt.requirementname = j.requirementname and pq.requirementname = tt.requirementname and pq.employeeid = j.employeeid group by tt.expireperiod  )
+                   END  AS double)  AS MonthsUntilRenewal
+
+
+from  ehr_compliancedb.employeerequirementexemptions j
+
+
+group by j.requirementname,j.employeeid
 
