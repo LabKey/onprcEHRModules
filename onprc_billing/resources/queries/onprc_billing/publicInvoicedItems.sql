993a4226f497721a4c0e3606a971e8642b41de68
gary Jones <oregonjonesga@outlook.com>
2022-05-03 16:57:46 -0700

    Update to Public Finance to pushg to 22.3 Merge

--- onprc_billing/resources/queries/onprc_billing/publicInvoicedItems.sql
+++ onprc_billing/resources/queries/onprc_billing/publicInvoicedItems.sql
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *Update 2022.05.03 change of source
+ *Update 2022.05.03 change of source update
  */
 SELECT
     i.rowId,
