f7dc61ca2bc01a9fd514faf1f28984b708d10476
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2022-05-05 16:04:05 -0700

    Updated the container path

--- onprc_billing/resources/queries/onprc_billing/ARTCoreBIllingCharges.sql
+++ onprc_billing/resources/queries/onprc_billing/ARTCoreBIllingCharges.sql
@@ -1,6 +1,6 @@
 -- Query to get ART core chargeable items
 SELECT rowid, category, activerate.unitCost, itemCode + ' - ' + name + '; Unit Cost - ' + CAST(activerate.unitCost as varchar) as chargeName
-FROM Site.{substitutePath moduleProperty('ONPRC_Billing','BillingContainer_Public')}.onprc_billing_public.chargeableItems
+FROM Site.{substitutePath moduleProperty('ONPRC_Billing','BillingContainer_Public')}.onprc_billing_public.chargeableItems  -- Changed by kolli, 5/5/22
 -- FROM "/onprc/admin/finance/public".onprc_billing_public.chargeableItems
 WHERE category = 'ART Core'
 AND Active = 'true'
