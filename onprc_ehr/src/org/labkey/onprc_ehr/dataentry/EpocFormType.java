a0d433e2981b148ad2bcf56ffcfdea97b7142fe2
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-05-11 16:37:24 -0700

    Created an EPOC input form, and all the supporting dependency files.

--- onprc_ehr/src/org/labkey/onprc_ehr/dataentry/EpocFormType.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/dataentry/EpocFormType.java
@@ -31,7 +31,7 @@ import java.util.Arrays;
  */
 public class EpocFormType extends TaskForm
 {
-    public static final String NAME = "iStat";
+    public static final String NAME = "Epoc";
 
     public EpocFormType(DataEntryFormContext ctx, Module owner)
     {
@@ -44,7 +44,7 @@ public class EpocFormType extends TaskForm
         for (FormSection s : getFormSections())
         {
             s.addConfigSource("Labwork");
-            s.addConfigSource("iStat");
+            s.addConfigSource("Epoc");
         }
 
         addClientDependency(ClientDependency.supplierFromPath("onprc_ehr/model/sources/Epoc.js"));
