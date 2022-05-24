ef42d9fd3626eb43c09c7d35814eacbe29bfce54
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2022-05-24 14:33:14 -0700

    Fixed the query with correct linked schema, take #2

--- sla/resources/queries/sla/allowableSLA.sql
+++ sla/resources/queries/sla/allowableSLA.sql
@@ -1,5 +1,6 @@
 --Changed by Kolli - 4/20/22
 --Added Linked schema template references
+--Removed linked schema 5/24/2022
 SELECT a.protocol,
        c.protocol_id as eIACUC_protocol_name,
        a.species,
@@ -12,8 +13,8 @@ SELECT a.protocol,
 FROM sla.allowableAnimals a,
     onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS c
 Where c.Protocol_State = 'Approved'
---   And (c.protocol_Id = a.protocol.displayname OR c.protocol_Id like '%' + a.protocol.displayname)
-  And (c.protocol_Id = a.protocol OR c.protocol_Id like '%' + a.protocol)
+  And (c.protocol_Id = a.protocol.displayname OR c.protocol_Id like '%' + a.protocol.displayname)
+ -- And (c.protocol_Id = a.protocol OR c.protocol_Id like '%' + a.protocol)
   And curdate() between a.startdate And a.endDate
 
 -- --Created ny kollil on 10/18/21 to dispaly the allowable SLAs with eIACUCNum but no breeding info.
