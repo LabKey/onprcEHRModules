bec23f866f6b82362b8e4fe46e96d10a1855b245
ohsudev <blasar@ohsu.edu>
2022-06-07 15:16:05 -0700

    Modified Epoc input form to use Epoc panel test listings.

---
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.000-23.001.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [ehr_lookups].[Epoc_tests]
+(
+    rowid  INT IDENTITY(1,1)NOT NULL,
+    testid nvarchar(500) NOT NULL,
+    name nvarchar(500) NULL,
+    units nvarchar(50) NULL,
+    alias nvarchar(200) NULL,
+    alertOnAbnormal  [bit] NULL,
+    alertOnAny [bit] NULL,
+    includeInPanel [bit]NULL,
+    sort_order [int] NULL,
+    container ENTITYID
+
+    CONSTRAINT PK_testidclass PRIMARY KEY (testid)
+
+    )
+    GO
