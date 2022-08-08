03f1d45b0218533e95e396bbc4f64e28346906d0
kollil <kollil@ohsu.edu>
2022-08-08 12:56:44 -0700

    Added the alert msg

--- onprc_ehr/resources/queries/study/TattooAlert.sql
+++ onprc_ehr/resources/queries/study/TattooAlert.sql
@@ -1,3 +1,4 @@
+-- Displays animals with no tattoo proc recorded
 Select Id, birth, gender from study.demographics a
 Where a.calculated_status = 'Alive'
   And a.id not in (Select id from study.arrival where acquisitiontype = 6) --6= Purchase
