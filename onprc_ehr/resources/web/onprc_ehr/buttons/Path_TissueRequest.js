4024895786b1894b1ee7da76fb766a93fce798c1
ohsudev <blasar@ohsu.edu>
2022-08-10 22:38:58 -0700

    Modified test scripts that was causing errors.

--- onprc_ehr/resources/web/onprc_ehr/buttons/Path_TissueRequest.js
+++ onprc_ehr/resources/web/onprc_ehr/buttons/Path_TissueRequest.js
@@ -1,20 +1,21 @@
 
 //Created: 7-18-2022  R.Blasa
 
-EHR.DataEntryUtils.registerDataEntryFormButton('PATH_REQUEST', {
-    text: 'Request',
-    name: 'request',
-    targetQC: 'Request: Pending',
-    requiredQC: 'Request: Pending',
-    errorThreshold: 'WARN',
-    successURL: LABKEY.ActionURL.getParameter('srcURL') || LABKEY.ActionURL.getParameter('returnUrl') || LABKEY.ActionURL.getParameter('returnURL') || LABKEY.ActionURL.buildURL('ehr', 'enterData.view'),
-    disabled: true,
-    itemId: 'requestBtn',
-    handler: function (btn) {
-        var panel = btn.up('ehr-dataentrypanel');
-        panel.onSubmit(btn);
+EHR.DataEntryUtils.registerDataEntryFormButton('PATH_SAVEDRAFT', {
+
+    SAVEDRAFT: {
+        text: 'Save Draft',
+        name: 'saveDraft',
+        requiredQC: 'Request: Pending',
+        errorThreshold: 'WARN',
+        disabled: true,
+        itemId: 'saveDraftBtn',
+        handler: function(btn){
+            var panel = btn.up('ehr-dataentrypanel');
+            panel.onSubmit(btn, false, true);
+        },
+        disableOn: 'ERROR'
     },
-    disableOn: 'WARN'
 
 });
 
