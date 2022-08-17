8a0ce2e4d2185f67daece407fbb9e4fb31e48791
Gary <jonesga@ohsu.edu>
2022-08-17 12:18:47 -0700

    Update of eIACUC Process to idenitfy external_id as route Protocol

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.004-23.005.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.004-23.005.sql
@@ -12,15 +12,16 @@ GO
   -- Create date: 2022/06/20
   -- Description:	Process to remove records created in eIACUC from protocol dataset
   -- =============================================
-CREATE PROCEDURE [onprc_ehr].[eIACUC_removeRecords]
+ALTER PROCEDURE [onprc_ehr].[eIACUC_removeRecords]
 
   AS
   BEGIN
-  	-- SET NOCOUNT ON added to prevent extra result sets from
-  	-- interfering with SELECT statements.
 
 
-      -- Insert statements for procedure here
+
+
+
   	Delete from ehr.protocol
-  	where created >= DateAdd(day,-3,GetDate())
+  	where project_type like '%Source'
   END
+
