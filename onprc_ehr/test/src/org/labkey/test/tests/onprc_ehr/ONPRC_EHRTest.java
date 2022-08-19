168d294979e57d0c91f23a728aa67ec021610cc4
ohsudev <blasar@ohsu.edu>
2022-08-19 11:28:59 -0700

    Modified test scripts that was causing errors.

--- onprc_ehr/test/src/org/labkey/test/tests/onprc_ehr/ONPRC_EHRTest.java
+++ onprc_ehr/test/src/org/labkey/test/tests/onprc_ehr/ONPRC_EHRTest.java
@@ -1830,9 +1830,11 @@ public class ONPRC_EHRTest extends AbstractGenericONPRC_EHRTest
         click(Locator.tagWithClassContaining("div","x4-trigger-index-1"));
         _ext4Helper.selectComboBoxItem("Center Project:",Ext4Helper.TextMatchTechnique.CONTAINS,"Other");
         _ext4Helper.selectComboBoxItem("Project:",Ext4Helper.TextMatchTechnique.CONTAINS,projectId);
+        waitAndClick(Ext4Helper.Locators.ext4Button("Submit"));
         click(Locator.tagWithClassContaining("div","x4-trigger-index-1"));
         _ext4Helper.selectComboBoxItem("Center Project Billing:",Ext4Helper.TextMatchTechnique.CONTAINS,"Other");
         _ext4Helper.selectComboBoxItem("Project:",Ext4Helper.TextMatchTechnique.CONTAINS,projectId);
+        waitAndClick(Ext4Helper.Locators.ext4Button("Submit"));
         setNecropsyFormElement("fastingtype", "N/A");
         setNecropsyFormElement("animaldelivery", "Deliver from Surgery");
         setNecropsyFormElement("remainingTissues", "Yes");
