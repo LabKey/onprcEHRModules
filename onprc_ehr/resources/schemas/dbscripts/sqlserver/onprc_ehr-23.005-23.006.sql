20ec62121b13c448155da66ddb9e4ce7b3cb48fb
Gary <jonesga@ohsu.edu>
2022-08-23 09:25:39 -0700

    Modification of Protocol Log fields to march protocol with extensible fields

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.005-23.006.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.005-23.006.sql
@@ -1,7 +1,7 @@
 --added to allow department descignation for R & L
-EXEC core.fn_dropifexists 'protocol_log', 'onprc_ehr', 'COLUMN', 'Protocol_State';
+EXEC core.fn_dropifexists 'protocol_logs', 'onprc_ehr', 'COLUMN', 'Protocol_State';
 GO
 
-ALTER TABLE onprc_ehr.protocol_log ADD [Protocol_State] varchar(250) Null;
-ALTER TABLE onprc_ehr.protocol_log ADD [PPQ_Numbers] varchar(250) Null;
-ALTER TABLE onprc_ehr.protocol_log ADD [Template_OID] varchar(250) Null;
+ALTER TABLE onprc_ehr.protocol_logs ADD [Protocol_State] varchar(250) Null;
+ALTER TABLE onprc_ehr.protocol_logs ADD [PPQ_Numbers] varchar(250) Null;
+ALTER TABLE onprc_ehr.protocol_logs ADD [Template_OID] varchar(250) Null;
