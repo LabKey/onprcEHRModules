7be0c54d6c244e8336ad3380b0b1abe449bdd698
Gary <jonesga@ohsu.edu>
2022-08-23 08:52:34 -0700

    Modification of Protocol Log fields to march protocol with extensible fields

--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -121,7 +121,7 @@ public class ONPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 23.005;
+        return 23.006;
     }
 
     @Override
