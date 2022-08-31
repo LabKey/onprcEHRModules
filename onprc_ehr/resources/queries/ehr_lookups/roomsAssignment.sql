e9ad864fdcd93bf62314234b68942a52370e9766
Gary <jonesga@ohsu.edu>
2022-08-31 13:12:56 -0700

    Update to protocol details

--- onprc_ehr/resources/queries/ehr_lookups/roomsAssignment.sql
+++ onprc_ehr/resources/queries/ehr_lookups/roomsAssignment.sql
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 2022-08-23 Changed source of assignment data to linked schema working to get in correct location
  */
 select
 
@@ -24,20 +25,19 @@ select
 
   sum(a.totalAnimals) as totalAssignments
 
-FROM (
-
-select
-  a.id.curLocation.room as room,
-  a.project.name as name,
-  a.project.investigatorId.lastname as investigator,
-  cast(a.project.name || ' (' || a.project.investigatorId.lastname || '): ' || cast(count(distinct a.Id) as varchar) as varchar) as projectIdTotal,
-  cast(a.project.name || ' (' || a.project.investigatorId.lastname || '): ' || cast(count(distinct a.id.curLocation.cage) as varchar) as varchar) as projectCageTotal,
+FROM (select
+    h.room as room,
+    p.name as name,
+    i.lastname as investigator,
+  cast(p.name || ' (' || i.lastname || '): ' || cast(count(distinct a.Id) as varchar) as varchar) as projectIdTotal,
+  cast(p.name || ' (' || i.lastname || '): ' || cast(count(distinct h.cage) as varchar) as varchar) as projectCageTotal,
   count(distinct a.id) as totalAnimals
-
-from study.assignment a
-where a.isActive = true and a.id.curLocation.room IS NOT NULL
-group by a.id.curLocation.room, a.project.name, a.project.investigatorId.lastname
-
+from finance_study.assignment a
+	left outer join finance_study.housing h on a.id = h.id and (h.enddate is null or h.enddate > Now())
+	left outer join pf_publicEHR.project p on a.project = p.project
+	left outer join pf_onprcEHRPublic.investigators i on i.rowid = p.investigatorId
+where a.isActive = true
+group by h.room, p.name, i.lastname
 ) a
 
 group by a.room
