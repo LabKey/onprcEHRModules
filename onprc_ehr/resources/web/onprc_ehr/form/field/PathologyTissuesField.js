b8ecc9cf7d69e3fbf4c568943c18dee2625b1793
ohsudev <blasar@ohsu.edu>
2022-09-20 12:43:09 -0700

    Modified MiscCharges selection listings to display all active pathology categories.

--- onprc_ehr/resources/web/onprc_ehr/form/field/PathologyTissuesField.js
+++ onprc_ehr/resources/web/onprc_ehr/form/field/PathologyTissuesField.js
@@ -256,8 +256,8 @@ Ext4.define('onprc_ehr.form.field.Path_BillingGrade', {
                 sort: 'name',
                 filterArray: [
                     LABKEY.Filter.create('category', 'Pathology', LABKEY.Filter.EQUAL),
-                    LABKEY.Filter.create('active', true, LABKEY.Filter.Types.EQUAL),
-                    LABKEY.Filter.create('rowid', '4484;4485;4486;4487;4488;4489;5296;5297;5298;4491;4492', LABKEY.Filter.Types.EQUALS_ONE_OF)],
+                    LABKEY.Filter.create('active', true, LABKEY.Filter.Types.EQUAL)],
+
                 autoLoad: true
             })
         });
