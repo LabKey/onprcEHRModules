ef17ea74faea16f5881d25a745ee3598baf07350
Gary <jonesga@ohsu.edu>
2022-10-04 10:53:29 -0700

    Updating related Items for

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-22.002-23.003.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-22.002-23.003.sql
@@ -1,5 +1,6 @@
 -- This stored procedure was incorrectly named and placed in the wrong schema when created in onprc_ehr-18.10-20.101.sql.
 -- It's also unused, so just drop it.
+--2203-10-04 Update for Latest processing of eIACUC
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND object_id = OBJECT_ID('[onprc_ehr].[eIACUC_removeRecords]'))
     DROP PROCEDURE [onprc_ehr].[eIACUC_removeRecords]
 SET ANSI_NULLS ON
