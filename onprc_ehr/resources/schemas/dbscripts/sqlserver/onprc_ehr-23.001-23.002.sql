a1cdbfe01ea508c08ba56121116a86819d3e667c
Gary <jonesga@ohsu.edu>
2022-10-05 10:54:28 -0700

    Update to add files to protocol_log

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.001-23.002.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.001-23.002.sql
@@ -40,3 +40,9 @@ CREATE TABLE [onprc_ehr].[Protocol_logs](
  ALTER TABLE [onprc_ehr].[protocol_logs] ADD  DEFAULT (newid()) FOR [objectid]
  GO
 
+EXEC core.fn_dropifexists 'protocol_logs', 'onprc_ehr', 'COLUMN', 'Protocol_State';
+GO
+
+ALTER TABLE onprc_ehr.protocol_logs ADD [Protocol_State] varchar(250) Null;
+ALTER TABLE onprc_ehr.protocol_logs ADD [PPQ_Numbers] varchar(250) Null;
+ALTER TABLE onprc_ehr.protocol_logs ADD [Template_OID] varchar(250) Null;
