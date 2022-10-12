fc3ece2c7442f794be1f49fb4434e04979ec462d
Gary <jonesga@ohsu.edu>
2022-10-12 11:43:22 -0700

    Update of MHC and Randal UPdate Queries

--- onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
+++ onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
@@ -1,13 +1,10 @@
 
-SELECT d.rowid,
-       d.subjectId,
-       d.datatype,
-       d.marker,
-       d.result,
-       d.score,
-       d.assaytype,
-       d.container,
-       d.totalTests
+SELECT d.Id,
+d.allele,
+d.shortName,
+d.totalTests,
+d.result,
+d.type
 
-FROM  StudyDetails_RandalData s, Site.{substitutePath moduleProperty('EHR','EHRStudyContainer')}.MHC_Data.MHC_Data_Unified d
+FROM  StudyDetails_RandalData s, MHCData.MHC_Data_Unified d
 where (Cast(s.rh as varchar(25)) = Cast(d.id as varchar(25)))
