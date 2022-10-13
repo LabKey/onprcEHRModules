5c3a9a416f372d7dcdc8f49531f6b77867721b87
ohsudev <blasar@ohsu.edu>
2022-10-12 17:06:28 -0700

    Modified program to provide a shorter list of Pathology charge name listings.

--- onprc_ehr/resources/web/onprc_ehr/form/field/PathologyTissuesField.js
+++ onprc_ehr/resources/web/onprc_ehr/form/field/PathologyTissuesField.js
@@ -246,17 +246,17 @@ Ext4.define('onprc_ehr.form.field.Path_BillingGrade', {
 
     initComponent: function(){
         Ext4.apply(this, {
-            displayField:'displayName',
-            valueField: 'idkey',
+            displayField:'name',
+            valueField: 'rowid',
             queryMode: 'local',
             store: Ext4.create('LABKEY.ext4.data.Store', {
-                schemaName: 'onprc_ehr',
-                queryName: 'Reference_Data_IDkey',
-                columns: 'idkey,columnName, displayName',
+                schemaName: 'onprc_billing_public',
+                queryName: 'chargeableItems',
+                columns: 'rowid,name',
                 sort: 'name',
                 filterArray: [
-                    LABKEY.Filter.create('columnName', 'Pathology_billing', LABKEY.Filter.EQUAL),
-                    LABKEY.Filter.create('endDate', null, LABKEY.Filter.Types.ISBLANK)],
+                    LABKEY.Filter.create('category', 'Pathology', LABKEY.Filter.EQUAL),
+                    LABKEY.Filter.create('active', true, LABKEY.Filter.Types.EQUAL)],
 
                 autoLoad: true
             })
