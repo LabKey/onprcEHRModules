23d01bf655bbd7ffcbb01144ff9fdd0b2bcc574f
Gary <jonesga@ohsu.edu>
2022-10-25 08:12:37 -0700

    Fixed linked schema name to match dname expected in test scripts

--- onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
+++ onprc_ehr/resources/queries/onprc_ehr/RandalData_MHCData.sql
@@ -6,5 +6,5 @@ d.totalTests,
 d.score
 
 
-FROM  StudyDetails_RandalData s, MHCData.mhc_data d
+FROM  StudyDetails_RandalData s, MHC_Data.mhc_data d
 where (Cast(s.rh as varchar(25)) = Cast(d.subjectid as varchar(25)))
