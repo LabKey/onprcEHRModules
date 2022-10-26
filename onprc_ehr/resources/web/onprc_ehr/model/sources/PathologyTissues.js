f411ddb860f6a6023c20b4e7eb1aeae3a5cb81a9
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-10-26 12:01:52 -0700

    Modified to display "Request Category" (#532)
    * Modified to display "Request Category"
    
    * Modified to allow "Request Category" column to be displayed.
    
    * Modified to allow "Request Category" column to be displayed.

--- onprc_ehr/resources/web/onprc_ehr/model/sources/PathologyTissues.js
+++ onprc_ehr/resources/web/onprc_ehr/model/sources/PathologyTissues.js
@@ -8,33 +8,164 @@ EHR.model.DataModelManager.registerMetadata('PathTissues', {
     allQueries: {
     },
     byQuery: {
+
         'study.encounters': {
             type: {
                 defaultValue: 'Tissues',
                 hidden: true
             },
+            chargetype: {
+                defaultValue: 'DCM: Pathology Services',
+                hidden: true
+            },
+            date: {
+                xtype: 'xdatetime',
+                header: 'Start Date',
+                extFormat: LABKEY.extDefaultDateTimeFormat,
+                editorConfig: {
+                    dateFormat: LABKEY.extDefaultDateFormat,
+                    timeFormat: 'H:i'
+                }
+            },
+            instructions: {
+                hidden: false,
+                height: 50,
+                label: 'Preferred Necropsy date range',
+                columnConfig: {
+                    width: 70
+                }
+            },
             enddate: {
                 hidden: true
             },
             project: {
-                header: 'Center Project'
+                xtype: 'onprc_ehr-projectentryfield',
+                label: 'Center Project'
+            },
+            billingproject: {
+                xtype: 'onprc_ehr-projectentryfield',
+                label: 'Center Project Billing',
+                hidden: false
+
             },
 
-
-            chargetype: {
+            performedby: {
                 hidden: true
             },
             procedureid: {
                 defaultValue: 1082,
                 hidden: true
             },
-
             caseno: {
                 hidden: true
             },
+            fastingtype: {
+                hidden: false,
+                label: 'What type of fast would you like to request?',
+                xtype: 'path_Fasting',
+                columnConfig: {
+                    width: 150
+                }
+            },
+
+            animaldelivery: {
+                hidden: false,
+                xtype: 'path_delivery',
+                label: 'Animal Delivery Requested',
+                columnConfig: {
+                    width: 150
+                }
+            },
+            necropsygrade: {
+                hidden: false,
+                xtype: 'path_billinggrade',
+                columnConfig: {
+                    width: 150
+                }
+            },
+            remainingTissues: {
+                hidden: false,
+                xtype: 'path_approval',
+                label: 'Are Remaining Tissues available for distribution?',
+                columnConfig: {
+                    width: 150
+                }
+            },
+            necropsylocation: {
+                hidden: false,
+                xtype: 'path_location',
+                label: 'Necropsy Location',
+                columnConfig: {
+                    width: 150
+                }
+            },
+
             remark: {
                 hidden: true
+
             }
+        },
+
+        'onprc_billing.miscCharges': {
+            Id: {
+                allowBlank: true,
+                nullable: true
+            },
+            chargetype: {
+                defaultValue: 'DCM: Pathology Services'
+            },
+            chargeId: {
+                hidden: false,
+                xtype: 'path_billinggrade',
+                label: 'Charge Name',
+                columnConfig: {
+                    width: 250
+                }
+            },
+        },
+        'study.tissueDistributions': {
+
+            date: {
+                hidden: false
+            },
+
+            project: {
+                xtype: 'onprc_ehr-projectentryfield',
+                label: 'Center Project'
+
+            },
+
+            requestcategory: {
+                hidden: false
+            },
+            recipient: {
+                columnConfig: {
+                    width: 150
+                }
+            },
+            sampletype: {
+                columnConfig: {
+                    width: 200
+                }
+            },
+            performedby: {
+                hidden: true
+            },
+            tissue: {
+                columnConfig: {
+                    width: 250
+                }
+            },
+
+            remark: {
+                header: 'Comments',
+                hidden: false,
+                columnConfig: {
+                    width: 250
+                }
+            }
+
         }
+
     }
 });
