d1a3a9eac304e085e91322e20fc16cb52f5f56d6
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-12-05 09:53:18 -0800

    modified program to account for employee trainings that was assigned, but not completed.

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceRecentTests.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceRecentTests.sql
@@ -5,7 +5,7 @@
  */
 
 -- Modified: 6-14-2022   R.Blasa     ComplianceRecentTests.sql
-
+-- Training that was completed by an employee and being recorded as  completed as a Unit or Category
 select b.requirementname,
        a.employeeid,
        group_concat(distinct b.unit,chr(10)) as unit,
@@ -45,13 +45,14 @@ from employeeperunit a ,requirementspercategory b
 where ( a.unit = b.unit or a.category = b.category )
   And b.requirementname not in (select distinct t.requirementname from ehr_compliancedb.employeerequirementexemptions t Where a.employeeid = t.employeeid
                                                                                                                           And b.requirementname = t.requirementname)
+  And a.employeeid in (select p.employeeid from ehr_compliancedb.employees p where p.enddate is null)
 
 
 group by b.requirementname,a.employeeid
 
 union
 
-
+-- Training that was completed by an employee but not recorded as being completed as a Unit or Category
 select a.requirementname,
        a.employeeid,
        null  as unit,
@@ -95,12 +96,14 @@ where a.requirementname not in (select distinct h.requirementname from ehr_compl
     or k.category = h.category) And a.employeeid = k.employeeid )
   And a.requirementname not in (select distinct t.requirementname from ehr_compliancedb.employeerequirementexemptions t Where a.employeeid = t.employeeid
                                                                                                                           And a.requirementname = t.requirementname)
+  And a.employeeid in (select p.employeeid from ehr_compliancedb.employees p where p.enddate is null)
 
 group by a.requirementname,a.employeeid
 
 
 union
 
+-- Training that was completed by as an employee training exemptions, and at least completed one, or more times
 select j.requirementname,
        j.employeeid,
        null as unit,
@@ -117,17 +120,19 @@ select j.requirementname,
 from  ehr_compliancedb.employeerequirementexemptions j
     Where j.requirementname in (select z.requirementname from ehr_compliancedb.completiondates z where z.requirementname = j.requirementname
                                             and z.employeeid = j.employeeid and z.date is not null)
+      And j.employeeid in (select p.employeeid from ehr_compliancedb.employees p where p.enddate is null)
 
 
 group by j.requirementname,j.employeeid
 
 UNION
 
+-- Additional requirements for employees that have not completed training, but is required
 select j.requirementname,
        j.employeeid,
        null as unit,
        null as category,
-       'No' as trackingflag,
+       'Yes' as trackingflag,
        null as timesCompleted,
        null as ExpiredPeriod,
        null as MostRecentDate,
@@ -138,6 +143,7 @@ select j.requirementname,
 from  ehr_compliancedb.RequirementsPerEmployee j
 Where j.requirementname NOT in (select z.requirementname from ehr_compliancedb.completiondates z where z.requirementname = j.requirementname
                                                                                                and z.employeeid = j.employeeid and z.date is not null)
+  And j.employeeid in (select p.employeeid from ehr_compliancedb.employees p where p.enddate is null)
 
 
 group by j.requirementname,j.employeeid
