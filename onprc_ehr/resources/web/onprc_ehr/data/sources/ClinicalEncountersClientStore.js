a8f4c9e6f7edba98fa0e7410855429cb293b4b28
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-01-17 19:09:51 -0800

    Modified program to add delay time sequence.

--- onprc_ehr/resources/web/onprc_ehr/data/sources/ClinicalEncountersClientStore.js
+++ onprc_ehr/resources/web/onprc_ehr/data/sources/ClinicalEncountersClientStore.js
@@ -101,13 +101,12 @@ Ext4.define('ONPRC_EHR.data.ClinicalEncountersClientStore', {
         var procedureStore = EHR.DataEntryUtils.getProceduresStore();
         LDK.Assert.assertNotEmpty('Unable to find procedureStore from ClinicalEncountersClientStore', procedureStore);
         setTimeout(function() {
-            var procRecIdx = procedureStore.findExact('rowid', procedureId);
-            LDK.Assert.assertTrue('Unable to find procedure record in ClinicalEncountersClientStore for procedureId: [' + procedureId + ']', procRecIdx > -1);
-            var procedureRec = procedureStore.getAt(procRecIdx);
-            LDK.Assert.assertNotEmpty('Unable to find procedure record from ClinicalEncountersClientStore.  ProcedureId was: [' + procedureId + ']', procedureRec);
-            return procedureRec;
         }, 3000);
 
-
+        var procRecIdx = procedureStore.findExact('rowid', procedureId);
+        LDK.Assert.assertTrue('Unable to find procedure record in ClinicalEncountersClientStore for procedureId: [' + procedureId + ']', procRecIdx > -1);
+        var procedureRec = procedureStore.getAt(procRecIdx);
+        LDK.Assert.assertNotEmpty('Unable to find procedure record from ClinicalEncountersClientStore.  ProcedureId was: [' + procedureId + ']', procedureRec);
+        return procedureRec;
     }
 });
