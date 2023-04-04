3150eca59c129c639214d70febd8d2dc5e8b9c16
kollil <kollil@ohsu.edu>
2023-04-04 12:50:25 -0700

    updated the query.

--- onprc_ehr/resources/queries/study/pregnancyChecks.sql
+++ onprc_ehr/resources/queries/study/pregnancyChecks.sql
@@ -27,30 +27,33 @@ Active notes
 Pregnancies-active: observations/tests
 Days since last group assignment
  */
-SELECT
-    d.id,
-    d.species,
-    d.gender,
-    d.birth,
-    d.project,
-    d.Id.MostRecentWeight,
-    d.Id.curlocation.area,
-    d.Id.curlocation.room,
-    d.Id.curlocation.cage,
-    d.mostRecentHx,
-    d.mostRecentClinicalObservations,
-    p.estDeliveryDate
-    ,(Select TIMESTAMPDIFF('SQL_TSI_DAY', endDate, now()) From pregnancyChecks_LastGroupAssignment Where Id = d.Id ) as DaysSinceLastGroupAssignment
-    --(Select count(*) from assignment Where Now() > date And enddate IS NULL and Id = d.Id) as num_projects
-FROM study.demographics d, study.pregnancyConfirmation p
-WHERE d.calculated_status = 'Alive'
-  AND TIMESTAMPDIFF('SQL_TSI_MONTH', d.birth, now()) >= 29 --greater than 2.5years
-  AND d.id = p.id
-  AND d.gender = 'F'
-  AND p.estDeliveryDate IS NULL
-  AND d.Id.curlocation.area NOT IN ('Corral', 'Shelters', 'ASB ESPF', 'Catch Area', 'Harem')
-  AND d.Id.curlocation.cage IS NOT NULL
-  AND d.Id NOT IN (Select Id From assignment Where Now() > date And enddate IS NULL) --# Research assignments = 0
-  AND d.Id NOT IN (Select Id From ehr.snomed_tags Where code like 'F-30980') -- Exclude animals with snomed 'non pregnanct state'
--- exclude those with a procedure: ultrasound or procedure: ultrasound-uterus or ultrasound - Pregnant in the record in the last 30 days?
-  AND d.Id NOT IN (Select Id From study.encounters Where procedureId in (799, 763, 873) And TIMESTAMPDIFF('SQL_TSI_DAY', date, now()) <= 30 )
+-- SELECT
+--     d.id,
+--     d.species,
+--     d.gender,
+--     d.birth,
+--     d.project,
+--     d.Id.MostRecentWeight,
+--     d.Id.curlocation.area,
+--     d.Id.curlocation.room,
+--     d.Id.curlocation.cage,
+--     --d.mostRecentHx,
+--     (Select ActiveNotes From demographicsActiveNotes Where Id = d.Id ) as ActiveNotes,
+--     d.mostRecentClinicalObservations,
+--     p.estDeliveryDate
+--     ,(Select TIMESTAMPDIFF('SQL_TSI_DAY', endDate, now()) From pregnancyChecks_LastGroupAssignment Where Id = d.Id ) as DaysSinceLastGroupAssignment
+--     --(Select count(*) from assignment Where Now() > date And enddate IS NULL and Id = d.Id) as num_projects
+-- FROM study.demographics d, study.pregnancyConfirmation p
+-- WHERE d.calculated_status = 'Alive'
+--   AND TIMESTAMPDIFF('SQL_TSI_MONTH', d.birth, now()) >= 29 --greater than 2.5years
+--   AND d.id = p.id
+--   AND d.gender = 'F'
+--   AND p.estDeliveryDate IS NULL
+--   AND d.Id.curlocation.area NOT IN ('Corral', 'Shelters', 'ASB ESPF', 'Catch Area', 'Harem')
+--   AND d.Id.curlocation.cage IS NOT NULL
+--   AND d.Id NOT IN (Select Id From assignment Where Now() > date And enddate IS NULL) --# Research assignments = 0
+--   AND d.Id NOT IN (Select Id From ehr.snomed_tags Where code like 'F-30980') -- Exclude animals with snomed 'non pregnanct state'
+-- -- exclude those with a procedure: ultrasound or procedure: ultrasound-uterus or ultrasound - Pregnant in the record in the last 30 days?
+--   AND d.Id NOT IN (Select Id From study.encounters Where procedureId in (799, 763, 873) And TIMESTAMPDIFF('SQL_TSI_DAY', date, now()) <= 30 )
+--
+Select * from demographics_FemalePregnancyChecks
