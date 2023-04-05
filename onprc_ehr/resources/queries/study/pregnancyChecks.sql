deec87ccf31f99b211c1a6ad4f0dc883a55006d7
kollil <kollil@ohsu.edu>
2023-04-05 09:36:39 -0700

    updated the query. The results look good.

--- onprc_ehr/resources/queries/study/pregnancyChecks.sql
+++ onprc_ehr/resources/queries/study/pregnancyChecks.sql
@@ -56,4 +56,33 @@ Days since last group assignment
 -- -- exclude those with a procedure: ultrasound or procedure: ultrasound-uterus or ultrasound - Pregnant in the record in the last 30 days?
 --   AND d.Id NOT IN (Select Id From study.encounters Where procedureId in (799, 763, 873) And TIMESTAMPDIFF('SQL_TSI_DAY', date, now()) <= 30 )
 --
-Select * from demographics_FemalePregnancyChecks
+SELECT
+    d.id,
+    d.species,
+    d.gender,
+    d.geographic_origin,
+    d.Id.age.ageInYears,
+    d.Id.currentCondition.condition,
+    d.Id.spfStatus.status,
+    d.Id.activeAssignments.projectAndInvestigator,
+    d.Id.activeAssignments.numResearchAssignments,
+    d.Id.MostRecentWeight.DaysSinceWeight,
+    d.Id.MostRecentWeight,
+    d.Id.curlocation.area,
+    d.Id.curlocation.room,
+    d.Id.curlocation.cage,
+    d.Id.activeNoteList.Notes,
+    d.Id.activepregnancies.estDeliveryDate,
+    d.Id.historicAnimalGroups.daysSinceLastAssignment
+FROM study.demographics d
+WHERE d.calculated_status = 'Alive'
+  AND d.Id.age.ageInYears >= 2.5 --greater than 2.5years
+  AND d.gender = 'F'
+  AND d.Id.activepregnancies.estDeliveryDate IS NULL
+  AND d.Id.curlocation.area NOT IN ('Corral', 'Shelters', 'ASB ESPF', 'Catch Area', 'Harem')
+  AND d.Id.curlocation.cage IS NOT NULL
+  AND d.Id.historicAnimalGroups.daysSinceLastAssignment between 20 and 176
+  AND d.Id.activeAssignments.numResearchAssignments = 0 --# Research assignments = 0
+  --AND d.Id NOT IN (Select Id From ehr.snomed_tags Where code like 'F-30980') -- Exclude animals with snomed 'non pregnanct state'
+-- exclude those with a procedure: ultrasound or procedure: ultrasound-uterus or ultrasound - Pregnant in the record in the last 30 days?
+  AND d.Id NOT IN (Select Id From study.encounters Where procedureId in (799, 763, 873) And TIMESTAMPDIFF('SQL_TSI_DAY', date, now()) <= 30 )
