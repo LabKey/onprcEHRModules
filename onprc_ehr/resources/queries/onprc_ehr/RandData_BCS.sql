e260064907574186f0ffa89b203639575d7cbe25
Gary <jonesgadev@outlook.com>
2023-04-25 06:31:10 -0700

    Additionof QUEry to access BCS Scores

---
+++ onprc_ehr/resources/queries/onprc_ehr/RandData_BCS.sql
@@ -0,0 +1,16 @@
+SELECT clinical_observations.Id,
+clinical_observations.date,
+clinical_observations.category,
+clinical_observations.area,
+clinical_observations.observation,
+clinical_observations.remark,
+clinical_observations.taskid,
+clinical_observations.performedby,
+clinical_observations.requestid,
+clinical_observations.Container,
+clinical_observations.history,
+clinical_observations.isAssignedAtTime,
+clinical_observations.isAssignedToProtocolAtTime,
+clinical_observations.enteredSinceVetReview,
+clinical_observations.QCState
+FROM clinical_observations
