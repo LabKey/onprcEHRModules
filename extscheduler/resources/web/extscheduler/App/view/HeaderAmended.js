f85e3990821a3aca76c6de599f940ce2895ee704
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-05-10 22:29:11 -0700

    Created new version Pathology Tissues Template import program.

--- extscheduler/resources/web/extscheduler/App/view/HeaderAmended.js
+++ extscheduler/resources/web/extscheduler/App/view/HeaderAmended.js
@@ -3,8 +3,11 @@ Ext.define('App.view.HeaderAmended', {
     alias   : 'widget.appheaderAmended',
     cls     : 'app-header',
     height  : 40,
+    layout: {
+        pack: 'center'
+    },
     padding : '5 10 5 5',
-    tpl     : 'Schedule for : <span style="color:blue;font-weight:bold" class="header-month">{month}</span><span  style="color:blue;font-weight:bold" class="header-year">{year}</span>',
+    tpl     : 'Schedule for : <span style="color:blue;div {text-align: center;};font-weight:bold" class="header-month">{month}</span><span  style="color:blue;font-weight:bold" class="header-year">{year}</span>',
     // tpl     : 'Schedule for <span class="header-month">{month}</span><span class="header-year">{year}</span>',
     bind    : {
         //each bind should have corresponding setter
