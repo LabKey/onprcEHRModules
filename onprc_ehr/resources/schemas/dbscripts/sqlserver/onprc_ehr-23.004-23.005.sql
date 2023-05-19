0b69bce9d20a15b534c15e7fe5c29738ff5e296c
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-05-19 11:49:58 -0700

    Created new version of PE Exam that contains several categories as options to generate PE Reports

---
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.004-23.005.sql
@@ -0,0 +1,10 @@
+---Create 5-18-2023  R. Blasa  notea; No longer needed
+
+
+EXEC core.fn_dropifexists 'PrimeProblemListTemp', 'onprc_ehr', 'TABLE', NULL;
+GO
+
+EXEC core.fn_dropifexists 'p_CaseToPRoblemListupdates', 'onprc_ehr', 'PROCEDURE', NULL;
+GO
+
+
