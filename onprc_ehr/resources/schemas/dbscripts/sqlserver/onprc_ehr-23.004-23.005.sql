d6806c805d2019d1e67f1fd55bee5afac2091f8e
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-05-25 10:44:28 -0700

    Created a new PE Exam with multiple option categories to generate a report.

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.004-23.005.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.004-23.005.sql
@@ -4,6 +4,9 @@
 EXEC core.fn_dropifexists 'PrimeProblemListTemp', 'onprc_ehr', 'TABLE', NULL;
 GO
 
+EXEC core.fn_dropifexists 'PrimeProblemListMaster', 'onprc_ehr', 'TABLE', NULL;
+GO
+
 EXEC core.fn_dropifexists 'p_CaseToPRoblemListupdates', 'onprc_ehr', 'PROCEDURE', NULL;
 GO
 
