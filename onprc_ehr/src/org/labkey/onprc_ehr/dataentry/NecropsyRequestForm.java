ef4f30c21ae2f24af15b12172e976f44c75e96c9
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-06-15 11:54:17 -0700

    Modified Path Tissues template 23.3 version

--- onprc_ehr/src/org/labkey/onprc_ehr/dataentry/NecropsyRequestForm.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/dataentry/NecropsyRequestForm.java
@@ -7,8 +7,6 @@ import org.labkey.api.ehr.dataentry.DataEntryFormContext;
 import org.labkey.api.ehr.dataentry.FormSection;
 import org.labkey.api.ehr.dataentry.NonStoreFormSection;
 import org.labkey.api.ehr.dataentry.RequestForm;
-import org.labkey.api.ehr.security.EHRPathologyEntryPermission;
-import org.labkey.api.ehr.security.EHRRequestPermission;
 import org.labkey.api.module.Module;
 import org.labkey.api.module.ModuleLoader;
 import org.labkey.api.view.template.ClientDependency;
@@ -57,14 +55,13 @@ public class NecropsyRequestForm extends RequestForm
         //Added 2-17-2021  R.Blasa
         addClientDependency(ClientDependency.supplierFromPath("onprc_ehr/form/field/PathologyTissuesField.js"));
 
+        //Added 5-8-2022 Blasa
+        addClientDependency(ClientDependency.supplierFromPath("onprc_ehr/window/PathTissueScanWindow.js"));
 
 //        //    Added: 7-20-2022  R.Blasa
         setStoreCollectionClass("onprc_ehr.data.sources.PathTissueRequestStoreCollection");
         addClientDependency(ClientDependency.supplierFromPath("onprc_ehr/data/sources/PathTissueRequestStoreCollection.js"));
 
-
-        //Added 5-8-2022 Blasa
-        addClientDependency(ClientDependency.supplierFromPath("onprc_ehr/window/PathTissueScanWindow.js"));
 
 
 
@@ -89,19 +86,4 @@ public class NecropsyRequestForm extends RequestForm
         defaultButtons.add("SAVEDRAFT");
         return defaultButtons;
     }
-
-    //Added 5-8-2022 Blasa
-    @Override
-    protected List<String> getMoreActionButtonConfigs()
-    {
-        List<String> defaultButtons = new ArrayList<>();
-        defaultButtons.addAll(super.getMoreActionButtonConfigs()) ;
-
-        defaultButtons.add("Tissues_SCAN_IMPORT");;
-
-        return defaultButtons;
-    }
-
-
-
 }
