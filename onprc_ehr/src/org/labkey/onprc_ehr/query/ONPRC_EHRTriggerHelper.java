eda7c38ff79dbec115c16a498cab6515584615b6
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-07-13 15:14:38 -0700

    Modified caging program to fix issues with oversize alerting bug

--- onprc_ehr/src/org/labkey/onprc_ehr/query/ONPRC_EHRTriggerHelper.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/query/ONPRC_EHRTriggerHelper.java
@@ -924,11 +924,12 @@ public class ONPRC_EHRTriggerHelper
             }
         }
 //        Modified: 3-13-2023  R. Blasa  Double the sq ft when divider is set as open
+//
+//        if (availableDivider.equals("No Slide"))
+//          {
+//            availableSqFt = (availableSqFt) * 2  ;
+//        }
 
-        if (availableDivider.equals("No Slide"))
-          {
-            availableSqFt = (availableSqFt) * 2  ;
-        }
         if (requiredSqFt > availableSqFt)
         {
             ret.add("These animals are too small for the cage.  Has " + Math.round(availableSqFt) + " sq ft. Requires " + Math.round(requiredSqFt) + ".");
