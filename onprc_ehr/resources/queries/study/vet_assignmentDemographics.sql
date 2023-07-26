88ccc1e74a879730fed89f4b86ac16887d23fa33
Gary <jonesga@ohsu.edu>
2023-07-26 10:31:04 -0700

    Update of queries for categoryusage on projects

--- onprc_ehr/resources/queries/study/vet_assignmentDemographics.sql
+++ onprc_ehr/resources/queries/study/vet_assignmentDemographics.sql
@@ -1,33 +1,36 @@
-SELECT nhp.id
-     ,housing.room
-     ,housing.room.area
-     ,demographics.gender
-     ,demographics.species
-     ,demographics.calculated_status
-     --,r1.id as ResearchAssigned
-  --   ,r1.use_category
-   --  ,r2.useCatgegory2
-     --,r2.id as ResourceAssigned
-     ,CMUcase.id AS IDofCase
-     ,CMUcase.assignedVet.userID as CaseVet
-     ,Case
-        when research.project is not null then research.projectType
-        when resource.project is not null then resource.projectType
-        Else Null
-    End as AssignmentType,
-    Case
-        when research.project is not null then research.VetAssignedProject --Update Changed to R from S alias
-        when resource.project is not null then resource.VetAssignedProject  --Update Changed to R from S alias
-        else  null
-        End As project
---UPDATE the query needs to display  protocol when appropriate
-    ,Case
-    When research.project is not null then research.project.protocol.external_id
-      When resource.project is not null then resource.project.protocol.external_id
-    else Null
-    End as Protocol
+SELECT nhp.id,
+       housing.room,
+       housing.room.area,
+       demographics.gender,
+       demographics.species,
+       demographics.calculated_status,
+       CMUcase.id AS IDofCase,
+       CMUcase.assignedVet.userID AS CaseVet,
+       CASE
+           WHEN research.project IS NOT NULL
+               THEN research.projectType
+           WHEN resource.project IS NOT NULL
+               THEN resource.projectType
+           ELSE NULL
+           END AS AssignmentType,
+       CASE
+           WHEN research.project IS NOT NULL
+               THEN research.VetAssignedProject --Update Changed to R from S alias
+           WHEN resource.project IS NOT NULL
+               THEN resource.VetAssignedProject --Update Changed to R from S alias
+           ELSE NULL
+           END AS project
+       --UPDATE the query needs to display  protocol when appropriate
+        ,
+       CASE
+           WHEN research.project IS NOT NULL
+               THEN research.project.protocol.external_id
+           WHEN resource.project IS NOT NULL
+               THEN resource.project.protocol.external_id
+           ELSE NULL
+           END AS Protocol
 FROM Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.animal nhp
-    LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.ClinicalCases_Open AS CMUcase ON CMUcase.id = nhp.id
+LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.ClinicalCases_Open AS CMUcase ON CMUcase.id = nhp.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.housing housing ON housing.id = nhp.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.demographics demographics ON demographics.id = nhp.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.vet_AssignedResearch research ON nhp.id = research.id
