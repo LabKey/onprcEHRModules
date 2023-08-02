c0dbbe825485d2c1684ab90674c77b1afb4d183b
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-08-02 14:55:05 -0700

    Modified Compliance reports to include snooze date

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceProcedureRecentTests.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceProcedureRecentTests.sql
@@ -24,8 +24,8 @@ select b.requirementname,
 
        (Select group_concat(distinct yy.comment, chr(10))  from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid )
                                                                                      And  yy.requirementname= b.requirementname and yy.employeeid= a.employeeid   ) as comment,
-       (Select yy.snooze_date from completiondates yy where yy.date in (select max(zz.snooze_date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid )
-                                                                                     And  yy.requirementname= b.requirementname and yy.employeeid= a.employeeid   ) as snoozedate,
+       (Select yy.snooze_date from completiondates yy where yy.snooze_date in (select max(zz.snooze_date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid )
+                                                                                     And  yy.requirementname= b.requirementname and yy.employeeid= a.employeeid   ) as snooze_date,
 
        CAST(
                CASE
@@ -82,8 +82,8 @@ select a.requirementname,
        (Select group_concat(distinct yy.comment, chr(10))  from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= a.requirementname and zz.employeeid= a.employeeid )
                                                                                      And  yy.requirementname= a.requirementname and yy.employeeid= a.employeeid   ) as comment,
 
-       (Select yy.snooze_date  from completiondates yy where yy.date in (select max(zz.snooze_date) from completiondates zz where zz.requirementname= a.requirementname and zz.employeeid= a.employeeid )
-                                                                                     And  yy.requirementname= a.requirementname and yy.employeeid= a.employeeid   ) as snoozedate,
+       (Select yy.snooze_date  from completiondates yy where yy.snooze_date in (select max(zz.snooze_date) from completiondates zz where zz.requirementname= a.requirementname and zz.employeeid= a.employeeid )
+                                                                                     And  yy.requirementname= a.requirementname and yy.employeeid= a.employeeid   ) as snooze_date,
 
        CAST(
                CASE
