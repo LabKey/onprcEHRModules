6c5209709541926d81a183d12f3ae866e91a09ab
Gary <jonesga@ohsu.edu>
2023-08-05 09:13:09 -0700

    Update of Vet_assignmentDemograohcis to display project, protocol and PI

--- onprc_ehr/resources/queries/study/vet_assignmentDemographics.sql
+++ onprc_ehr/resources/queries/study/vet_assignmentDemographics.sql
@@ -4,42 +4,28 @@ SELECT nhp.id,
        demographics.gender,
        demographics.species,
        demographics.calculated_status,
-       CMUcase.id AS IDofCase,
+    --   CMUcase.id AS IDofCase,
        CMUcase.assignedVet.userID AS CaseVet,
-       CASE
+       assignment.project AS project,
+       assignment.project.protocol as Protocol,
+       assignment.project.protocol.investigatorID.lastName as ProtocolPI,
+
+     CASE
            WHEN research.project IS NOT NULL
                THEN research.projectType
            WHEN resource.project IS NOT NULL
                THEN resource.projectType
            ELSE NULL
-           END AS AssignmentType,
-       CASE
-           WHEN research.project IS NOT NULL
-               THEN research.VetAssignedProject
-           WHEN resource.project IS NOT NULL
-               THEN resource.VetAssignedProject
-           ELSE NULL
-           END AS project,
-       CASE
-           WHEN research.project IS NOT NULL
-               THEN research.project.protocol.external_id
-           WHEN resource.project IS NOT NULL
-               THEN resource.project.protocol.external_id
-           ELSE NULL
-           END AS Protocol,
-    CASE
-           WHEN research.project IS NOT NULL
-               THEN research.project.protocol.investigatorID.lastName
-           WHEN resource.project IS NOT NULL
-               THEN resource.project.protocol.investigatorID.lastName
-           ELSE NULL
-END AS ProtocolPI
+           END AS AssignmentType
+
 FROM Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.animal nhp
 LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.ClinicalCases_Open AS CMUcase ON CMUcase.id = nhp.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.housing housing ON housing.id = nhp.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.demographics demographics ON demographics.id = nhp.id
+    LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.assignment assignment ON assignment.id = nhp.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.vet_AssignedResearch research ON nhp.id = research.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.vet_assignedResource resource ON nhp.id = resource.id
 WHERE demographics.Calculated_Status = 'Alive'
   AND nhp.id NOT LIKE '[A-Z]%'
   AND housing.enddateTimeCoalesced >= now()
+  AND (Assignment.enddate is null or assignment.enddate >= now())
