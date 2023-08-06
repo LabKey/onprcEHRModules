7de89177fd0fea60bda33d134c84349d920040fd
Gary <jonesga@ohsu.edu>
2023-08-06 12:02:15 -0700

    Update of Vet_assignmentDemograohcis to allow all alive animals to be presented

--- onprc_ehr/resources/queries/study/vet_assignmentDemographics.sql
+++ onprc_ehr/resources/queries/study/vet_assignmentDemographics.sql
@@ -22,10 +22,10 @@ FROM Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.ani
 LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.ClinicalCases_Open AS CMUcase ON CMUcase.id = nhp.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.housing housing ON housing.id = nhp.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.demographics demographics ON demographics.id = nhp.id
-    LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.assignment assignment ON assignment.id = nhp.id
+    LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.assignment assignment ON assignment.id = nhp.id   AND (Assignment.enddate is null or assignment.enddate >= now())
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.vet_AssignedResearch research ON nhp.id = research.id
     LEFT JOIN Site.{substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.vet_assignedResource resource ON nhp.id = resource.id
 WHERE demographics.Calculated_Status = 'Alive'
   AND nhp.id NOT LIKE '[A-Z]%'
   AND housing.enddateTimeCoalesced >= now()
-  AND (Assignment.enddate is null or assignment.enddate >= now())
+
