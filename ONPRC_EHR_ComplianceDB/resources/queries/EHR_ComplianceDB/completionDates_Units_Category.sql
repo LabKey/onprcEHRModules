0e5693a2683c386d0e987194360b094f46fe66b5
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-08-09 15:06:36 -0700

    Created a new revised version of Completion Dates with units and category options

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/completionDates_Units_Category.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/completionDates_Units_Category.sql
@@ -1,16 +1,13 @@
-select a.employeeid,
-       a.date,
-       a.requirementname,
-       (select group_Concat(distinct t.category,chr(10)) from ehr_compliancedb.employeeperUnit k, ehr_compliancedb.requirementspercategory t where  t.requirementname = a.requirementname and k.category = t.category and k.employeeid = a.employeeid) as category,
-       (select group_concat(distinct j.unit,chr(10)) from ehr_compliancedb.employeeperUnit k, ehr_compliancedb.requirementspercategory j
-        where j.requirementname = a.requirementname and k.unit = j.unit and k.employeeid = a.employeeid) as unit,
-       a.result,
-       a.comment,
-       a.filename,
-       a.trainer,
-       a.snooze_date
+select b.employeeid,
+       (select Case when` max(t.date) is not null then max(t.date) else null end  from ehr_compliancedb.completiondates t where t.requirementname = a.requirementname and t.employeeid = b.employeeid) as completiondate,
+        a.requirementname,
+       b.unit as unit,
+       b.category as category
 
 
-from  ehr_compliancedb.completiondates a
 
----Where a.employeeId in ( select distinct h.employeeId from ehr_compliancedb.employees h where h.enddate is null)
+
+from  ehr_compliancedb.requirementspercategory a, employeeperUnit b
+where (a.unit = b.unit or a.category = b.category)
+group by b.employeeid,a.requirementname, b.unit, b.category
+order by b.unit, b.category
