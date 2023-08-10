7079253616c9bec388666ca718a62a5b1df812fc
Gary <jonesga@ohsu.edu>
2023-08-10 07:09:56 -0700

    Update of Public Finacne Rate Calc

---
+++ onprc_billingpublic/resources/queries/onprc_billing_public/NIHIndustryRate_New.sql
@@ -0,0 +1,21 @@
+PARAMETERS (MARGIN FLOAT DEFAULT .25, FARATE FLOAT DEFAULT 1)
+Select
+n.category,
+n.name,
+--UnitCost as CurrentYear,
+(SUM((n.UnitCost/(1-N.subsidy))*(1+MARGIN))*FARATE )  as CurrentYear,
+(SUM((N.year1/(1-N.subsidy))*(1+MARGIN))*FARATE ) as year1,
+(SUM((N.year2/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year2,
+(SUM((N.year3/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year3,
+(SUM((N.year4/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year4,
+(SUM((N.year5/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year5,
+(SUM((N.year6/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year6,
+(SUM((N.year7/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year7,
+(SUM((N.year8/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year8,
+n.PostedDate
+
+FROM NIHRateConfig n
+Group by
+n.category,
+n.name,
+n.postedDate
