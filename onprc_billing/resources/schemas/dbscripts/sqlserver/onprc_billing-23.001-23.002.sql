39b220c00cd889082c6c8b0a52e5cb1b329b2a65
Gary <jonesga@ohsu.edu>
2023-08-15 09:22:02 -0700

    New Stored Proecure and ETL for LabFees

--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-23.001-23.002.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-23.001-23.002.sql
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ ---Create 5-2023-08-15 jonesga
  */
 -- ================================================
 -- Template generated from Template Explorer using:
--- onprc_billing/src/org/labkey/onprc_billing/ONPRC_BillingModule.java
+++ onprc_billing/src/org/labkey/onprc_billing/ONPRC_BillingModule.java
@@ -11,7 +11,8 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License
+ * ---Create 5-2023-08-15 jonesga.
  */
 
 package org.labkey.onprc_billing;
