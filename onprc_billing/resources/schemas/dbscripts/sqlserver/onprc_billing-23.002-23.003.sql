96d0190c78e92099b959290479279ca0558eb6f6
Gary <jonesga@ohsu.edu>
2023-08-16 10:23:51 -0700

    New Stored Proecure and ETL for LabFees removing unwanted Files

--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-23.002-23.003.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-23.002-23.003.sql
@@ -6,6 +6,7 @@ CREATE PROCEDURE onprc_billing.UpdateClinPathEndDate
     AS
 BEGIN
     --Updates end Date for ClinPath when complete but no dateUpdate [Labkey_uat].[studyDataset].[c6d199_clinpathruns]
+    --update todya 8/16/2023
 Update [studyDataset].[c6d199_clinpathruns]
 set datefinalized =  date
 where dateFinalized is null and date > '5/1/2023' and qcstate = 18
--- onprc_billing/src/org/labkey/onprc_billing/ONPRC_BillingModule.java
+++ onprc_billing/src/org/labkey/onprc_billing/ONPRC_BillingModule.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *  New Build with Module Number Change 8/16/2023
+ *  New Build with Module Number Change 8/16/2023 new FB
  * */
 
 package org.labkey.onprc_billing;
