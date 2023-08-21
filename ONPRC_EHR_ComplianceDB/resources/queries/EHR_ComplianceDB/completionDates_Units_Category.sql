6ec60c6828be9a295a8d118a338b633f2bf131f2
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-08-21 14:52:55 -0700

    Modified Compliance Units and category reports

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/completionDates_Units_Category.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/completionDates_Units_Category.sql
@@ -1,11 +1,7 @@
 select b.employeeid,
-       (select Case when` max(t.date) is not null then max(t.date) else null end  from ehr_compliancedb.completiondates t where t.requirementname = a.requirementname and t.employeeid = b.employeeid) as completiondate,
-        a.requirementname,
+       a.requirementname,
        b.unit as unit,
        b.category as category
-
-
-
 
 from  ehr_compliancedb.requirementspercategory a, employeeperUnit b
 where (a.unit = b.unit or a.category = b.category)
