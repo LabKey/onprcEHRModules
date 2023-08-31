24785978e34f44529457aae50a301c03f7288ea4
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-08-31 13:56:11 -0700

    Modified Ovsersize cage program updates.

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.005-23.006.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.005-23.006.sql
@@ -12,9 +12,6 @@ CREATE Procedure [onprc_ehr].[p_CageStatusupdates]
 
 AS
 
-DECLARE
-
-
 BEGIN
 
     --- Set Cage status to Normal                       )
@@ -22,6 +19,85 @@ BEGIN
      Update ehr_lookups.cage
           Set status = 'Normal'
          Where status is null
+
+
+    If @@Error <> 0
+	  GoTo Err_Proc
+
+
+ RETURN 0
+
+
+Err_Proc:
+                    -------Error Generated
+	RETURN 1
+
+
+END
+
+GO
+
+
+- Author:	R. Blasa
+-- Created: 8-30-2023
+-- Description:	Temp table for cage information audit history.
+EXEC core.fn_dropifexists 'CageAuditLog', 'onprc_ehr', 'TABLE', NULL;
+GO
+
+CREATE TABLE [onprc_ehr].[CageAuditLog](
+    [searchid] [int] IDENTITY(100,1) NOT NULL,
+    [rowid] [int] NULL,
+    [location] [nvarchar](100) NULL,
+    [room] [varchar](200) NULL,
+    [cage] [varchar](200) NULL,
+    [divider] [int] NULL,
+    [cage_type] [varchar](100) NULL,
+    [hasTunnel] [bit] NULL,
+    [status] [varchar](200) NULL,
+    [Container] [dbo].[ENTITYID] NOT NULL,
+    [date_created] [smalldatetime] NULL
+    ) ON [PRIMARY]
+    GO
+
+
+
+
+
+
+
+
+
+
+EXEC core.fn_dropifexists 'p_CageAuditHistoryProcess', 'onprc_ehr', 'PROCEDURE', NULL;
+GO
+
+-- Author:	R. Blasa
+-- Created: 8-30-2023
+-- Description:	Stored procedure program to provide historical cage information audit history.
+
+
+CREATE Procedure [onprc_ehr].[p_CageAuditHistoryProcess]
+
+AS
+
+
+BEGIN
+
+    --- Create historical cage data                    )
+
+Insert into onprc_ehr.CageAuditLog
+Select  rowid,
+        location,
+        room,
+        cage,
+        divider,
+        cage_type,
+        hasTunnel,
+        status,
+        container,
+        getdate()
+
+from ehr_lookups.cage
 
 
     If @@Error <> 0
