effc0c80efbbc5f4f2047e03a7cc8d701ff7357b
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-09-05 15:30:14 -0700

    Modified Ovsersize cage program updates.

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.005-23.006.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.005-23.006.sql
@@ -83,19 +83,22 @@ BEGIN
     --- Create historical cage data
 If exists (select * from onprc_ehr.CageAuditLog)
 BEGIN
-   Insert into onprc_ehr.CageAuditLog
-    Select  rowid,
-        location,
-        room,
-        cage,
-        divider,
-        cage_type,
-        hasTunnel,
-        status,
-        container,
+Insert into onprc_ehr.CageAuditLog
+Select  rowid,
+        a.location,
+        a.room,
+        a.cage,
+        a.divider,
+        a.cage_type,
+        a.hasTunnel,
+        a.status,
+        a.container,
+        (select h.area from ehr_lookups.rooms h where h.room = a.room) as area,
+        (select s.value from ehr_lookups.rooms h, ehr_lookups.lookups s where h.room = a.room and s.rowid = h.housingtype) as housingtype,
+        (select s.value from ehr_lookups.rooms h, ehr_lookups.lookups s  where h.room = a.room and s.rowid = h.housingcondition) as housingcondition,
         getdate()
 
-  from ehr_lookups.cage
+from ehr_lookups.cage a
 
     If @@Error <> 0
 	  GoTo Err_Proc
