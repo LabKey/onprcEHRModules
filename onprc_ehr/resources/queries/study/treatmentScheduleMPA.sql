eb03fe07d54d25696b512f623a298b57623256af
kollil <kollil@ohsu.edu>
2023-10-03 17:06:08 -0700

    Added new alert for the MPA injection.

---
+++ onprc_ehr/resources/queries/study/treatmentScheduleMPA.sql
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+
+ -- Created by Kollil on 9/25/2023
+ Tkt# 9939
+ Creating a new alert for this medication. This MPA injection is a post op med category but scheduled once a month.
+
+*/
+Select * From treatmentSchedulePostops
+Where code like 'E-85760'
