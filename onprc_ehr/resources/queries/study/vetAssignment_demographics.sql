44f72dbfe5355ae228bd62a5a2ff9738eb0e4299
kollil <kollil@ohsu.edu>
2023-10-09 23:13:57 -0700

    Updated vet sql queries

--- onprc_ehr/resources/queries/study/vetAssignment_demographics.sql
+++ onprc_ehr/resources/queries/study/vetAssignment_demographics.sql
@@ -1,4 +1,49 @@
 /*
+study.vetAssignment_demographics
+
+Replaces: study.vetAssignmentDemographics
+
+Returns at least one record for all living NHPs at the center.
+Notes:
+  * Multiple open cases and multiple assignments for a single animal
+    result in open cases * assignments for that animal
+  * Left joins are needed for CMUcases and assignedProject as those tables
+    might not have records for an animal ID. Left joins are used for all
+	other joins to be resiliant against missing data.
+  * enddate.TimeCoalesced is never in the future
+
+Future modifications:
+  * Limit open cases to those that are not inactive	after confirming w/ Heather
+ */
+
+SELECT demographics.id
+     , CMUcases.assignedVet.displayName AS caseVet
+     , CMUcases.date AS caseDate
+     , housing.room
+     , housing.room.area
+     , assignedProject.project AS project
+     , assignedProject.Protocol AS protocol
+     , assignedProject.PI AS protocolPI
+     , assignedProject.projectType AS assignmentType
+     , demographics.calculated_status
+     , demographics.gender
+     , demographics.species
+     , demographics.history
+FROM Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.animal AS nhp
+LEFT JOIN Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.ClinicalCases_Open AS CMUcases
+ON CMUcases.id = nhp.id
+    LEFT JOIN Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.housing AS housing
+    ON housing.id = nhp.id
+    LEFT JOIN Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.demographics AS demographics
+    ON demographics.id = nhp.id
+    LEFT JOIN Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.vetAssignment_projects AS assignedProject
+    ON assignedProject.id = nhp.id
+WHERE demographics.Calculated_Status = 'Alive'
+  AND nhp.id NOT LIKE '[A-Z]%'
+  AND housing.enddate IS NULL
+
+
+/*
 study.vetAssignment_demographics.sql
 
 Replaces: study.vetAssignmentDemographics
@@ -14,7 +59,7 @@ Notes:
 Future modifications:
   * Limit open cases to those that are not inactive
  */
-
+/*
 SELECT demographics.id
      , CMUcases.assignedVet.displayName AS caseVet
      , CMUcases.DATE AS caseDate
@@ -43,3 +88,5 @@ WHERE demographics.Calculated_Status = 'Alive'
     housing.enddateTimeCoalesced >= Curdate()
    OR housing.enddateTimeCoalesced IS NULL
     )
+
+*/
