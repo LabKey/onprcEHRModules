ca10f954dd9bbaf7c98447eb3059dcf47e6faf91
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2023-10-11 13:11:59 -0700

    Vet assignment query edits (#815)
    * Updated vet sql queries
    
    * Added the query.xml

--- onprc_ehr/resources/queries/study/demographicsAssignedVet.sql
+++ onprc_ehr/resources/queries/study/demographicsAssignedVet.sql
@@ -1,34 +1,58 @@
 /*
-study.demographicsAssignedVet.sql
+study.demographicsAssignedVet
 
 Returns one or more assigned vets per animal ID
-  * When there is an open case, it returns one record for each open case
-  * When there is not an open case, it returns one record for each matched rule
-    *at the same level* taking into account research vs. non-research. In other
-    words, it returns all matching records at the lowest number rule for each
-    animal
+  * When there is an open case, it returns one record for each vet having an open
+    case for that animal
+  * When there is not an open case, it returns one record for the lowest-numbered
+    matched rule
   * VAF2 determines the lowest matched rule for each animal. Doing an inner join
     with VAF1 results in only those records matching the lowest matched rule per
-    animal.
+    animal. The select distinct limits it to a single record.
+
+Future enhancements
+  * Add ProjectType to select statement and hide in XML
+  * Add Protocol_PI to study.vet_assignmentFilter and to onprc_ehr.vet_assignment
+  * Add species to study.vet_assignmentFilter and to onprc_ehr.vet_assignment
+  * Add validation to onprc_ehr.vet_assignment to prevent creating a rule that has
+  *   no match in the filter
  */
 
-SELECT VAF1.Id
-     , VAF1.AssignedVet
-     , VAF1.AssignmentType
-/*	, VAF1.AssignmentType AS codeAssignmentType*/
-     , VAF1.Room
-     , VAF1.Area
-     , VAF1.Project
-     , VAF1.Protocol
-     , VAF1.ProtocolPI
-     , VAF1.Calculated_Status
-     , VAF1.MatchedRule
-     , VAF1.history
+SELECT DISTINCT VAF1.Id
+              , VAF1.AssignedVet
+              , VAF1.AssignmentType
+              , VAF1.Room
+              , VAF1.Area
+              , VAF1.Calculated_Status
+              , VAF1.MatchedRule
 FROM vetAssignment_filter AS VAF1
          INNER JOIN (
     SELECT VAF2.Id
          , MIN(VAF2.matchedRule) AS minRule
     FROM vetAssignment_filter AS VAF2
     GROUP BY VAF2.id
-) AS VAF3 ON VAF1.id = VAF3.id
+) AS VAF3
+    ON VAF1.id = VAF3.id
     AND VAF1.matchedRule = VAF3.minRule
+
+
+-- SELECT VAF1.Id
+--      , VAF1.AssignedVet
+--      , VAF1.AssignmentType
+-- /*	, VAF1.AssignmentType AS codeAssignmentType*/
+--      , VAF1.Room
+--      , VAF1.Area
+--      , VAF1.Project
+--      , VAF1.Protocol
+--      , VAF1.ProtocolPI
+--      , VAF1.Calculated_Status
+--      , VAF1.MatchedRule
+--      , VAF1.history
+-- FROM vetAssignment_filter AS VAF1
+--          INNER JOIN (
+--     SELECT VAF2.Id
+--          , MIN(VAF2.matchedRule) AS minRule
+--     FROM vetAssignment_filter AS VAF2
+--     GROUP BY VAF2.id
+-- ) AS VAF3 ON VAF1.id = VAF3.id
+--     AND VAF1.matchedRule = VAF3.minRule
