7d329d009a351b7746917090fa79429986a9f6c3
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-10-19 11:54:02 -0700

    Modified Completion Date data set to include employee requirements. report Units and category as optional reporting columns

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/completionDates_Units_Category.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/completionDates_Units_Category.sql
@@ -4,7 +4,23 @@ select b.employeeid,
        b.unit as unit,
        b.category as category
 
-from  ehr_compliancedb.requirementspercategory a, employeeperUnit b
+from  ehr_compliancedb.requirementspercategory a, ehr_compliancedb.employeeperUnit b
 where (a.unit = b.unit or a.category = b.category)
 group by b.employeeid,a.requirementname, b.unit, b.category
-order by b.unit, b.category
+
+
+union
+
+select j.employeeid,
+      null as completiondate,
+    j.requirementname,
+    null as unit,
+    null as category
+
+from  ehr_compliancedb.RequirementsPerEmployee j
+Where j.requirementname NOT in (select z.requirementname from ehr_compliancedb.completiondates z where z.requirementname = j.requirementname
+                                                                                               and z.employeeid = j.employeeid and z.date is not null)
+  And j.employeeid in (select p.employeeid from ehr_compliancedb.employees p where p.enddate is null)
+
+
+group by j.employeeid, j.requirementname
