e40e8ff9c0c67d9200bbf65be03d742af6f6e4c4
kollil <kollil@ohsu.edu>
2023-10-23 20:39:15 -0700

    updated a file

--- onprc_ehr/resources/queries/study/IPC_Embedding.sql
+++ onprc_ehr/resources/queries/study/IPC_Embedding.sql
@@ -1,2 +1,3 @@
+-- Get Embedding values
 SELECT value, name from Site.{substitutePath moduleProperty('EHR','EHRStudyContainer')}.onprc_ehr.IPC_Reference_Data
 Where name like 'IPCEmbedding' and dateDisabled is null
