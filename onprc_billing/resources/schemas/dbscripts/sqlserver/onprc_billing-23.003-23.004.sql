974a96bf7eac7510f9c22afee83c083c074adf14
Gary <jonesga@ohsu.edu>
2023-10-31 08:52:35 -0700

    Remove orphaned views for Covid

--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-23.003-23.004.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-23.003-23.004.sql
@@ -1,4 +1,7 @@
 EXEC DropifExists 'annualinflationrate','onprc_billing','table',Null
-EXEC DropifExists 'vw_covid19dcmdaily','onprc_billing','view',Null
+/*EXEC DropifExists 'vw_covid19dcmdaily','onprc_billing','view',Null
 EXEC DropifExists 'vw_covid19dcmschedule','onprc_billing','view',Null
-EXEC DropifExists 'vw_covid19research','onprc_billing','view',Null
+EXEC DropifExists 'vw_covid19research','onprc_billing','view',Null*/
+DROP VIEW IF EXISTS extcheculer.vw_covid19dcmdaily ;
+DROP VIEW IF EXISTS extcheculer.vw_covid19dcmschedule ;
+DROP VIEW IF EXISTS extcheculer.vw_covid19research ;
