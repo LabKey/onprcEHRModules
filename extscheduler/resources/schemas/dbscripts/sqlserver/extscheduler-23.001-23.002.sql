9631a061cc847fb8fc4590b587c5544e0bd63e47
Gary <jonesga@ohsu.edu>
2023-11-07 14:22:26 -0700

    Removed sql script pointing to extscheduler which was in the worng folder.

--- extscheduler/resources/schemas/dbscripts/sqlserver/extscheduler-23.001-23.002.sql
+++ extscheduler/resources/schemas/dbscripts/sqlserver/extscheduler-23.001-23.002.sql
@@ -3,8 +3,14 @@ EXEC core.fn_dropifexists 'TempCoV19Final', 'extScheduler', 'TABLE', NULL;
 EXEC core.fn_dropifexists 'covid19testing', 'extScheduler', 'TABLE', NULL;
 GO
 
+EXEC core.fn_dropifexists 'tempscheduler', 'extScheduler', 'TABLE', NULL;
+GO
+
 EXEC core.fn_dropifexists 'TempCoV19Interim', 'extScheduler', 'TABLE', NULL;
 GO
 
 EXEC core.fn_dropifexists 'vw_Covid19Research', 'extScheduler', 'VIEW', NULL;
+GO
+
+EXEC core.fn_dropifexists 'vw_covid19dcmschedule', 'extScheduler', 'VIEW', NULL;
 GO
