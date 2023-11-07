4c6f402cdb85f335fd752872fe5c033f71ca2ddc
Gary <jonesga@ohsu.edu>
2023-11-07 07:56:07 -0700

    Update of process to remove orphaned sql scripts associated with extscheduler and insure that onprc_billing actions are consistent

---
+++ extscheduler/resources/schemas/dbscripts/sqlserver/extscheduler-23.001-23.002.sql
@@ -0,0 +1,10 @@
+EXEC core.fn_dropifexists 'TempCoV19Final', 'extScheduler', 'TABLE', NULL;
+    GO
+EXEC core.fn_dropifexists 'covid19testing', 'extScheduler', 'TABLE', NULL;
+GO
+
+EXEC core.fn_dropifexists 'TempCoV19Interim', 'extScheduler', 'TABLE', NULL;
+GO
+
+EXEC core.fn_dropifexists 'vw_Covid19Research', 'extscheculer', 'VIEW', NULL;
+GO
--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-23.003-23.004.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-23.003-23.004.sql
@@ -1,2 +1,1 @@
 EXEC core.fn_dropifexists 'annualinflationrate','onprc_billing','table',Null
-EXEC core.fn_dropifexists 'vw_Covid19Research', 'onprc_billing', 'VIEW'
