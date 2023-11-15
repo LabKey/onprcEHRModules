19f2eeb8b1083e71c473e17717d493e2f700e853
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2023-11-15 12:37:31 -0800

    Modified sql program to include a specific filters for Clinical Category.

--- onprc_ehr/resources/queries/study/duplicateCases.sql
+++ onprc_ehr/resources/queries/study/duplicateCases.sql
@@ -3,6 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
+-- Modified: 11-15-2023 R. Blasa
 SELECT
 id,
 category,
@@ -10,6 +11,7 @@ count(*) as totalCases
 
 FROM study.cases a
 WHERE a.isActive = true
+And a.category = 'Clinical'
 GROUP BY a.id, a.category
 HAVING count (*) > 1
 
