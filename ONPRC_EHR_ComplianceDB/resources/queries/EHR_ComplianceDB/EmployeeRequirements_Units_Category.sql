86a9c34b0491e8e6348db11f71047819762fc2d2
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-01-12 11:44:37 -0800

    Modified Completion date data set reporting.

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/EmployeeRequirements_Units_Category.sql
+++
@@ -1,9 +0,0 @@
-select b.employeeid,
-       a.requirementname,
-       b.unit as unit,
-       b.category as category
-
-from  ehr_compliancedb.requirementspercategory a, employeeperUnit b
-where (a.unit = b.unit or a.category = b.category)
-group by b.employeeid,a.requirementname, b.unit, b.category
-order by b.unit, b.category
