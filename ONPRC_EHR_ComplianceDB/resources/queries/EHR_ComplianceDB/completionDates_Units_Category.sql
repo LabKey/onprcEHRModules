56e427896db62eff0ecc90f3f74dabc9f54d1d3f
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-01-12 10:20:09 -0800

    Modified Completion date data set reporting.

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/completionDates_Units_Category.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/completionDates_Units_Category.sql
@@ -3,35 +3,26 @@ select e.employeeid,
        (Select k.Firstname from ehr_compliancedb.employees k where k.employeeid = e.employeeid) as firstname,
        (Select k.majorudds from ehr_compliancedb.employees k where k.employeeid = e.employeeid) as host,
        (Select k.enddate from ehr_compliancedb.employees k where k.employeeid = e.employeeid) as enddate,
-
-       (select max(zz.date) from ehr_compliancedb.completiondates zz where zz.requirementname = b.requirementname and zz.employeeid= e.employeeid   ) as completiondate,
-
-       b.requirementname,
-
-       (Select group_concat(distinct k.type, chr(10)) from ehr_compliancedb.requirements k where k.requirementname = b.requirementname) as type,
-       group_concat(distinct e.unit, chr(10)) as unit,
-       group_concat(distinct e.category, chr(10)) as category,
-
-       (select group_concat(distinct k.result, chr(10)) from ehr_compliancedb.completiondates k where k.requirementname = b.requirementname and k.employeeid = e.employeeid
-                                                                                                  And k.date in (select max(zz.date) from ehr_compliancedb.completiondates zz where zz.requirementname = b.requirementname and zz.employeeid= e.employeeid ))  as result,
-
-       (select group_concat(distinct k.comment, chr(10)) from ehr_compliancedb.completiondates k where k.requirementname = b.requirementname and k.employeeid = e.employeeid
-                                                                                                   And k.date in (select max(zz.date) from ehr_compliancedb.completiondates zz where zz.requirementname = b.requirementname and zz.employeeid= e.employeeid ))  as comment,
-
-       (select group_concat(distinct k.filename, chr(10)) from ehr_compliancedb.completiondates k where k.requirementname = b.requirementname and k.employeeid = e.employeeid
-                                                                                                    And k.date in (select max(zz.date) from ehr_compliancedb.completiondates zz where zz.requirementname = b.requirementname and zz.employeeid= e.employeeid ))  as filename,
-
-       (select group_concat(distinct k.trainer, chr(10)) from ehr_compliancedb.completiondates k where k.requirementname = b.requirementname and k.employeeid = e.employeeid
-                                                                                                   And k.date in (select max(zz.date) from ehr_compliancedb.completiondates zz where zz.requirementname = b.requirementname and zz.employeeid= e.employeeid ))  as trainer,
-
-       (select group_concat(distinct k.snooze_date, chr(10)) from ehr_compliancedb.completiondates k where k.requirementname = b.requirementname and k.employeeid = e.employeeid
-                                                                                                       And k.date in (select max(zz.date) from ehr_compliancedb.completiondates zz where zz.requirementname = b.requirementname and zz.employeeid= e.employeeid ))  as snooze_date
+       group_concat(distinct e.date, chr(10)) as completiondate ,
+       e.requirementname,
+       (Select hj.type from ehr_compliancedb.requirements  hj where hj.requirementname = e.requirementname) as type,
 
 
-from  ehr_compliancedb.requirementspercategory b, ehr_compliancedb.employeeperUnit e
-Where  (b.unit = e.unit or b.category = e.category )
-  And e.employeeid  in (select distinct kk.employeeid from ehr_compliancedb.Employees kk where kk.enddate is null)
+       group_concat(distinct e.result, chr(10)) as result,
+       group_concat(distinct e.comment, chr(10)) as comment,
+       group_concat(distinct e.filename, chr(10)) as filename,
+       group_concat(distinct e.trainer, chr(10)) as trainer,
+       group_concat(distinct e.snooze_date, chr(10)) as snooze_date,
+       (select group_concat(distinct sk.unit, chr(10)) from ehr_compliancedb.requirementspercategory jb, ehr_compliancedb.employeeperUnit sk where jb.requirementname = e.requirementname
+                                                                                                                                               and sk.employeeid = e.employeeid    And  (jb.unit = sk.unit  )) as Unit,
+
+       (select group_concat(distinct jk.category, chr(10)) from ehr_compliancedb.requirementspercategory b, ehr_compliancedb.employeeperUnit jk where b.requirementname = e.requirementname
+                                                                                                                                                  and jk.employeeid = e.employeeid  And  (b.category = jk.category ) ) as  category
 
 
-group by e.employeeid, b.requirementname
+from ehr_compliancedb.completiondates e
+----
+Where e.employeeid  in (select distinct kk.employeeid from ehr_compliancedb.Employees kk where kk.enddate is null)
+
+group by e.employeeid, e.requirementname
 
