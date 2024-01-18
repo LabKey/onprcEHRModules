3b2bb6d145b0f7864ed24125c29dec66ec2cd104
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-01-17 16:21:02 -0800

    Created Environmental Assessmentinput forms.

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/EmployeeToRequirementsXcel.sql
+++
@@ -1,8 +0,0 @@
-select
-    jj.employeeid as employeeid,
-    coalesce(jj.requirementname, '') as requirementname
-from ehr_compliancedb.employeeTraining  jj where
-        jj.employeeid in (select ss.employeeid from ehr_compliancedb.Employees ss where ss.MajorUDDS = 'OPS - ASB')
-
-group by  jj.employeeid, jj.Requirementname
-    PIVOT   requirementname by employeeid
