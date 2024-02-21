5caa0e737098fcf3e8615afb31871382c6b4519e
Marty Pradere <martyp@labkey.com>
2024-02-21 14:57:53 -0800

    Schema

---
+++ ONPRC_EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/onprc_ehr_compliancedb-0.00-24.00.sql
@@ -0,0 +1,41 @@
+
+CREATE SCHEMA onprc_ehr_compliancedb;
+GO
+
+CREATE TABLE onprc_ehr_compliancedb.SciShield_Data
+(
+    RowId INT IDENTITY(1,1) NOT NULL,
+    employeeId nvarchar(255) not null,
+    requirementname nvarchar(255) null,
+    Date datetime null,
+    Container ENTITYID NOT NULL,
+    comment nvarchar(2000) null,
+    CreatedBy USERID,
+    Created datetime,
+    ModifiedBy USERID,
+    Modified datetime,
+    processed int NULL
+
+    CONSTRAINT PK_ScieShield_Data PRIMARY KEY (RowId),
+    CONSTRAINT FK_ONPRC_EHR_COMPLIANCE_SCISHIELD_DATA_CONTAINER FOREIGN KEY (Container) REFERENCES core.Containers (EntityId)
+);
+
+
+CREATE TABLE onprc_ehr_compliancedb.SciShield_Reference_Data
+(
+    rowId int identity(1,1),
+    label nvarchar(250) NULL,
+    value nvarchar(255) NOT NULL ,
+    columnName nvarchar(255)  NOT NULL,
+    sort_order integer  null,
+    endDate  datetime  NULL,
+    Container ENTITYID NOT NULL,
+    CreatedBy USERID,
+    Created datetime,
+    ModifiedBy USERID,
+    Modified datetime,
+
+    CONSTRAINT pk_SciShield_reference PRIMARY KEY (value),
+    CONSTRAINT FK_ONPRC_EHR_COMPLIANCE_REFERENCE_DATA_CONTAINER FOREIGN KEY (Container) REFERENCES core.Containers (EntityId)
+);
+GO
