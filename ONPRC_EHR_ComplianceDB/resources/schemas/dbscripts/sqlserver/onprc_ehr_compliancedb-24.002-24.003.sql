72e2194b12bd54b7e2565cafe45c5983a0d47959
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-03-23 00:59:04 -0700

    Created OccHealth input form proces

---
+++ ONPRC_EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/onprc_ehr_compliancedb-24.002-24.003.sql
@@ -0,0 +1,65 @@
+
+
+
+
+-- Author:	R. Blasa
+-- Created: 3-22-2024
+-- Description:	Stored procedure program process SciShield INitial Data
+
+CREATE TABLE onprc_ehr_compliancedb.OccHealth_Data
+(
+    RowId INT IDENTITY(1,1) NOT NULL,
+    Email                  nvarchar(500) null,
+    [Person Type]          nvarchar(255) null,
+    [Employee Status]      nvarchar(100) null,
+    [Total Compliance]      nvarchar(100) null,
+    [West Campus]           nvarchar(100) null,
+    [Hep B]                 nvarchar(100) null,
+    [Measles]               nvarchar(100) null,
+    [Mumps]                 nvarchar(100) null,
+    [Rubella]                nvarchar(100) null,
+    [Varicella]              nvarchar(100) null,
+    [Tdap]                   nvarchar(100) null,
+    [Full Face Respirator]   nvarchar(100) null,
+    [Standard Respirator]    nvarchar(100) null,
+    [Flu]                    nvarchar(100) null,
+    [TB Health Surveillance] nvarchar(100) null,
+    [TB Annual]              nvarchar(100) null,
+    [TB West Campus]         nvarchar(100) null,
+    [Clinical MRI]           nvarchar(100) null,
+    [Supervisor Email]       nvarchar(100) null
+
+
+
+
+        CONSTRAINT PK_OccHealth_Data PRIMARY KEY (RowId),
+
+);
+CREATE INDEX IX_ONPRC_EHR_COMPLIANCEDB_OccHealth_DATA_CONTAINER ON onprc_ehr_compliancedb.OccHealth_Data (Container);
+
+
+
+CREATE TABLE [onprc_ehr_compliancedb].[OccHealthTemp] (
+    [searchID] [int] IDENTITY(100,1) NOT NULL,
+    [employeeid] [varchar](500) NULL,
+    [requirementname] [varchar](3000) NULL,
+    [completeddate] [smalldatetime] NULL,
+    [comment] [varchar](300) NULL,
+    [createddate] [smalldatetime] NULL,
+    [rowid] [int] NULL
+    ) ON [PRIMARY]
+    GO
+
+CREATE TABLE [onprc_ehr_compliancedb].[OccHealthMasterTemp](
+    [searchID] [int] IDENTITY(100,1) NOT NULL,
+    [employeeid] [varchar](500) NULL,
+    [requirementname] [varchar](3000) NULL,
+    [completeddate] [smalldatetime] NULL,
+    [comment] [varchar](300) NULL,
+    [createddate] [smalldatetime] NULL,
+    [rowid] [int] NULL
+    ) ON [PRIMARY]
+    GO
+
+
+
