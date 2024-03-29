f9d741a0f0e428581271a3e0201dc52588b1286e
Gary Jones <jonesga@ohsu.edu>
2024-03-29 07:22:36 -0700

    Update of SQL Tables and creation of queries to be used to populate the datasets

---
+++ onprc_ehr/resources/queries/onprc_ehr/eIACUCPopulateEIACUCdetails.sql
@@ -0,0 +1,9 @@
+SELECT p.rowid,
+       p.Protocol_Id,
+       BAseProtocol,
+       RevisionNumber,
+       p.Template_OID,
+       p.Protocol_Title,
+       p.PROTOCOL_State,
+       p.PPQ_Numbers
+FROM eIACUCPopulateeIACUCData p
