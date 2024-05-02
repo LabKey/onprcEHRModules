a18de031de9ca70c77da775727fcd25397641eb4
Gary Jones <jonesga@ohsu.edu>
2024-05-02 13:22:16 -0700

    Revision of queries and related xml, also removed qview.xml for NIH Sheets, NIHRedueced Rates and NIH INductrial Rates

--- onprc_billingpublic/resources/queries/onprc_billing_public/NIHIndustryRates.sql
+++ onprc_billingpublic/resources/queries/onprc_billing_public/NIHIndustryRates.sql
@@ -1,26 +1,24 @@
-
-----2024-05-01 update for year 66
--- PARAMETERS (MARGIN FLOAT DEFAULT .25)
+PARAMETERS (MARGIN FLOAT DEFAULT .25, FARATE FLOAT DEFAULT 1)
 Select
-    'Industrial Rate Sheet' as RateSheetSelected,
-    Margin as ParameterSelected,
-n.category,
-n.name,
---UnitCost as CurrentYear,
-(SUM((n.UnitCost/(1-N.subsidy))*(1.91)*(1+MARGIN)))  as CurrentYear,
-(SUM((N.year1/(1-N.subsidy))*(1.91)*(1+MARGIN)) ) as year1,
-(SUM((N.year2/(1-N.subsidy))*(1.91)*(1+MARGIN)) ) as year2,
-(SUM((N.year3/(1-N.subsidy))*(1.91)*(1+MARGIN)) ) as year3,
-(SUM((N.year4/(1-N.subsidy))*(1.91)*(1+MARGIN)) ) as year4,
-(SUM((N.year5/(1-N.subsidy))*(1.91)*(1+MARGIN)) ) as year5,
-(SUM((N.year6/(1-N.subsidy))*(1.91)*(1+MARGIN)) ) as year6,
-(SUM((N.year7/(1-N.subsidy))*(1.91)*(1+MARGIN)) ) as year7,
-(SUM((N.year8/(1-N.subsidy))*(1.91)*(1+MARGIN)) ) as year8,
-n.PostedDate
+    'Industrial Rate' as SheetSelected,
+    Margin  as ParameterSelected,
+    FARate as Parameter2Selected,
+    n.category,
+    n.name,
+    UnitCost,
+    (SUM((n.UnitCost/(1-N.subsidy))*(1+MARGIN))*FARATE )  as CurrentYear,
+    (SUM((N.year1/(1-N.subsidy))*(1+MARGIN))*FARATE ) as year1,
+    (SUM((N.year2/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year2,
+    (SUM((N.year3/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year3,
+    (SUM((N.year4/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year4,
+    (SUM((N.year5/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year5,
+    (SUM((N.year6/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year6,
+    (SUM((N.year7/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year7,
+    (SUM((N.year8/(1-N.subsidy))*(1+MARGIN))*FARATE  ) as year8
+
 
 FROM NIHRateConfig n
 Group by
-n.category,
-n.name,
-n.UnitCost,
-n.postedDate
+    n.category,
+    n.name,
+    unitCost
