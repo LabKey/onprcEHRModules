6d5293be82e99722224408c856b41e9bc890cd20
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-05-24 14:42:17 -0700

    Modified Blood processing reporting program based on newer criteria.

--- onprc_reports/resources/queries/study/processingBloodDraws.sql
+++ onprc_reports/resources/queries/study/processingBloodDraws.sql
@@ -1,20 +1,17 @@
 SELECT
   d.Id,
-  ---g.isU42,
-  case when g.Id != Null then 'U42 = Y'
-       Else
-         'U42 = N'
-  End as isU42,
- Case when g.Id <> null And (s.PCRbloodVol > 0 ) then 2
+  g.isU42,
+
+ Case when (g.isU42 = 'Y'  And (s.PCRbloodVol > 0 )) then 2
    ELSE
     0
  End as PCRBloodVolume,
   s.SRVbloodVol as serologyBloodVol,
- -- g.parentageBloodDrawVol,
- -- g.mhcBloodDrawVol,
- -- g.dnaBloodDrawVol,
- -- g.totalBloodDrawVol as geneticsBloodVol,
-  coalesce(s.srvbloodVol, 0) +  Case when g.Id <> Null And (s.pcrbloodVol > 0 )  then 2
+  g.parentageBloodDrawVol,
+  g.mhcBloodDrawVol,
+  g.dnaBloodDrawVol,
+  g.totalBloodDrawVol as geneticsBloodVol,
+  coalesce(s.srvbloodVol, 0) +  Case when g.isU42 = 'Y' And (s.pcrbloodVol > 0 )  then 2
    ELSE
      0
   End + 0 as totalBloodDrawVol
@@ -22,12 +19,4 @@ SELECT
 FROM study.demographics d
 
 LEFT JOIN study.processingSerology s ON (d.Id = s.Id)
---LEFT JOIN study.processingGeneticsBloodDraws g ON (g.Id = s.Id)
-LEFT JOIN (
-    SELECT
-        a.Id
-        --count(*) as total
-    FROM study.assignment a
-    WHERE a.isActive = true and a.project.name = javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.U42_PROJECT')
-    GROUP BY a.Id
-) g ON (g.Id = d.Id)
+LEFT JOIN study.processingGeneticsBloodDraws g ON (g.Id = s.Id)
