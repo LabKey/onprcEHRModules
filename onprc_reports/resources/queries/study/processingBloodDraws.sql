d48fd04f7e3ab9ab40a36958a4d9db15fe83c47b
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-05-29 15:47:02 -0700

    Modified Blood processing report to include "Compromised SPF Surveillance".

--- onprc_reports/resources/queries/study/processingBloodDraws.sql
+++ onprc_reports/resources/queries/study/processingBloodDraws.sql
@@ -1,8 +1,11 @@
 SELECT
   d.Id,
-  g.isU42,
+--   g.isU42,
+  CASE when a.Id != Null then 'Y'
+  ELSE 'N'
+  END as isU42,
 
- Case when (g.isU42 = 'Y'  And (s.PCRbloodVol > 0 )) then 2
+ Case when (a.Id != nULL  And (s.PCRbloodVol > 0 )) then 2
    ELSE
     0
  End as PCRBloodVolume,
@@ -23,3 +26,10 @@ FROM study.demographics d
 
 LEFT JOIN study.processingSerology s ON (d.Id = s.Id)
 LEFT JOIN study.processingGeneticsBloodDraws g ON (g.Id = s.Id)
+LEFT JOIN (SELECT
+    a.Id
+    --count(*) as total
+FROM study.assignment a
+WHERE a.isActive = true and a.project.name = javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.U42_PROJECT')
+GROUP BY a.Id
+    ) a ON (a.Id = d.Id)
