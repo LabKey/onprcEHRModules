65c954256b31b63ed4c1011e95cd19d9fb705cf6
Gary Jones <jonesga@ohsu.edu>
2024-07-24 13:24:23 -0700

    FIles for Phase 1 of eiACUAC to Protocol.  Updates the eriCIC Data to add Base Prtocol

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.002-24.003.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.002-24.003.sql
@@ -1,4 +1,3 @@
-================================================
 -- Template generated from Template Explorer using:
 -- Create Procedure (New Menu).SQL
 --
@@ -19,7 +18,7 @@ GO
 -- Description:	Iniitial Stage of update of eIACUC to protocol processing
 -- =============================================
 
-CREATE PROCEDURE onprc_ehr.eIACUCBaseProtocol
+ALTER PROCEDURE onprc_ehr.eIACUCBaseProtocol
 
     AS
 BEGIN
@@ -27,17 +26,17 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 --Drop Temporary Table
-DROP TABLE #eIACUCBaseProtocol
+--DROP TABLE #eIACUCBaseProtocol
 -- Insert statements for procedure here
 cREATE tABLE #eIACUCBaseProtocol
 (BaseProtocol varchar(50) Not Null,
- RevisionNumber varchar(20) Not Null,
+ RenewalNumber varchar(20) Not Null,
  Protocol_Id varchar(50) Not Null,
  Last_Modified varchar(30) not null ,
- LatestInstance varchar (30) Not Null);
+ LatestREnewal varchar (30) Not Null);
 
 --Populate the temporary table from query
-Insert INTO #eIACUCBaseProtocol(BaseProtocol,RevisionNumber,Protocol_id,Last_Modified,LatestInstance)
+Insert INTO #eIACUCBaseProtocol(BaseProtocol,RenewalNumber,Protocol_id,Last_Modified,LatestRenewal)
 Select
 
     Case when len(p.protocol_id) > 10 then substring(p.protocol_id, 6,15)
@@ -45,36 +44,30 @@ Select
         End as BaseProtocol,
     Case when len(p.protocol_id) > 10 then substring(p.protocol_id, 1,4)
          else 'Original'
-        End as RevisionNumber,
+        End as RenewalNumber,
     protocol_id,
     Last_Modified,
-    ' ' as LatestInstance
+    ' ' as LatestRenewal
 
 from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS p
 
 Update p1
 Set p1.BaseProtocol = e.BaseProtocol
 
-    from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS p1 join #eIACUCUpdate e on p1.protocol_id = e.Protocol_Id
+    from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS p1 join #eIACUCBaseProtocol e on p1.protocol_id = e.Protocol_Id
 
 Update p1
-Set p1.RevisionNumber = e.RevisionNumber
+Set p1.ReNewalNumber = e.ReNewalNumber
 
-    from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS p1 join #eIACUCUpdate e on p1.protocol_id = e.Protocol_Id
+    from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS p1 join #eIACUCBaseProtocol e on p1.protocol_id = e.Protocol_Id
 
+Update p1
+Set p1.LatestRenewal = e.LatestRenewal
+
+    from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS p1 join #eIACUCBaseProtocol e on p1.protocol_id = e.Protocol_Id
 
 
 --Now will limit the records to be inserted to most recent record
-
-
-update p
-Set p.updateStatus = 'Update'
-
-    from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS p
-where (p.BaseProtocol is not null and p.last_Modified = (Select Max(p1.Last_Modified) from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS p1 where p1.BaseProtocol = p.BaseProtocol))
-
-
-
 
 
 END
