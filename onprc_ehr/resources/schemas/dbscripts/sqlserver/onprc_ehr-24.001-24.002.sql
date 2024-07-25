c6ae7ee279b01b0eef9d2f1b89a38b021aa622c4
Gary Jones <jonesga@ohsu.edu>
2024-07-25 15:13:25 -0700

    Added new fields to eIACUCProtocol for Base Protocol and Renewal Number

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.001-24.002.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.001-24.002.sql
@@ -1,3 +1,4 @@
+-- Add three fields to Onprc_EHR to processing with ehr.protoocol
 ALTER TABLE onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS ADD BaseProtocol varchar(75);
 ALTER TABLE onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS ADD RenewalNumber varchar(75);
 ALTER TABLE onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS ADD LatestRenewal Bit;
--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * Upodate 2024/07/25
  */
 package org.labkey.onprc_ehr;
 
