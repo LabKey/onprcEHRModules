81a4848415a0666f67620436225ac5940a9c3b2d
Gary Jones <jonesga@ohsu.edu>
2024-08-02 09:10:50 -0700

    Updatge to the Stored Proecdueer to properly mark Latest Update

--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -127,7 +127,7 @@ public class ONPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 24.003;
+        return 24.004;
     }
 
     @Override
