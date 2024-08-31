27d3b0c030535e6832a077dff4429ddc34976032
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-08-30 19:35:39 -0700

    Modified TB Test corrected issues with orphan task ids.

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.015-23.016.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.015-23.016.sql
@@ -72,7 +72,8 @@ Where a.participantid not in (select b.participantid  from studydataset.c6d171_c
                               where a.participantid = b.participantid
                                And cast(b.date as date)  = dateadd(day,3,cast(a.date as date))
                                And b.category = 'TB TST Score (72 hr)'
-                                And a.created >=   dateadd(day, -1, cast(getdate() as date)) )
+                                And a.created >=   dateadd(day, -1, cast(getdate() as date))
+                                 And a.type = 'Procedure' And a.qcstate = 18 And procedureid = 802   )
 
   And a.type = 'Procedure' And a.qcstate = 18 And procedureid = 802         -----'TB Test Intradermal'
   And a.created >=   dateadd(day, -1, cast(getdate() as date))
