e9ddb520b0c34fe41a6bb92960117de442d7c997
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-09-12 11:08:23 -0700

    Modified TB testing storedprocedure program

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.015-23.016.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-23.015-23.016.sql
@@ -6,7 +6,7 @@
 /*
 **
 ** 	Created by
-**      R. Blasa   6-5-2024                 A  Program Process that reviews all TB Test entries on a given date, and creates a
+**      R. Blasa   6-5-2024      A  Program Process that reviews all TB Test entries on a given date, and creates a
 **                                               new TB Test Clinical Observation record based on
 **                                               having the same monkey id, date, and to be assigned to a Data Admin for reviews.
 **
@@ -72,11 +72,11 @@ Where a.participantid not in (select b.participantid  from studydataset.c6d171_c
                               where a.participantid = b.participantid
                                And cast(b.date as date)  = dateadd(day,3,cast(a.date as date))
                                And b.category = 'TB TST Score (72 hr)'
-                                And a.created >=   dateadd(day, -1, cast(getdate() as date))
+                                And a.created >=  cast(getdate() as date)
                                  And a.type = 'Procedure' And a.qcstate = 18 And procedureid = 802   )
 
   And a.type = 'Procedure' And a.qcstate = 18 And procedureid = 802         -----'TB Test Intradermal'
-  And a.created >=   dateadd(day, -1, cast(getdate() as date))
+  And a.created >=  cast(getdate() as date)
 And a.participantid in ( select k.participantid from studydataset.c6d203_demographics k
     where k.calculated_status = 'alive')
 
