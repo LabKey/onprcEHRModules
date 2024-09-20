b15e0e5836bc96bee226b41006757247e32dd573
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-09-19 18:56:08 -0700

    Modified Surgery input forms to include Surgery Procedure unique identifier.

--- onprc_ehr/resources/web/onprc_ehr/model/sources/Surgery.js
+++ onprc_ehr/resources/web/onprc_ehr/model/sources/Surgery.js
@@ -1,133 +1,213 @@
-/*
- * Copyright (c) 2013-2019 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
+    /*
+     * Copyright (c) 2013-2019 LabKey Corporation
+     *
+     * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+     */
 
-// Created: 4-20-2021  R. Blasa
-EHR.model.DataModelManager.registerMetadata('onprc_Surgery', {
-    allQueries: {
-        performedby: {
-            allowBlank: true
-        }
-    },
-    byQuery: {
-        'onprc_billing.miscCharges': {
-            chargeId: {
-                lookup: {
-                    filterArray: [
-                        LABKEY.Filter.create('active', true, LABKEY.Filter.Types.EQUAL),
-                        LABKEY.Filter.create('category', 'Lease Fee', LABKEY.Filter.Types.NEQ),
-                        LABKEY.Filter.create('category', 'Animal Per Diem', LABKEY.Filter.Types.NEQ),
-                        LABKEY.Filter.create('category', 'Small Animal Per Diem', LABKEY.Filter.Types.NEQ),
-                        LABKEY.Filter.create('category', 'Timed Mated Breeders', LABKEY.Filter.Types.NEQ)
-                    ]
-                }
-            },
-            chargetype: {
-                //NOTE: this will be inherited from the encounters record, so we dont want a default
-                //defaultValue: 'DCM: Surgery Services',
-                allowBlank: false
-            }
-        },
-        'study.treatment_order': {
-            category: {
-                shownInGrid: true,
-                defaultValue: 'Surgical',
-                allowBlank: false
-            }
-
-        },
-        'study.drug': {
-            enddate: {
-                hidden: false
-            },
-            category: {
-                shownInGrid: true,
-                hidden: false,
-                defaultValue: 'Surgical'
-            },
-            reason: {
-                defaultValue: 'Procedure'
-            },
-            chargetype: {
-                //NOTE: this will be inherited from the encounters record, so we dont want a default
-                //defaultValue: 'DCM: Surgery Services',
-                allowBlank: false
-            }
-        },
-        'study.encounters': {
-            type: {
-                defaultValue: 'Surgery',
-                hidden: true
-            },
-            title: {
-                hidden: true
-            },
-            caseno: {
-                hidden: true
-            },
-            procedureid: {
-                lookup: {
-                    filterArray: [
-                        LABKEY.Filter.create('category', 'Surgery;Procedure', LABKEY.Filter.Types.EQUALS_ONE_OF),
-                        LABKEY.Filter.create('active', true, LABKEY.Filter.Types.EQUAL)
-                    ]
-                }
-            },
+    // Created: 4-20-2021  R. Blasa
+    EHR.model.DataModelManager.registerMetadata('onprc_Surgery', {
+        allQueries: {
             performedby: {
-                hidden: true
+                allowBlank: true
+            }
+        },
+        byQuery: {
+            'onprc_billing.miscCharges': {
+                chargeId: {
+                    lookup: {
+                        filterArray: [
+                            LABKEY.Filter.create('active', true, LABKEY.Filter.Types.EQUAL),
+                            LABKEY.Filter.create('category', 'Lease Fee', LABKEY.Filter.Types.NEQ),
+                            LABKEY.Filter.create('category', 'Animal Per Diem', LABKEY.Filter.Types.NEQ),
+                            LABKEY.Filter.create('category', 'Small Animal Per Diem', LABKEY.Filter.Types.NEQ),
+                            LABKEY.Filter.create('category', 'Timed Mated Breeders', LABKEY.Filter.Types.NEQ)
+                        ]
+                    }
+                },
+                chargetype: {
+                    //NOTE: this will be inherited from the encounters record, so we dont want a default
+                    //defaultValue: 'DCM: Surgery Services',
+                    allowBlank: false
+                },
+                 parentid: {
+                       shownInGrid: true,
+                       hidden: false,
+                       header: 'Procedure Link',
+                       columnConfig: {
+                                    width: 250
+                       }
+                }
             },
-            remark: {
-                hidden: true
+            'study.treatment_order': {
+                category: {
+                    shownInGrid: true,
+                    defaultValue: 'Surgical',
+                    allowBlank: false
+
+               },
+              parentid: {
+                  shownInGrid: true,
+                  hidden: false,
+                  header: 'Procedure Link',
+                  columnConfig: {
+                           width: 250
+                     }
+                  }
             },
-            chargetype: {
-                allowBlank: false
+
+           'study.blood': {
+               parentid: {
+                  shownInGrid: true,
+                  hidden: false,
+                  header: 'Procedure Link',
+                  columnConfig: {
+                           width: 250
+                     }
+               }
+           },
+            'study.drug': {
+                enddate: {
+                    hidden: false
+                },
+                category: {
+                    shownInGrid: true,
+                    hidden: false,
+                    defaultValue: 'Surgical'
+                },
+                reason: {
+                    defaultValue: 'Procedure'
+                },
+                parentid: {
+                     shownInGrid: true,
+                     hidden: false,
+                     header: 'Procedure Link',
+                     columnConfig: {
+                              width: 250
+                       }
+                  },
+                chargetype: {
+                    //NOTE: this will be inherited from the encounters record, so we dont want a default
+                    //defaultValue: 'DCM: Surgery Services',
+                    allowBlank: false
+                }
             },
-            assistingstaff: {
-                hidden: false,
-                allowBlank: true //will be handled in trigger script
-            },
-            enddate: {
-                editorConfig: {
-                    getDefaultDate: function(){
-                        var rec = EHR.DataEntryUtils.getBoundRecord(this);
-                        if (rec){
-                            if (rec.get('date')){
-                                return rec.get('date');
+            'study.encounters': {
+                type: {
+                    defaultValue: 'Surgery',
+                    hidden: true
+                },
+                title: {
+                    hidden: true
+                },
+                caseno: {
+                    hidden: true
+                },
+                procedureid: {
+                    lookup: {
+                        filterArray: [
+                            LABKEY.Filter.create('category', 'Surgery;Procedure', LABKEY.Filter.Types.EQUALS_ONE_OF),
+                            LABKEY.Filter.create('active', true, LABKEY.Filter.Types.EQUAL)
+                        ]
+                    }
+                },
+                performedby: {
+                    hidden: true
+                },
+                remark: {
+                    hidden: true
+                },
+                chargetype: {
+                    allowBlank: false
+                },
+                objectid: {
+                     shownInGrid: true,
+                     hidden: false,
+                     header: 'Procedure Link',
+                     columnConfig: {
+                          width: 250
+                     }
+                 },
+                assistingstaff: {
+                    hidden: false,
+                    allowBlank: true //will be handled in trigger script
+                },
+                enddate: {
+                    editorConfig: {
+                        getDefaultDate: function(){
+                            var rec = EHR.DataEntryUtils.getBoundRecord(this);
+                            if (rec){
+                                if (rec.get('date')){
+                                    return rec.get('date');
+                                }
                             }
                         }
                     }
-                }
-            }
-        },
-        'ehr.snomed_tags': {
-            code: {
-                editorConfig: {
-                    xtype: 'ehr-snomedcombo',
-                    defaultSubset: 'Diagnostic Codes'
+
                 }
             },
-            set_number: {
-                hidden: true
-            },
-            sort: {
-                hidden: true
-            }
-        },
-        'ehr.encounter_participants': {
-            comment: {
-                hidden: false,
-                header: 'Remarks',
-                columnConfig: {
-                    width: 300
+            'ehr.snomed_tags': {
+                code: {
+                    editorConfig: {
+                        xtype: 'ehr-snomedcombo',
+                        defaultSubset: 'Diagnostic Codes'
+                    }
+                },
+                set_number: {
+                    hidden: true
+                },
+                parentid: {
+                 shownInGrid: true,
+                 hidden: false,
+                 header: 'Procedure Link',
+                 columnConfig: {
+                           width: 250
+                     }
+                },
+                sort: {
+                    hidden: true
                 }
-            }
-        },
-        'ehr.encounter_summaries': {
-            category: {
-                defaultValue: 'Narrative'
+            },
+
+          'ehr.encounter_participants': {
+                comment: {
+                    hidden: false,
+                    header: 'Remarks',
+                    columnConfig: {
+                        width: 300
+                      }
+                },
+                parentid: {
+                         shownInGrid: true,
+                         hidden: false,
+                         header: 'Procedure Link',
+                         columnConfig: {
+                                width: 250
+                          }
+                 }
+            },
+
+         'study.weight': {
+                 parentid: {
+                         shownInGrid: true,
+                         hidden: false,
+                         header: 'Procedure Link',
+                         columnConfig: {
+                                width: 250
+                          }
+                     }
+                },
+            'ehr.encounter_summaries': {
+                category: {
+                    defaultValue: 'Narrative'
+                },
+                parentid: {
+                         shownInGrid: true,
+                         hidden: false,
+                         header: 'Procedure Link',
+                         columnConfig: {
+                                  width: 250
+                            }
+                   }
             }
         }
-    }
-});
+    });
