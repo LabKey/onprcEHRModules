87c3e3b51402b5ba33f7f18457e4a44e5debb5f1
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-10-09 10:42:34 -0700

    Modified Surgical Rounds validation process.

--- onprc_ehr/resources/web/onprc_ehr/grid/SurgicalRoundsRemarksGridPanel.js
+++ onprc_ehr/resources/web/onprc_ehr/grid/SurgicalRoundsRemarksGridPanel.js
@@ -86,18 +86,18 @@ Ext4.define('ONPRC_EHR.grid.SurgicalRoundsRemarksGridPanel', {
                         }
 
                         if (!Ext4.isEmpty(r.get('inflammation'))){
-                                    line += '; [Inflammation]-->  ' + r.get('inflammation');
+                              line += '; [Inflammation]-->  ' + r.get('inflammation');
                         }
 
                         if (!Ext4.isEmpty(r.get('bruising'))){
-                                    line += '; [Bruising]-->  ' +r.get('bruising');
+                              line += '; [Bruising]-->  ' +r.get('bruising');
                         }
                         if (!Ext4.isEmpty(r.get('other'))){
-                                    line += '; [Other]-->  ' +r.get('other');
+                             line += '; [Other]-->  ' + r.get('other');
                         }
 
                         if (r.get('remark')){
-                            line += '.  ' + r.get('remark');
+                            line +=  '; [Remarks]--> ' + r.get('remark');
                         }
 
                         if (!r.get('remark') && Ext4.isEmpty(r.get('observation'))){
