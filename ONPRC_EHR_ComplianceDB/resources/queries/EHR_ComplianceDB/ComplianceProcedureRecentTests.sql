c1d20516d19226371d084abdbf46fa598af4ce7d
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-10-11 14:29:08 -0700

    Modified Compliance query to remove sql reusltset errors

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceProcedureRecentTests.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/ComplianceProcedureRecentTests.sql
@@ -24,7 +24,7 @@ select b.requirementname,
 
        (Select group_concat(distinct yy.comment, chr(10))  from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid )
                                                                                      And  yy.requirementname= b.requirementname and yy.employeeid= a.employeeid   ) as comment,
-       (Select yy.snooze_date from completiondates yy where yy.snooze_date in (select max(zz.snooze_date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid )
+       (Select group_concat(distinct yy.snooze_date , chr(10)) from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= b.requirementname and zz.employeeid= a.employeeid )
                                                                                      And  yy.requirementname= b.requirementname and yy.employeeid= a.employeeid   ) as snooze_date,
 
        CAST(
@@ -82,7 +82,7 @@ select a.requirementname,
        (Select group_concat(distinct yy.comment, chr(10))  from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= a.requirementname and zz.employeeid= a.employeeid )
                                                                                      And  yy.requirementname= a.requirementname and yy.employeeid= a.employeeid   ) as comment,
 
-       (Select yy.snooze_date  from completiondates yy where yy.snooze_date in (select max(zz.snooze_date) from completiondates zz where zz.requirementname= a.requirementname and zz.employeeid= a.employeeid )
+       (Select group_concat(distinct yy.snooze_date , chr(10)) from completiondates yy where yy.date in (select max(zz.date) from completiondates zz where zz.requirementname= a.requirementname and zz.employeeid= a.employeeid )
                                                                                      And  yy.requirementname= a.requirementname and yy.employeeid= a.employeeid   ) as snooze_date,
 
        CAST(
