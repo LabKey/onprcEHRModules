aa3577c96edd2e75079c24f15f501d7fdf6077b0
kollil <kollil@ohsu.edu>
2024-10-17 19:27:19 -0700

    Updated stored proc to add expected, requested dates and numordered fields.

--- sla/resources/schemas/dbscripts/sqlserver/sla-23.002-23.003.sql
+++ sla/resources/schemas/dbscripts/sqlserver/sla-23.002-23.003.sql
@@ -65,26 +65,30 @@ CREATE PROCEDURE  [onprc_ehr].[SLAWeaningDataTransfer]
 AS
 
 DECLARE
-    @WCount			Int,
-    @project		varchar(100),
-    @center_project int,
-    @PI				varchar(250),
-    @investigaorid	int,
-    @vendorlocation varchar(250),
-    @DOB			smalldatetime,
-    @DOM			smalldatetime,
-    @strain			varchar(250),
-    @sex			varchar(100),
-    @species		varchar(100),
-    @numAlive		int,
-    @numDead		int,
-    @totalPups		int,
-    @dateofTransfer smalldatetime,
-    @alias			varchar(100),
-    @purchaseId		entityid,
-    @ageindays		int,
-    @counter		int,
-	@weaning_rowid	int
+    @WCount			        Int,
+    @project		        varchar(100),
+    @center_project         int,
+    @PI				        varchar(250),
+    @investigaorid	        int,
+    @vendorlocation         varchar(250),
+    @DOB			        smalldatetime,
+    @DOM			        smalldatetime,
+    @strain			        varchar(250),
+    @sex			        varchar(100),
+    @species		        varchar(100),
+    @numAlive		        int,
+    @numDead		        int,
+    @totalPups		        int,
+    @dateofTransfer         smalldatetime,
+    @alias			        varchar(100),
+    @purchaseId		        entityid,
+    @ageindays		        int,
+    @counter		        int,
+	@weaning_rowid	        int,
+    @RequestedArrivalDate   smalldatetime,
+    @ExpectedArrivalDate    smalldatetime,
+    @slaDOB                 smalldatetime,
+    @numAnimalsOrdered      int
 
 BEGIN
     --Check if any rodents age is 21 days and above and not transferred into SLA tables
@@ -125,7 +129,9 @@ BEGIN
 
             --Get the age of the rodent and weaning_rowid, species, sex, strain and vendorlocation
             Select @ageindays = DateDiff(dd, DOB, GETDATE()),@weaning_rowid = weaning_rowid,
-                   @species = species, @sex = sex, @strain = strain, @vendorlocation = vendorlocation
+                   @species = species, @sex = sex, @strain = strain, @vendorlocation = vendorlocation,
+                   @RequestedArrivalDate = DateAdd(dd, 21, DOB), @ExpectedArrivalDate = DateAdd(dd, 21, DOB),
+                   @slaDOB = DOB, @numAnimalsOrdered = numAlive
             From sla.TempWeaning Where rowid = @counter
 
             --Insert weaning data into sla.purchase table as a pending order
@@ -133,7 +139,7 @@ BEGIN
             (project, account, requestorid, vendorid, hazardslist, dobrequired, comments, confirmationnum, housingconfirmed,
              iacucconfirmed, requestdate, orderdate, orderedby, objectid, container, createdby, created, modifiedby, modified, DARComments, VendorContact)
             Select @center_project, @alias ,'7B3F1ED1-4CD9-4D9A-AFF4-FE0618D49C4B','E1EE1B64-B7BE-1035-BFC4-5107380AE41E','',0,'',null,null,null,null,null,'',NEWID(),
-                   '4831D09C-4169-1034-BAD2-5107380A9819',1294,GETDATE(),null,null,'',''
+            '4831D09C-4169-1034-BAD2-5107380A9819',1294,GETDATE(),null,null,'',''
 
             --Get the newly created purchaseid from sla.purchase
             Select top 1 @purchaseid = objectid From sla.purchase order by created desc
@@ -144,9 +150,9 @@ BEGIN
             totalcost, housingInstructions, requestedarrivaldate, expectedarrivaldate, receiveddate, receivedby, cancelledby, datecancelled,
             objectid, container, createdby, created, modifiedby, modified, sla_DOB, vendorLocation)
             VALUES
-            (@purchaseId, @species, CONVERT(VARCHAR, @ageindays) + ' days','','','', @sex, @strain, '',null,null,null,'','',
-            '','',null,null,null,'','',null,
-            NewId(),'4831D09C-4169-1034-BAD2-5107380A9819',1003,GETDATE(),null,null,null,@vendorLocation)
+            (@purchaseId, @species, CONVERT(VARCHAR, @ageindays) + ' days','','','', @sex, @strain, '',@numAnimalsOrdered,null,null,'','',
+            '','',@RequestedArrivalDate,@ExpectedArrivalDate,null,'','',null,
+            NewId(),'4831D09C-4169-1034-BAD2-5107380A9819',1003,GETDATE(),null,null,@slaDOB,@vendorLocation)
 
             --Update the sla.weaning row with the date of transfer date set for the transferred weaning row
             Update sla.weaning
