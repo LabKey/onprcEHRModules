7917db87f4d589beb908b7019b09cd1531ad2e0a
jonesgaohsu <jonesga@ohsu.edu>
2024-10-30 06:00:04 -0700

    Latest Change is now functional in Dev Instance pushing to Prime sandbox for review

--- onprc_billing/resources/web/onprc_billing/window/ReverseChargeWindow.js
+++ onprc_billing/resources/web/onprc_billing/window/ReverseChargeWindow.js
@@ -2,6 +2,8 @@
  * Copyright (c) 2013 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ * Modified jonesga October 30, 2024
+ * **Per Finance Issue Tracker 11380 added debit Aklias add function to allow change of debit alias when no center project is associated
  */
 Ext4.define('ONPRC_Billing.window.ReverseChargeWindow', {
     extend: 'Ext.window.Window',
