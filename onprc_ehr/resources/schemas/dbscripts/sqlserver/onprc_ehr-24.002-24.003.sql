e802e0f13da289a10354baefadbfc912a41625dd
jonesgaohsu <jonesga@ohsu.edu>
2024-11-26 14:12:21 -0700

    Update of first Query to return only latgestest record using Approval Date

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.002-24.003.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.002-24.003.sql
@@ -1,3 +1,6 @@
+--Created 2024-11-26
+--Adding Fields to Table
+
 ALTER TABLE onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS ADD  BaseProtocol varchar(25);
 ALTER TABLE onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS ADD  RevisionNumber varchar(25);
 ALTER TABLE onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS ADD  RN int;
