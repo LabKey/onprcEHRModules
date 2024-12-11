852e7c5d3bae504e346bc3b835318c942f8e1e90
brentlogan <gbrentlogan@gmail.com>
2024-12-10 16:28:02 -0800

    Add Vet Assignment Tools webpart

--- onprc_ehr/resources/queries/study/vetAssignment_demographics.sql
+++ onprc_ehr/resources/queries/study/vetAssignment_demographics.sql
@@ -10,9 +10,6 @@ Notes:
   * Left joins are needed for CMUcases and assignedProject as those tables
     might not have records for an animal ID. Left joins are used for all
 	other joins to be resilient against missing data.
-
-Future potential modifications:
-  * Limit open cases to those that are not inactive	after confirming w/ Heather
  */
 
 SELECT demographics.id
@@ -28,15 +25,11 @@ SELECT demographics.id
      , demographics.gender
      , demographics.species
      , demographics.history
-FROM Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.animal AS nhp
-LEFT JOIN Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.ClinicalCases_Open AS CMUcases
-ON CMUcases.id = nhp.id
-    LEFT JOIN Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.housing AS housing
-    ON housing.id = nhp.id
-    LEFT JOIN Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.demographics AS demographics
-    ON demographics.id = nhp.id
-    LEFT JOIN Site.{ substitutePath moduleProperty('EHR', 'EHRStudyContainer') }.study.vetAssignment_projects AS assignedProject
-    ON assignedProject.id = nhp.id
+FROM study.animal AS nhp
+LEFT JOIN study.ClinicalCases_Open AS CMUcases ON CMUcases.id = nhp.id
+LEFT JOIN study.housing AS housing ON housing.id = nhp.id
+LEFT JOIN study.demographics AS demographics ON demographics.id = nhp.id
+LEFT JOIN study.vetAssignment_projects AS assignedProject ON assignedProject.id = nhp.id
 WHERE demographics.Calculated_Status = 'Alive'
   AND nhp.id NOT LIKE '[A-Z]%'
   AND housing.enddate IS NULL
--- onprc_ehr/resources/queries/study/vetAssignment_projects.sql
+++ onprc_ehr/resources/queries/study/vetAssignment_projects.sql
@@ -30,9 +30,9 @@ SELECT Id
      , enddate
      , project.use_category AS projectType
 FROM study.assignment
-WHERE Curdate() >= date  /* The assignment has started */
+WHERE Curdate() >= date                         -- The assignment has started
   AND (
-    enddate IS NULL            /* Not a day lease or scheduled end of assignment */
-    OR Curdate() < enddate   /* The assignment hasn't yet ended */
-    OR (Curdate() = enddate AND enddate = date) /* 1-day day lease where date = enddate */
+    enddate IS NULL                             -- Not a day lease or scheduled end of assignment
+    OR Curdate() < enddate                      -- The assignment hasn't yet ended
+    OR (Curdate() = enddate AND enddate = date) -- 1-day day lease where date = enddate
   )
