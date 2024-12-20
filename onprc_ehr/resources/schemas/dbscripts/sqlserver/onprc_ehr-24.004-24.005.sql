4d042e6d8ed3084e9069eab56249946c7cd26d10
jonesgaohsu <jonesga@ohsu.edu>
2024-12-20 09:14:13 -0700

    Upd[atre of Stored Proecure to End Date Protocols

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.004-24.005.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.004-24.005.sql
@@ -1,31 +1,14 @@
-/*
-2024-12-13 this is an update based on recent changes from Brent
-Explanation of process
-ApprovedProtocols CTE:
-
-This Common Table Expression (CTE) selects the BaseProtocol and the maximum Approval_Date for protocols that are in the states 'expired', 'terminated', or 'withdrawn'.
-It groups the results by BaseProtocol to ensure each base protocol has only one entry with the latest approval date.
-DistinctProtocols CTE:
-
-This CTE selects distinct protocol records from onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS that match the base protocols and approval dates identified in the ApprovedProtocols CTE.
-It joins the onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS table with the ApprovedProtocols CTE on BaseProtocol and Approval_Date.
-The result is a set of distinct protocols with their latest approval dates and other relevant details.
-Select Statement:
-
-This part of the procedure selects protocols from the ehr.protocol table that need to be updated.
-It joins the ehr.protocol table with the DistinctProtocols CTE on external_id and BaseProtocol.
-It filters the results to include only those protocols where enddate is NULL.
-Update Statement:
-
-This statement updates the enddate and contacts fields in the ehr.protocol table for the protocols identified in the previous step.
-It sets the enddate to the Approval_Date from the ExpiredProtocol and updates the contacts field with a message indicating the protocol's status.
-In summary, the procedure identifies protocols that have expired, terminated, or withdrawn statuses, finds the latest approval date for each base protocol, and updates the enddate and contacts fields in the ehr.protocol table accordingly. This ensures that the protocol records are up-to-date with their current status.
-
-*/
-CREATE PROCEDURE onprc_ehr.ExpiredProtocolUpdate
+USE [Labkey_GJ]
+GO
+/****** Object:  StoredProcedure [onprc_ehr].[ExpiredProtocolUpdate]    Script Date: 12/20/2024 9:09:09 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [onprc_ehr].[ExpiredProtocolUpdate]
     AS
 BEGIN
-    -- Create a CTE to get the latest approval date for each base protocol
+
 WITH ApprovedProtocols AS (
     SELECT
         BaseProtocol,
@@ -33,12 +16,12 @@ WITH ApprovedProtocols AS (
     FROM
         onprc_ehr.eIACUC_PRIME_VIEW_PROTOCOLS
     WHERE
-        Protocol_State IN ('expired', 'terminated', 'withdrawn')
+        Protocol_State IN ('approved','expired', 'terminated', 'withdrawn')
     GROUP BY
         BaseProtocol
 ),
 
-     -- Create a CTE to get distinct protocols with the latest approval date
+
      DistinctProtocols AS (
          SELECT DISTINCT
              p.rowID,
@@ -55,29 +38,32 @@ WITH ApprovedProtocols AS (
                                 AND p.Approval_Date = ap.maxApprovalDate
      ),
 
--- Select protocols that need to be updated
-     ExpiredProtocol as
-         (SELECT
-              p.Protocol,
-              p.external_Id,
-              d.BaseProtocol,
-              d.RevisionNumber,
-              d.Protocol_State,
-              p.enddate
-          FROM
-              ehr.protocol p
-                  INNER JOIN DistinctProtocols d
-                             ON p.external_id = d.BaseProtocol
-          WHERE
-              p.enddate IS NULL),
+     ExpiredProtocol as (
+         SELECT
+             p.Protocol,
+             p.external_Id,
+             d.BaseProtocol,
+             d.RevisionNumber,
+             d.Protocol_State,
+             p.enddate,
+             Case
+                 when d.approval_date >= d.three_year_Expiration then d.approval_Date
+                 When d.three_Year_Expiration <= d.Last_Modified then d.last_modified
+                 End as LatestDate
+         FROM
+             ehr.protocol p
+                 INNER JOIN DistinctProtocols d
+                            ON p.external_id = d.BaseProtocol
+         WHERE
+             p.enddate IS NULL)
 
--- Update the end date and contacts for expired protocols
-     UPDATE e
+
+UPDATE e
 SET
-    e.enddate = p.Approval_Date,
+    e.enddate =p.LatestDate,
     e.contacts = 'Protocol enddated based on eiACUC Status ' + p.Protocol_State
-FROM
-    ehr.protocol e
+    FROM
+        ehr.protocol e
     INNER JOIN ExpiredProtocol p
 ON e.external_id = p.BaseProtocol;
 END;
