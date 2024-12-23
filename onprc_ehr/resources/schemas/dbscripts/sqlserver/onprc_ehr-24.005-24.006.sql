a3793151e7ee2bd2a4294ed165601ef515760025
jonesgaohsu <jonesga@ohsu.edu>
2024-12-23 08:37:01 -0700

    Modification the Storedc Proec ure was missing the end statment was erroring out

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.005-24.006.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.005-24.006.sql
@@ -41,9 +41,10 @@ WITH ApprovedProtocols AS (
         ,
      ExpiredProtocol as (
          Select d.*,p.protocol,p.enddate from DistinctProtocols d inner join ehr.protocol p on d.BaseProtocol = p.external_ID
-         where (d.Protocol_StaTe != 'Approved' and p.enddate is Null))
+         where (d.Protocol_Stae != 'Approved' and p.enddate is Null))
 
 Update p
 Set p.enddate = getDate() , p.contacts = 'EndDated based on Protocol_State ' + e.PROTOCOL_State
 
     from ehr.protocol p inner join expiredProtocol e on p.external_id = e.BaseProtocol
+End
