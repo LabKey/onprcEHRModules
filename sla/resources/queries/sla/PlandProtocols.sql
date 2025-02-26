6da47eb05b420ec7ffca0efb2eafe6aa0f3da3a5
kollil <kollil@ohsu.edu>
2025-02-26 12:31:45 -0800

    Removed the breeding info from the protocol selection drop down list.

--- sla/resources/queries/sla/PlandProtocols.sql
+++ sla/resources/queries/sla/PlandProtocols.sql
@@ -2,7 +2,6 @@
 Changed by Kolli - 4/20/2022, Added Linked schema template references
 Changed by Kolli - 2/14/2025, Removed the Breeding info from the protocol field. EHR Issue# 11797
 */
-
 SELECT a.project as ProjectID,
        aa.species,
        a.account as Alias,
@@ -30,22 +29,22 @@ FROM publicehr.project a
 WHERE
 -- filter based on the current date compared with the start and end dates
     (
-        (aa.StartDate IS NOT NULL AND aa.EndDate IS NULL AND now() > aa.StartDate) OR
-        (aa.StartDate IS NULL AND aa.EndDate IS NOT NULL AND now() < aa.EndDate) OR
-        (now() between aa.StartDate AND aa.EndDate)
-        )
+    (aa.StartDate IS NOT NULL AND aa.EndDate IS NULL AND now() > aa.StartDate) OR
+    (aa.StartDate IS NULL AND aa.EndDate IS NOT NULL AND now() < aa.EndDate) OR
+    (now() between aa.StartDate AND aa.EndDate)
+    )
   AND
 --Check for the project enddate. Added by LK on 1/16/2019
     (now() between a.StartDate AND a.EndDate)
 -- and filtered based on dataAccess for the given user
   AND
     (
-        (SELECT max(rowid) as expr FROM financepublic.dataAccess da
+    (SELECT max(rowid) as expr FROM financepublic.dataAccess da
 -- current logged in user is the dataAccess user
-         WHERE isMemberOf(da.userid)
+     WHERE isMemberOf(da.userid)
 -- has access to all data
-           AND (da.allData = true
+       AND (da.allData = true
 -- has access to the specified investigatorId and the specified project (if applicable)
-             OR (da.investigatorId = i.rowId AND (da.project IS NULL OR da.project = a.project)))
-        ) IS NOT NULL
-        )
+         OR (da.investigatorId = i.rowId AND (da.project IS NULL OR da.project = a.project)))
+    ) IS NOT NULL
+    )
