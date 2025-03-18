c0a1b42e99ff4dbfa4c6e5b1cb54a978b3803e8c
Adam Rauch <adam@labkey.com>
2025-03-18 14:16:06 -0700

    Remove unused methods from DisplayColumn, refactor (#1286)

--- onprc_ehr/src/org/labkey/onprc_ehr/table/ONPRC_EHRCustomizer.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/table/ONPRC_EHRCustomizer.java
@@ -61,6 +61,7 @@ import org.labkey.api.study.Study;
 import org.labkey.api.study.StudyService;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.view.HttpView;
+import org.labkey.api.writer.HtmlWriter;
 import org.labkey.onprc_ehr.ONPRC_EHRManager;
 import org.labkey.onprc_ehr.ONPRC_EHRModule;
 
@@ -1810,11 +1811,11 @@ public class ONPRC_EHRCustomizer extends AbstractTableCustomizer
                     return new DataColumn(colInfo){
 
                         @Override
-                        public void renderGridCellContents(RenderContext ctx, Writer out) throws IOException
+                        public void renderGridCellContents(RenderContext ctx, Writer oldWriter, HtmlWriter out) throws IOException
                         {
                             String runId = (String)ctx.get(new FieldKey(getBoundColumn().getFieldKey().getParent(), "runIdPLT"));
                             String id = (String)ctx.get(new FieldKey(getBoundColumn().getFieldKey().getParent(), "Id"));
-                            out.write("<span style=\"white-space:nowrap\"><a class=\"labkey-text-link srg-chk-lst\" data-runid=\"" + PageFlowUtil.filter(runId) + "\" data-id=\"" + PageFlowUtil.filter(id) + "\">" + getFormattedHtml(ctx) + "</a></span>");
+                            oldWriter.write("<span style=\"white-space:nowrap\"><a class=\"labkey-text-link srg-chk-lst\" data-runid=\"" + PageFlowUtil.filter(runId) + "\" data-id=\"" + PageFlowUtil.filter(id) + "\">" + getFormattedHtml(ctx) + "</a></span>");
                             if (!_surgeryChecklistClickHandlerAdded)
                             {
                                 HttpView.currentPageConfig().addHandlerForQuerySelector("a.srg-chk-lst", "click", "EHR.panel.LabworkSummaryPanel.showRunSummary(this.attributes.getNamedItem('data-runid').value, this.attributes.getNamedItem('data-id').value, this);");
@@ -1846,11 +1847,11 @@ public class ONPRC_EHRCustomizer extends AbstractTableCustomizer
                     {
 
                         @Override
-                        public void renderGridCellContents(RenderContext ctx, Writer out) throws IOException
+                        public void renderGridCellContents(RenderContext ctx, Writer oldWriter, HtmlWriter out) throws IOException
                         {
                             String runId = (String) ctx.get(new FieldKey(getBoundColumn().getFieldKey().getParent(), "runIdHCT"));
                             String id = (String) ctx.get(new FieldKey(getBoundColumn().getFieldKey().getParent(), "Id"));
-                            out.write("<span style=\"white-space:nowrap\"><a class=\"labkey-text-link hct-row\" data-runid=\"" + PageFlowUtil.filter(runId) + "\" data-id=\"" + PageFlowUtil.filter(id) + "\">" + getFormattedHtml(ctx) + "</a></span>");
+                            oldWriter.write("<span style=\"white-space:nowrap\"><a class=\"labkey-text-link hct-row\" data-runid=\"" + PageFlowUtil.filter(runId) + "\" data-id=\"" + PageFlowUtil.filter(id) + "\">" + getFormattedHtml(ctx) + "</a></span>");
                             if (!_hctRowClickHandlerAdded)
                             {
                                 HttpView.currentPageConfig().addHandlerForQuerySelector("a.hct-row", "click", "EHR.panel.LabworkSummaryPanel.showRunSummary(this.attributes.getNamedItem('data-runid').value, this.attributes.getNamedItem('data-id').value, this);");
@@ -1971,12 +1972,12 @@ public class ONPRC_EHRCustomizer extends AbstractTableCustomizer
                 return new DataColumn(colInfo){
 
                     @Override
-                    public void renderGridCellContents(RenderContext ctx, Writer out) throws IOException
+                    public void renderGridCellContents(RenderContext ctx, Writer oldWriter, HtmlWriter out) throws IOException
                     {
                         String objectid = (String)ctx.get("objectid");
                         String id = (String)ctx.get("Id");
 
-                        out.write("<span style=\"white-space:nowrap\"><a class=\"labkey-text-link cs-h-row\" data-objectid=\"" + PageFlowUtil.filter(objectid) + "\" data-id=\"" + PageFlowUtil.filter(id) + "\">[Show Case Hx]</a></span>");
+                        oldWriter.write("<span style=\"white-space:nowrap\"><a class=\"labkey-text-link cs-h-row\" data-objectid=\"" + PageFlowUtil.filter(objectid) + "\" data-id=\"" + PageFlowUtil.filter(id) + "\">[Show Case Hx]</a></span>");
                         if (!_caseHistoryClickHandlerAdded)
                         {
                             HttpView.currentPageConfig().addHandlerForQuerySelector("a.cs-h-row", "click", "EHR.window.CaseHistoryWindow.showCaseHistory(this.attributes.getNamedItem('data-objectid').value, this.attributes.getNamedItem('data-id').value, this);");
