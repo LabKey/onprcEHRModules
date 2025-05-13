094255bcb3de7a153bf98998c9e020b787d3853e
Marty Pradere <martyp@labkey.com>
2025-05-13 13:48:28 -0700

    sql script

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.007-24.008.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.007-24.008.sql
@@ -127,19 +127,5 @@ CREATE TABLE [onprc_ehr].[Prima_CassetteBases](
     [CurrentContainerId] [int] NULL
     )
 ;
-CREATE TABLE onprc_ehr.snomed_counter
-(
-    subset		nvarchar(255) NOT NULL,
-    count		integer NOT NULL,
-    prefix		nvarchar(10) NOT NULL,
-    container   entityid,
-    createdby   userid,
-    created     DATETIME,
-    modifiedby  userid,
-    modified    DATETIME,
-
-    CONSTRAINT pk_snomed_counter PRIMARY KEY (subset),
-    CONSTRAINT fk_onprc_snomed_counter_container FOREIGN KEY (container) REFERENCES core.Containers (EntityId)
-)
 
 GO
---
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-24.008-24.009.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE onprc_ehr.snomed_counter
+(
+    subset		nvarchar(255) NOT NULL,
+    count		integer NOT NULL,
+    prefix		nvarchar(10) NOT NULL,
+    container   entityid,
+    createdby   userid,
+    created     DATETIME,
+    modifiedby  userid,
+    modified    DATETIME,
+
+    CONSTRAINT pk_snomed_counter PRIMARY KEY (subset),
+    CONSTRAINT fk_onprc_snomed_counter_container FOREIGN KEY (container) REFERENCES core.Containers (EntityId)
+)
