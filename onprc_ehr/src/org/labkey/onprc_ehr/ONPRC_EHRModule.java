d1f8f06f3242a625198fbf5e8e9981eaff08e4dc
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-05-13 16:29:39 -0700

    Modified program to include versioin njumberfor th scripts.

--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -129,7 +129,7 @@ public class ONPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 24.008;
+        return 24.009;
     }
 
     @Override
