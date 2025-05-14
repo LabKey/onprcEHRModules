d96f3748bc94f80d4300b1eabebcf5c2740373f1
jonesgaohsu <jonesga@ohsu.edu>
2025-05-14 13:20:26 -0700

    Udpate of Archive Process

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-25.002-25.003.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-25.002-25.003.sql
@@ -3,7 +3,7 @@
   Script Date: 2025-03-03 TOday ******/
   --review and deploy with checule to run
 --Update to Add Release Build
-/*Update 2025-05-14 Resumed testing*/
+/*Update 2025-05-14 Resumed testing New*/
 SET ANSI_NULLS ON;
 GO
 SET QUOTED_IDENTIFIER ON;
