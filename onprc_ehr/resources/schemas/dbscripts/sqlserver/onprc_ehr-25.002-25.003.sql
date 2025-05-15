74be2eae64d9f33a6fc2c52fa95c81e16c797ab3
jonesgaohsu <jonesga@ohsu.edu>
2025-05-15 11:51:21 -0700

    Update of Stored Preocured as it is causing an error on Test F

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-25.002-25.003.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-25.002-25.003.sql
@@ -1,233 +1,218 @@
-/****** Object:  StoredProcedure [dbo].[ArchiveAuditTables_Update]
-  Code rewritten with Deep Seek assistance
-  Script Date: 2025-03-03 TOday ******/
-  --review and deploy with checule to run
---Update to Add Release Build for 1 Year
---Update 2025-05-15 For Testing F
-
-SET ANSI_NULLS ON;
+USE [Labkey_testF]
 GO
-SET QUOTED_IDENTIFIER ON;
+/****** Object:  StoredProcedure [dbo].[ArchiveAuditTables_DeepSeek1]    Script Date: 5/15/2025 11:50:18 AM ******/
+SET ANSI_NULLS ON
 GO
-
-CREATE PROCEDURE [dbo].[ArchiveAuditTables]
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[ArchiveAuditTables_DeepSeek1]
 AS
 BEGIN
     SET NOCOUNT ON;
 
     -- Declare configurable variables
-    DECLARE @SourceDB NVARCHAR(128) = 'Labkey_testf',
-        @DestDB NVARCHAR(128) = 'primeaudit_sandbox',
-        @RetentionYears INT = 7,
-        @SchemaName NVARCHAR(128) = 'audit',
-        @BatchSize INT = 1000; -- Batch size for archiving
+    DECLARE @SourceDB NVARCHAR(128) = 'Labkey_TestF',
+            @DestDB NVARCHAR(128) = 'primeaudit_sandbox',
+            @RetentionYears INT = 3,
+            @SchemaName NVARCHAR(128) = 'audit';
 
     -- Calculate cutoff date
     DECLARE @CutoffDate DATETIME = DATEADD(YEAR, -@RetentionYears, GETDATE());
 
     -- Validate database existence
     IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE name = @SourceDB)
-        BEGIN
-            RAISERROR('Source database "%s" does not exist', 16, 1, @SourceDB);
-            RETURN;
-        END
+BEGIN
+        RAISERROR('Source database "%s" does not exist', 16, 1, @SourceDB);
+        RETURN;
+END
 
     IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE name = @DestDB)
-        BEGIN
-            RAISERROR('Destination database "%s" does not exist', 16, 1, @DestDB);
-            RETURN;
-        END
+BEGIN
+        RAISERROR('Destination database "%s" does not exist', 16, 1, @DestDB);
+        RETURN;
+END
+
+    -- Create ArchiveLog table if it doesn't exist
+    DECLARE @CreateLogTableSQL NVARCHAR(MAX) = N'
+    USE ' + QUOTENAME(@DestDB) + N';
+    IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+                   WHERE TABLE_SCHEMA = ''dbo'' AND TABLE_NAME = ''ArchiveAuditLog'')
+    BEGIN
+        CREATE TABLE dbo.ArchiveAuditLog (
+            LogID INT IDENTITY(1,1) PRIMARY KEY,
+            TableName NVARCHAR(128) NOT NULL,
+            Operation NVARCHAR(50) NOT NULL,
+            StartTime DATETIME NOT NULL,
+            EndTime DATETIME NULL,
+            Status NVARCHAR(50) NULL,
+            RecordsProcessed INT NULL,
+            ErrorMessage NVARCHAR(MAX) NULL,
+            RetentionYears INT NULL
+        );
+    END';
+
+EXEC sp_executesql @CreateLogTableSQL;
 
     -- Validate schema existence in source
+    IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = @SourceDB)
+BEGIN
+        RAISERROR('Source database does not exist', 16, 1);
+        RETURN;
+END
+
     DECLARE @SourceSchemaCheck NVARCHAR(MAX) = N'
-    IF NOT EXISTS (SELECT 1 FROM ' + QUOTENAME(@SourceDB) + N'.sys.schemas WHERE name = @SchemaName)
+    IF NOT EXISTS (SELECT 1 FROM ' + QUOTENAME(@SourceDB) + N'.sys.schemas WHERE name = ''' + @SchemaName + ''')
     BEGIN
-        RAISERROR(''Source schema "%s" does not exist'', 16, 1, @SchemaName);
+        RAISERROR(''Source schema "%s" does not exist'', 16, 1, ''' + @SchemaName + ''');
     END';
 
-    BEGIN TRY
-        EXEC sp_executesql @SourceSchemaCheck,
-             N'@SchemaName NVARCHAR(128)',
-             @SchemaName = @SchemaName;
-    END TRY
-    BEGIN CATCH
-        DECLARE @SourceSchemaError NVARCHAR(4000) =
-            ERROR_MESSAGE() + ' in database ' + @SourceDB;
-        RAISERROR(@SourceSchemaError, 16, 1);
-        RETURN;
-    END CATCH
+EXEC sp_executesql @SourceSchemaCheck;
 
     -- Create schema in destination if needed
-    DECLARE @QuotedSchemaName NVARCHAR(128) = QUOTENAME(@SchemaName);
     DECLARE @CreateDestSchemaSQL NVARCHAR(MAX) = N'
     USE ' + QUOTENAME(@DestDB) + N';
-    IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = @SchemaName)
+    IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = ''' + @SchemaName + ''')
     BEGIN
-        EXEC(''CREATE SCHEMA ' + @QuotedSchemaName + N''');
+        EXEC(''CREATE SCHEMA ' + QUOTENAME(@SchemaName) + N''');
     END';
 
-    BEGIN TRY
-        EXEC sp_executesql @CreateDestSchemaSQL, N'@SchemaName NVARCHAR(128)', @SchemaName;
-    END TRY
-    BEGIN CATCH
-        DECLARE @DestSchemaError NVARCHAR(4000) =
-            'Error creating destination schema: ' + ERROR_MESSAGE();
-        RAISERROR(@DestSchemaError, 16, 1);
-        RETURN;
-    END CATCH
+EXEC sp_executesql @CreateDestSchemaSQL;
 
-    -- Retrieve list of audit tables
-    DECLARE @TableList TABLE (TableName NVARCHAR(128));
-    INSERT INTO @TableList
+    -- Create temporary table to hold list of tables
+CREATE TABLE #TableList (TableName NVARCHAR(128));
+
+-- Get list of audit tables from source database
+DECLARE @GetTablesSQL NVARCHAR(MAX) = N'
+    INSERT INTO #TableList
     SELECT TABLE_NAME
-    FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_SCHEMA = @SchemaName;
+    FROM ' + QUOTENAME(@SourceDB) + N'.INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_SCHEMA = ''' + @SchemaName + '''';
+
+EXEC sp_executesql @GetTablesSQL;
 
     -- Cursor to process tables
     DECLARE @CurrentTable NVARCHAR(128);
     DECLARE TableCursor CURSOR LOCAL FAST_FORWARD FOR
-        SELECT TableName FROM @TableList;
+SELECT TableName FROM #TableList;
 
-    OPEN TableCursor;
-    FETCH NEXT FROM TableCursor INTO @CurrentTable;
+OPEN TableCursor;
+FETCH NEXT FROM TableCursor INTO @CurrentTable;
 
-    WHILE @@FETCH_STATUS = 0
-        BEGIN
-            DECLARE @LogID INT;
+WHILE @@FETCH_STATUS = 0
+BEGIN
+        DECLARE @LogID INT;
 
-            -- Log the start of the operation
-            INSERT INTO dbo.ArchiveAuditLog (TableName, Operation, StartTime, Status, RetentionYears)
-            VALUES (@CurrentTable, 'Archive', GETDATE(), 'Started', @RetentionYears);
+        -- Log the start of the operation
+        DECLARE @InsertLogSQL NVARCHAR(MAX) = N'
+        USE ' + QUOTENAME(@DestDB) + N';
+        INSERT INTO dbo.ArchiveAuditLog
+            (TableName, Operation, StartTime, Status, RetentionYears)
+        VALUES (''' + @CurrentTable + ''', ''Archive'', GETDATE(), ''Started'', ' + CAST(@RetentionYears AS NVARCHAR(10)) + ');
+        SELECT @LogIDOUT = SCOPE_IDENTITY();';
 
-            SET @LogID = SCOPE_IDENTITY(); -- Get the LogID for the current operation
+EXEC sp_executesql @InsertLogSQL, N'@LogIDOUT INT OUTPUT', @LogIDOUT = @LogID OUTPUT;
 
-            BEGIN TRY
-                DECLARE @FullSourceTable NVARCHAR(512) = QUOTENAME(@SourceDB) + N'.' + QUOTENAME(@SchemaName) + N'.' + QUOTENAME(@CurrentTable),
+BEGIN TRY
+            DECLARE @FullSourceTable NVARCHAR(512) = QUOTENAME(@SourceDB) + N'.' + QUOTENAME(@SchemaName) + N'.' + QUOTENAME(@CurrentTable),
                     @FullDestTable NVARCHAR(512) = QUOTENAME(@DestDB) + N'.' + QUOTENAME(@SchemaName) + N'.' + QUOTENAME(@CurrentTable),
-                    @CheckTableSQL NVARCHAR(MAX),
-                    @CreateTableSQL NVARCHAR(MAX),
-                    @ArchiveSQL NVARCHAR(MAX),
-                    @TableExists BIT = 0;
+                    @ColumnList NVARCHAR(MAX) = N'';
 
-             EndDate   -- Check if destination table exists
-                SET @CheckTableSQL = N'
-            IF NOT EXISTS (SELECT 1
-                          FROM ' + QUOTENAME(@DestDB) + N'.INFORMATION_SCHEMA.TABLES
-                          WHERE TABLE_SCHEMA = @SchemaName
-                            AND TABLE_NAME = @CurrentTable)
+            -- Check if destination table exists and create if needed
+            DECLARE @CheckTableSQL NVARCHAR(MAX) = N'
+            USE ' + QUOTENAME(@DestDB) + N';
+            IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+                          WHERE TABLE_SCHEMA = ''' + @SchemaName + '''
+                            AND TABLE_NAME = ''' + @CurrentTable + ''')
             BEGIN
-                PRINT ''Skipping table ' + @CurrentTable + N' as it does not exist in the destination database.'';
-                RETURN;
-            END';
-
-                EXEC sp_executesql @CheckTableSQL,
-                     N'@SchemaName NVARCHAR(128), @CurrentTable NVARCHAR(128)',
-                     @SchemaName = @SchemaName,
-                     @CurrentTable = @CurrentTable;
-
-                -- Check if the "Created" column exists in the source table
-                DECLARE @CreatedColumnExists BIT = 0;
-                SET @CheckTableSQL = N'
-            IF EXISTS (SELECT 1
-                       FROM ' + QUOTENAME(@SourceDB) + N'.INFORMATION_SCHEMA.COLUMNS
-                       WHERE TABLE_SCHEMA = @SchemaName
-                         AND TABLE_NAME = @CurrentTable
-                         AND COLUMN_NAME = ''Created'')
-            BEGIN
-                SET @CreatedColumnExists = 1;
-            END';
-
-                EXEC sp_executesql @CheckTableSQL,
-                     N'@SchemaName NVARCHAR(128), @CurrentTable NVARCHAR(128), @CreatedColumnExists BIT OUTPUT',
-                     @SchemaName = @SchemaName,
-                     @CurrentTable = @CurrentTable,
-                     @CreatedColumnExists = @CreatedColumnExists OUTPUT;
-
-                IF @CreatedColumnExists = 0
-                    BEGIN
-                        RAISERROR('Table "%s" does not have a "Created" column. Skipping.', 10, 1, @CurrentTable);
-                        FETCH NEXT FROM TableCursor INTO @CurrentTable;
-                        CONTINUE;
-                    END
-
-                -- Get the column list (excluding the IDENTITY column)
-                DECLARE @ColumnList NVARCHAR(MAX);
-                SELECT @ColumnList = STRING_AGG(QUOTENAME(COLUMN_NAME), ', ')
-                FROM INFORMATION_SCHEMA.COLUMNS
-                WHERE TABLE_SCHEMA = @SchemaName
-                  AND TABLE_NAME = @CurrentTable
-                  AND COLUMNPROPERTY(OBJECT_ID(QUOTENAME(@SchemaName) + N'.' + QUOTENAME(@CurrentTable)), COLUMN_NAME, 'IsIdentity') = 0;
-
-                -- Archive data in batches
-                DECLARE @RecordsProcessed INT = 0,
-                    @TotalRecordsProcessed INT = 0;
-
-                WHILE 1=1
-                    BEGIN
-                        BEGIN TRANSACTION;
-
-                        SET @ArchiveSQL = N'
-                INSERT INTO ' + @FullDestTable + N' (' + @ColumnList + N')
-                SELECT TOP (@BatchSize) ' + @ColumnList + N'
+                SELECT * INTO ' + @FullDestTable + N'
                 FROM ' + @FullSourceTable + N'
-                WHERE Created < @CutoffDate
-                ORDER BY Created;
+                WHERE 1 = 0;
+            END';
 
-                -- Capture the number of records inserted
-                SET @RecordsProcessed = @@ROWCOUNT;
+EXEC sp_executesql @CheckTableSQL;
 
-                DELETE FROM ' + @FullSourceTable + N'
-                WHERE Created < @CutoffDate
-                  AND Created IN (SELECT TOP (@BatchSize) Created
-                                 FROM ' + @FullSourceTable + N'
-                                 WHERE Created < @CutoffDate
-                                 ORDER BY Created);
+            -- Get the column list (excluding the IDENTITY column) from source table
+CREATE TABLE #Columns (ColumnName NVARCHAR(128), IsIdentity BIT);
 
-                -- Update total records processed
-                SET @TotalRecordsProcessed = @TotalRecordsProcessed + @RecordsProcessed;
+DECLARE @GetColumnsSQL NVARCHAR(MAX) = N'
+            INSERT INTO #Columns
+            SELECT c.name AS ColumnName,
+                   COLUMNPROPERTY(OBJECT_ID(''' + QUOTENAME(@SourceDB) + N'.' + QUOTENAME(@SchemaName) + N'.' + QUOTENAME(@CurrentTable) + N'''), c.name, ''IsIdentity'') AS IsIdentity
+            FROM ' + QUOTENAME(@SourceDB) + N'.sys.columns c
+            JOIN ' + QUOTENAME(@SourceDB) + N'.sys.tables t ON c.object_id = t.object_id
+            JOIN ' + QUOTENAME(@SourceDB) + N'.sys.schemas s ON t.schema_id = s.schema_id
+            WHERE s.name = ''' + @SchemaName + '''
+              AND t.name = ''' + @CurrentTable + '''';
 
-                -- Log the total records processed
-                UPDATE dbo.ArchiveAuditLog
-                SET RecordsProcessed = @TotalRecordsProcessed,
-                    EndTime = GETDATE(),
-                    Status = ''Success''
-                WHERE LogID = @LogID;';
+EXEC sp_executesql @GetColumnsSQL;
 
-                        EXEC sp_executesql @ArchiveSQL,
-                             N'@CutoffDate DATETIME, @LogID INT, @BatchSize INT, @RecordsProcessed INT OUTPUT, @TotalRecordsProcessed INT OUTPUT',
-                             @CutoffDate = @CutoffDate,
-                             @LogID = @LogID,
-                             @BatchSize = @BatchSize,
-                             @RecordsProcessed = @RecordsProcessed OUTPUT,
-                             @TotalRecordsProcessed = @TotalRecordsProcessed OUTPUT;
+            -- Build column list string
+SELECT @ColumnList = @ColumnList + QUOTENAME(ColumnName) + ', '
+FROM #Columns
+WHERE IsIdentity = 0;
 
-                        COMMIT TRANSACTION;
+-- Remove trailing comma
+IF LEN(@ColumnList) > 0
+                SET @ColumnList = LEFT(@ColumnList, LEN(@ColumnList) - 1);
 
-                        -- Exit loop if no more records to process
-                        IF @RecordsProcessed < @BatchSize BREAK;
-                    END
-            END TRY
-            BEGIN CATCH
-                IF @@TRANCOUNT > 0
-                    ROLLBACK TRANSACTION;
+DROP TABLE #Columns;
 
-                -- Log the error
-                DECLARE @ErrorMessage NVARCHAR(4000) =
-                    'Error archiving ' + @CurrentTable + N': ' + ERROR_MESSAGE();
+-- Archive data with transaction
+BEGIN TRANSACTION;
 
-                UPDATE dbo.ArchiveAuditLog
-                SET EndTime = GETDATE(),
-                    Status = 'Error',
-                    ErrorMessage = @ErrorMessage
-                WHERE LogID = @LogID;
+            DECLARE @ArchiveSQL NVARCHAR(MAX) = N'
+            USE ' + QUOTENAME(@DestDB) + N';
+            INSERT INTO ' + @FullDestTable + N' (' + @ColumnList + N')
+            SELECT ' + @ColumnList + N'
+            FROM ' + @FullSourceTable + N'
+            WHERE Created < @CutoffDate;
 
-                PRINT @ErrorMessage; -- Log the error to the console as well
-            END CATCH
+            DECLARE @RecordsInserted INT = @@ROWCOUNT;
 
-            FETCH NEXT FROM TableCursor INTO @CurrentTable;
-        END
+            DELETE FROM ' + @FullSourceTable + N'
+            WHERE Created < @CutoffDate;
 
-    CLOSE TableCursor;
-    DEALLOCATE TableCursor;
+            DECLARE @RecordsDeleted INT = @@ROWCOUNT;
+
+            UPDATE dbo.ArchiveAuditLog
+            SET RecordsProcessed = @RecordsInserted,
+                EndTime = GETDATE(),
+                Status = ''Success''
+            WHERE LogID = @LogID;';
+
+EXEC sp_executesql @ArchiveSQL,
+                N'@CutoffDate DATETIME, @LogID INT',
+                @CutoffDate = @CutoffDate,
+                @LogID = @LogID;
+
+COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+                ROLLBACK TRANSACTION;
+
+            -- Log the error
+            DECLARE @ErrorMessage NVARCHAR(4000) =
+                'Error archiving ' + @CurrentTable + N': ' + ERROR_MESSAGE();
+
+            DECLARE @UpdateLogSQL NVARCHAR(MAX) = N'
+            USE ' + QUOTENAME(@DestDB) + N';
+            UPDATE dbo.ArchiveAuditLog
+            SET EndTime = GETDATE(),
+                Status = ''Error'',
+                ErrorMessage = @ErrorMessage
+            WHERE LogID = ' + CAST(@LogID AS NVARCHAR(10));
+
+EXEC sp_executesql @UpdateLogSQL, N'@ErrorMessage NVARCHAR(4000)', @ErrorMessage = @ErrorMessage;
+
+            PRINT @ErrorMessage;
+END CATCH
+
+FETCH NEXT FROM TableCursor INTO @CurrentTable;
+END
+
+CLOSE TableCursor;
+DEALLOCATE TableCursor;
+
+DROP TABLE #TableList;
 END;
-GO
