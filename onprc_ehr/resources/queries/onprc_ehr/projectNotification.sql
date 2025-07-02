7acd44e35646351f97e71d4db76d3b9b13f4cdc5
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-07-01 22:57:27 -0700

    MOdified project notification to include last modified project record.

--- onprc_ehr/resources/queries/onprc_ehr/projectNotification.sql
+++ onprc_ehr/resources/queries/onprc_ehr/projectNotification.sql
@@ -1,14 +1,18 @@
 
-SELECT proj.project,
-        proj.name,
-        proj.protocol,
-        proj.investigatorid,
-        proj.startdate,
-        proj.enddate,
-        (select j.enddate from onprc_ehr.CenterProjectsTemp j where j.project = proj.project and j.date_posted in (select max(cps.date_posted)
-                                from onprc_ehr.CenterProjectsTemp cps where cps.date_posted <= proj.modified ) ) as previousdate
+SELECT  proj.project,
+        group_concat(proj.name) as name,
+        group_concat(proj.protocol) as protocol,
+        group_concat(proj.investigatorid) as investigatorid,
+        group_concat(proj.startdate) as startdate,
+        group_concat(proj.enddate) as enddate,
+        group_concat(cps.enddate) as previousdate
 
- FROM ehr.project proj
- WHERE (proj.enddate is null or proj.enddate >= now() )
- And proj.modified >= cast(now() as date))
- group by proj.project
+FROM ehr.project proj, onprc_ehr.CenterProjectsTemp cps
+WHERE (proj.enddate is null or proj.enddate >= now() )
+  And proj.modified >= cast(now() as date)
+  And proj.project = cps.project
+  And cps.date_posted in (select max(j.date_posted)
+                          from onprc_ehr.CenterProjectsTemp j where j.project = proj.project )
+
+
+group by proj.project
