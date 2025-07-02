43bb8b99bb89ecdaea1777d16237c4046cd53e49
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-07-01 22:41:45 -0700

    MOdified project notification to include last modified project record.

--- onprc_ehr/resources/queries/onprc_ehr/projectNotification.sql
+++ onprc_ehr/resources/queries/onprc_ehr/projectNotification.sql
@@ -8,7 +8,7 @@ SELECT proj.project,
         (select j.enddate from onprc_ehr.CenterProjectsTemp j where j.project = proj.project and j.date_posted in (select max(cps.date_posted)
                                 from onprc_ehr.CenterProjectsTemp cps where cps.date_posted <= proj.modified ) ) as previousdate
 
- FROM ehr.project proj,
+ FROM ehr.project proj
  WHERE (proj.enddate is null or proj.enddate >= now() )
  And proj.modified >= cast(now() as date))
  group by proj.project
