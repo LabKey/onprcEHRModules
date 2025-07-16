a32d5051efc60b9dd824a0e4bcc9dce3a356a742
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-07-15 18:40:40 -0700

    Modified project notification to account for new project entries.

--- onprc_ehr/resources/queries/onprc_ehr/projectNotification.sql
+++ onprc_ehr/resources/queries/onprc_ehr/projectNotification.sql
@@ -5,15 +5,15 @@ SELECT  proj.project,
         group_concat(proj.investigatorid) as investigatorid,
         group_concat(proj.startdate) as startdate,
         group_concat(proj.enddate) as enddate,
-        group_concat(cps.enddate) as previousdate,
+       ( select group_concat(cps.enddate) from  onprc_ehr.CenterProjectsTemp cps where cps.project = proj.project
+             And cps.date_posted in (select max(j.date_posted)
+                                from onprc_ehr.CenterProjectsTemp j where j.project = proj.project )  )as previousdate,
         group_concat(proj.protocol.external_iD) as protocolID
 
-FROM ehr.project proj, onprc_ehr.CenterProjectsTemp cps
+FROM ehr.project proj
 WHERE (proj.enddate is null or proj.enddate >= now() )
   And proj.modified >= cast(now() as date)
-  And proj.project = cps.project
-  And cps.date_posted in (select max(j.date_posted)
-                          from onprc_ehr.CenterProjectsTemp j where j.project = proj.project )
 
 
 group by proj.project
+
