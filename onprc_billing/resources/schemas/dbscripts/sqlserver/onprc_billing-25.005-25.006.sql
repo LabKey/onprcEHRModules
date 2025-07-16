072dd6d5b0da2ccfe38b91e02498a1b4c8b6631a
Gary Jones <jonesga@ohsu.edu>
2025-07-16 14:46:28 -0700

    Corrected issue the enddate for indirectRate setting to allow nul was pointing to the wrong SQL Table

--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-25.005-25.006.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-25.005-25.006.sql
@@ -1,5 +1,2 @@
---This update allow sthe end date of the row to contain a null value
---Revised 2025-0714
-ALTER TABLE ogasynchIR
-ALTER COLUMN EndDate DATE NULL;
-
+ALTER TABLE onprc_billing.indirectRate
+ALTER COLUMN enddate [data_type] NULL;
