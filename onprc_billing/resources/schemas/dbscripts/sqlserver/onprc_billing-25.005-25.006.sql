b9cea6b1e2e5935e50d507f2561d87db997c622d
Brent Logan <loganb@ohsu.edu>
2025-07-21 09:55:59 -0700

    Update onprc_billing-25.005-25.006.sql
    Stylistic changes

--- onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-25.005-25.006.sql
+++ onprc_billing/resources/schemas/dbscripts/sqlserver/onprc_billing-25.005-25.006.sql
@@ -1,6 +1,4 @@
---This update allow sthe end date of the row to contain a null value
---Revised 2025-0630
-ALTER TABLE onprc_billing.inDirectRates
-ALTER COLUMN EndDate DATETime NULL;
-
-
+--This update allows the endDate field to be NULL
+--Revised 2025-06-30
+ALTER TABLE onprc_billing.IndirectRates
+ALTER COLUMN EndDate DATETIME NULL;
