f8eacdc8faf2ba699730ec941ac9653571cebb7d
Gary Jones <jonesga@ohsu.edu>
2025-09-09 13:55:53 -0700

    UPdate of Audit Archiving for 2507 3 Files 0- Original MIsnamed

--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -113,7 +113,7 @@ import java.util.Collections;
 /**
  * User: jonesga
  * Date: 6/26/2018
- * Change of ONPRC Module Number
+ * Change of ONPRC Module Number 20250909
  */
 public class ONPRC_EHRModule extends ExtendedSimpleModule
 {
