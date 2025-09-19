1d656fac5638b559027bf154836867c54b33d9c1
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-09-19 00:02:28 -0700

    Created an upload process.

---
+++ onprc_ehr/resources/web/onprc_ehr/panel/ExamInstructionsPanel.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014-2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+Ext4.define('ONPRC_EHR.panel.ExamInstructionsPanel', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.onprc_ehr-examinstructionspanel',
+
+    initComponent: function(){
+        var buttons = [];
+        LDK.Assert.assertNotEmpty('No data entry panel', this.dataEntryPanel);
+        var btnCfg = EHR.DataEntryUtils.getDataEntryFormButton('APPLYFORMTEMPLATE_NO_ID');
+        if (btnCfg){
+            btnCfg = this.dataEntryPanel.configureButton(btnCfg);
+            if (btnCfg){
+                btnCfg.defaultDate = new Date();
+                btnCfg.text = 'Apply Template To Form';
+                buttons.push(btnCfg);
+            }
+        }
+
+        // var btnCfg2 = EHR.DataEntryUtils.getDataEntryFormButton('OPENCLINICALCASE');
+        // if (btnCfg2){
+        //     btnCfg2 = this.dataEntryPanel.configureButton(btnCfg2);
+        //     buttons.push(btnCfg2);
+        // }
+
+        Ext4.apply(this, {
+            defaults: {
+
+            },
+            bodyStyle: 'padding: 5px;',
+            title: 'Instructions',
+            items: [{
+                html: 'Use the \'Apply Form Template\' button below to load predefined input values ',
+                maxWidth: Ext4.getBody().getWidth() * 0.8,
+                style: 'padding-top: 10px;padding-bottom: 10px;',
+                border: false
+            },{
+                layout: 'hbox',
+                border: false,
+                defaults: {
+                    style: 'margin-right: 5px;'
+                },
+                items: buttons
+            }]
+        });
+
+        this.callParent(arguments);
+    }
+
+});
