51ef72ddf5336fa0e16fe2385d03462baf56b35a
Marty Pradere <martyp@labkey.com>
2025-10-12 16:41:37 -0700

    Refactors

--- onprc_billing/resources/queries/onprc_billing/perDiemsByDay.sql
+++ onprc_billing/resources/queries/onprc_billing/perDiemsByDay.sql
@@ -1,21 +1,16 @@
-/*
- * Copyright (c) 2010-2013 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
 
 SELECT
-  t.*,
-  CASE
-    WHEN t.overlappingProjects IS NULL then 1
-    WHEN t.tmbAssignments > 0 then 0  --note: when co-assigned to TMB, per diem is never charged.  if TMB is single-assigned, it needs to pay per diem, and will be caught above
-    WHEN t.assignedProject IS NULL AND t.overlappingProjects IS NOT NULL THEN 0
-    WHEN t.ProjectType != 'Research' and t.overlappingProjectsCategory LIKE '%Research%' Then 0
-    WHEN t.ProjectType != 'Research' and t.overlappingProjectsCategory NOT LIKE '%Research%' Then (1.0 / (t.totalOverlappingProjects + 1))
+    t.*,
+    CASE
+        WHEN t.overlappingProjects IS NULL then 1
+        WHEN t.tmbAssignments > 0 then 0  --note: when co-assigned to TMB, per diem is never charged.  if TMB is single-assigned, it needs to pay per diem, and will be caught above
+        WHEN t.assignedProject IS NULL AND t.overlappingProjects IS NOT NULL THEN 0
+        WHEN t.ProjectType != 'Research' and t.overlappingProjectsCategory LIKE '%Research%' Then 0
+    WHEN t.ProjectType != 'Research' and t.overlappingProjectsCategory NOT LIKE '%Research%' Then (1.0 / NULLIF((t.totalOverlappingProjects + 1), 0))
     WHEN t.ProjectType = 'Research' and t.overlappingProjectsCategory NOT LIKE '%Research%' Then 1
-    WHEN t.ProjectType = 'Research' and t.overlappingProjectsCategory LIKE '%Research%' Then (1.0 / (t.totalOverlappingResearchProjects + 1))
+    WHEN t.ProjectType = 'Research' and t.overlappingProjectsCategory LIKE '%Research%' Then (1.0 / NULLIF((t.totalOverlappingResearchProjects + 1), 0))
     ELSE 1
-  END as effectiveDays,
+END as effectiveDays,
   CASE
     WHEN (t.assignedProject IS NULL AND t.overlappingProjects IS NULL) THEN 'Base Grant'
     WHEN t.overlappingProjects IS NULL then 'Single Project'
@@ -27,7 +22,23 @@ SELECT
     WHEN t.ProjectType = 'Research' and t.overlappingProjectsCategory NOT LIKE '%Research%' Then 'Single Project'
     WHEN t.ProjectType = 'Research' and t.overlappingProjectsCategory LIKE '%Research%' Then 'Multiple Research'
     ELSE 'Unknown'
-  END as category,
+END as category,
+
+  CASE
+      WHEN (t.perDiemFeeCount > 1) THEN null  --this should catch duplicate chargeIds
+     --use the treatmentOtrder to look for BottleFed
+      WHEN (t.bottleFedRecordCount > 0 AND t.researchRecordCount > 0) THEN maxPdfChargeId
+      --if this item supports infants, charge that
+
+      WHEN (pdfChargeInfantCount > 0 AND maxPdfChargeId IS NOT NULL) THEN maxPdfChargeId
+      --otherwise infants are a special rate
+      WHEN (perDiemAge < CAST(javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.INFANT_PER_DIEM_AGE') AS INTEGER)) THEN (SELECT ci.rowid FROM onprc_billing_public.chargeableItems ci WHERE ci.name = javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.INFANT_PER_DIEM'))
+      --add quarantine flags, which trump housing type
+      WHEN (quarantineFlagCount > 0) THEN (SELECT ci.rowid FROM onprc_billing_public.chargeableItems ci WHERE ci.name = javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.QUARANTINE_PER_DIEM'))
+      --finally defer to housing condition
+      ELSE maxPdfChargeId
+END as chargeId,
+
   --find overlapping tier flags on that day
   coalesce((
      SELECT group_concat(DISTINCT f.flag.value) as tier
@@ -60,24 +71,13 @@ SELECT
     group_concat(DISTINCT a.objectid) as assignmentRecords,
     group_concat(DISTINCT h3.room.housingCondition.value) as housingConditions,
     group_concat(DISTINCT h3.room.housingType.value) as housingTypes,
-    CASE
-      WHEN (count(DISTINCT pdf.chargeId) > 1) THEN null  --this should catch duplicate chargeIds
-      --determine if the animal has been bottle fed by checking the flag - bottle fed
-      --When (count(*) from study.treatment_Order q1 where q1.id = i2.id and q1.code.value like '%bottle%' and q1.dateonly <=i2.dateOnly) then 'BottleFed' and Animal Is research assigned
-     --use the treatmentOtrder to look for BottleFed
-      WHEN (Select count(*) AS c from study.treatment_Order t1 where t1.id = i2.id and t1.code.meaning like '%Bottle%' and t1.date <=i2.dateOnly) > 0
-   				  and (Select count(*) AS c from study.assignment a3 where a3.id = i2.id and a3.date <= i2.dateOnly and a3.endDateCoalesced > i2.dateOnly and a3.project.Use_Category like '%Research%') > 0
-   			  THEN max(pdf.chargeId)
-      --if this item supports infants, charge that
-
-      WHEN (count(CASE WHEN pdf.canChargeInfants = true THEN 1 ELSE null END) > 0 AND max(pdf.chargeId) IS NOT NULL) THEN max(pdf.chargeId)
-      --otherwise infants are a special rate
-      WHEN (max(timestampdiff('SQL_TSI_DAY', d.birth, i2.dateOnly)) < CAST(javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.INFANT_PER_DIEM_AGE') AS INTEGER)) THEN (SELECT ci.rowid FROM onprc_billing_public.chargeableItems ci WHERE ci.name = javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.INFANT_PER_DIEM'))
-      --add quarantine flags, which trump housing type
-      WHEN (SELECT count(*) AS c FROM study.flags q WHERE q.Id = i2.Id AND q.flag.value LIKE '%Quarantine%' AND q.dateOnly <= i2.dateOnly AND q.enddateCoalesced >= i2.dateOnly) > 0 THEN (SELECT ci.rowid FROM onprc_billing_public.chargeableItems ci WHERE ci.name = javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.QUARANTINE_PER_DIEM'))
-      --finally defer to housing condition
-      ELSE max(pdf.chargeId)
-    END as chargeId,
+    max(timestampdiff('SQL_TSI_DAY', d.birth, i2.dateOnly)) as perDiemAge,
+    count(DISTINCT pdf.chargeId) as perDiemFeeCount,
+    count(*) AS bottleFedRecordCount,
+    count(*) AS researchRecordCount,
+    count(CASE WHEN pdf.canChargeInfants = true THEN 1 ELSE null END) AS pdfChargeInfantCount,
+     max(pdf.chargeId) AS maxPdfChargeId,
+    (SELECT count(*) AS c FROM study.flags q WHERE q.Id = i2.Id AND q.flag.value LIKE '%Quarantine%' AND q.dateOnly <= i2.dateOnly AND q.enddateCoalesced >= i2.dateOnly) AS quarantineFlagCount,
     max(i2.startDate) as startDate @hidden,
     count(tmb.Id) as tmbAssignments,
     SUM(CASE WHEN a.projectName = javaConstant('org.labkey.onprc_ehr.ONPRC_EHRManager.TMB_PROJECT') THEN 1 ELSE 0 END) as isTMBProject
@@ -100,6 +100,10 @@ FROM (
 JOIN study.demographics d ON (
   i2.Id = d.Id
 )
+
+LEFT JOIN study.treatment_Order t1 ON t1.id = i2.id AND t1.code.meaning LIKE '%Bottle%' AND t1.date <=i2.dateOnly
+
+LEFT JOIN study.assignment a3 ON a3.id = i2.id AND a3.date <= i2.dateOnly AND a3.endDateCoalesced > i2.dateOnly AND a3.project.Use_Category LIKE '%Research%'
 
 -- housing is a little tricky.  using the query above, we want to find the max start date, on or before this day
 -- the housingType from this room will be used
