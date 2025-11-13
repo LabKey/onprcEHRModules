ba750752db6e5497eb821d5b68216a5553dc00ad
ohsudev <blasar@ohsu.edu>
2025-11-13 12:51:24 -0800

    Modified Chemistry Results to flag Panic values.

--- onprc_ehr/resources/scripts/onprc_ehr/onprc_triggers.js
+++ onprc_ehr/resources/scripts/onprc_ehr/onprc_triggers.js
@@ -1378,7 +1378,7 @@ exports.init = function(EHR){
 
     // Added: 10-6-2025
     EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Events.AFTER_UPSERT, 'study', 'chemistryResults', function (helper, scriptErrors, row, oldRow) {
-        if (row.Id && row.qualresults == '%panic%')  {
+        if (row.Id && ['panic','Panic'].indexOf(row.qualresults ) != -1)  {
             triggerHelper.sendClinpatPanicEmail(row.Id, row.objectid,row.vet);
         }
     });
