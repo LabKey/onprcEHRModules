f09523db45b29dc5a65f78d3cce1e8730b4490b7
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-11-17 13:24:27 -0800

    Created a new Compliance Exception report that identifies employeeids that are not members of the the Employee units/categories data set.

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/complianceEmployeelist_rpt.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/complianceEmployeelist_rpt.sql
@@ -1,5 +1,6 @@
-select a.employeeid, a.unit, null as category, a.enddate from ehr_compliancedb.Employees a where
-    unit not in (select unit from ehr_compliancedb.EmployeePerUnit j where j.EmployeeId = a.employeeid)
+select a.employeeid, a.unit, null as category, a.enddate
+from ehr_compliancedb.Employees a where
+    a.unit not in (select unit from ehr_compliancedb.EmployeePerUnit j where j.EmployeeId = a.employeeid)
                                                                                              and a.unit is not null
 ---and a.EndDate is null
 
@@ -9,9 +10,10 @@ group by a.employeeid, a.unit, a.enddate
 
 union
 
-select a.employeeid, null as unit, a.category, a.enddate from ehr_compliancedb.Employees a where
+select a.employeeid, null as unit, a.category, a.enddate
+from ehr_compliancedb.Employees a where
     category not in (select category from ehr_compliancedb.EmployeePerUnit j where j.EmployeeId = a.employeeid)
-                                                                                             and a.category is not null
+    a.                                                                                         and a.category is not null
 ---and a.EndDate is null
 
 group by a.employeeid, a.category, a.enddate
