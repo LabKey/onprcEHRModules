1c9943296329e74f7a12963c3be459360c0ff54b
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-11-17 11:28:33 -0800

    Created a new Compliance Exception report that identifies employeeids that are not members of the the Employee units/categories data set.

---
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/complianceEmployeelist_rpt.sql
@@ -0,0 +1,19 @@
+select a.employeeid, a.unit, null as category, a.enddate from ehr_compliancedb.Employees a where
+    unit not in (select unit from ehr_compliancedb.EmployeePerUnit j where j.EmployeeId = a.employeeid)
+                                                                                             and a.unit is not null
+---and a.EndDate is null
+
+
+group by a.employeeid, a.unit, a.enddate
+
+
+union
+
+select a.employeeid, null as unit, a.category, a.enddate from ehr_compliancedb.Employees a where
+    category not in (select category from ehr_compliancedb.EmployeePerUnit j where j.EmployeeId = a.employeeid)
+                                                                                             and a.category is not null
+---and a.EndDate is null
+
+group by a.employeeid, a.category, a.enddate
+
+order by a.employeeid
