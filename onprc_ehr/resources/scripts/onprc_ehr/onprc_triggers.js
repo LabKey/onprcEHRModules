1e8ffabd5e609ef51f85cfc989f5530987b82683
ohsudev <blasar@ohsu.edu>
2025-11-19 15:05:37 -0800

    Created a email notification for Chemistry entries that shows a "qualresutl" values that contain string called"panic"/

--- onprc_ehr/resources/scripts/onprc_ehr/onprc_triggers.js
+++ onprc_ehr/resources/scripts/onprc_ehr/onprc_triggers.js
@@ -1378,8 +1378,8 @@ exports.init = function(EHR){
 
     // Added: 10-6-2025
     EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Events.AFTER_UPSERT, 'study', 'chemistryResults', function (helper, scriptErrors, row, oldRow) {
-        // ['panic','Panic'].indexOf(row.qualresult) !== -1
-        if (row.Id && row.qualresult.toLowerCase().indexOf('panic') !== -1)  {
+
+        if (row.Id && row.qualresult && row.qualresult.indexOf('panic') !== -1)  {
                console.log("panic values:  " + row.qualresult);
             triggerHelper.sendClinpathPanicEmail(row.Id, row.objectid);
         }
