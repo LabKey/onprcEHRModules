959c5fd62e87ac68ca9b9508987ca3ba986f3c1d
loganb <loganb@ohsu.edu>
2025-11-27 09:17:28 -0800

    Minor comment change

--- onprc_ehr/src/org/labkey/onprc_ehr/notification/ColonyAlertsNotification.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/notification/ColonyAlertsNotification.java
@@ -2335,7 +2335,7 @@ public class ColonyAlertsNotification extends AbstractEHRNotification
 
     /**
      *  Created by Kollil, 10/25
-     *  Get a list of tasks daily on a rolling 7 day window to review for QC.
+     *  Get a list of tasks daily on a rolling 7-day window to review for QC.
      *  This will allow techs to see what new IDs were created by whom, and review for accuracy,
      *  housing history, group ids and flags.
      *  Refer tkt # 13504
