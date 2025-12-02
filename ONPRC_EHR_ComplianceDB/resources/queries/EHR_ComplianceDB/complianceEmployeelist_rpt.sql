9a4a5a94db62f45e3dee8b901f2b312b1ef48a63
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-12-01 23:00:20 -0800

    Created a new Employee unit/category exception reports.

--- ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/complianceEmployeelist_rpt.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/EHR_ComplianceDB/complianceEmployeelist_rpt.sql
@@ -1,21 +1,23 @@
-select a.employeeid, a.unit, null as category, a.enddate
-from ehr_compliancedb.Employees a where
-    a.unit not in (select unit from ehr_compliancedb.EmployeePerUnit j where j.EmployeeId = a.employeeid)
-                                                                                             and a.unit is not null
----and a.EndDate is null
+select a.employeeid, a.unit, null as category, a.enddate, a.lastName, a.firstName, a.email, a.type, a.title, a.category
+from ehr_compliancedb.employees a
+where a.unit not in (select unit from ehr_compliancedb.employeeperUnit j where j.EmployeeId = a.employeeid)
+and a.unit is not null
+and a.enddate is null
 
 
-group by a.employeeid, a.unit, a.enddate
+
+group by a.employeeid, a.unit, a.enddate, a.lastName, a.firstName,  a.email, a.type, a.title, a.category
 
 
 union
 
-select a.employeeid, null as unit, a.category, a.enddate
-from ehr_compliancedb.Employees a where
-    a.category not in (select category from ehr_compliancedb.EmployeePerUnit j where j.EmployeeId = a.employeeid)
-                                                                                           and a.category is not null
----and a.EndDate is null
+select a.employeeid, null as unit, a.category, a.enddate, a.lastName,
+       a.firstName, a.email, a.type, a.title, a.category
+from ehr_compliancedb.employees a where
+    a.category not in (select category from ehr_compliancedb.employeeperUnit j where j.EmployeeId = a.employeeid)
+and a.category is not null
+and a.enddate is null
 
-group by a.employeeid, a.category, a.enddate
+group by a.employeeid, a.category, a.enddate, a.lastName, a.firstName,  a.email, a.type, a.title, a.category
 
 order by a.employeeid
