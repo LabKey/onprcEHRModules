7e3fe7d4b578a9f4d271e4da405ee8ac36769a61
Gary Jones <jonesga@ohsu.edu>
2025-12-16 14:58:11 -0700

    New QUery to Match fields in leaseFeeRates query. Dropping old CTE lease fee query

---
+++ onprc_billing/resources/queries/onprc_billing/leasefeerates.sql
@@ -0,0 +1,294 @@
+PARAMETERS (StartDate TIMESTAMP, EndDate TIMESTAMP)
+
+WITH
+-- =====================================================================================
+-- 1) Base Lease Fees
+-- =====================================================================================
+base_lease AS (
+    SELECT
+        p.id,
+        p.date,
+        p.enddate,
+        p.assignmentStart,
+        p.project,
+        p.project.displayName AS projectDisplayName,
+        p.projectedReleaseCondition,
+        p.releaseCondition,
+        p.assignCondition,
+        p.releaseType,
+        p.ageAtTime,
+        p.category,
+        p.chargeId,
+        p.leaseCharge1,
+        p.leaseCharge2,
+        p.sourceRecord,
+        p.chargeCategory,
+        p.quantity,
+        p.isAdjustment,
+        p.datefinalized,
+        p.enddatefinalized
+    FROM onprc_billing.leaseFees p
+    WHERE CAST(p.date AS DATE)
+          BETWEEN CAST(StartDate AS DATE) AND CAST(EndDate AS DATE)
+      AND p.category = 'Lease Fees'
+),
+
+-- =====================================================================================
+-- 2) Alias / Account Context
+-- =====================================================================================
+alias_context AS (
+    SELECT
+        bl.*,
+        a.alias                               AS account,
+        a.category                            AS aliasCategory,
+        a.faRate                              AS faRate,
+        a.aliasType.aliasType                 AS aliasType,
+        a.aliasType.removeSubsidy             AS removeSubsidy,
+        a.aliasType.canRaiseFA                AS canRaiseFA,
+        a.fiscalAuthority.faid                AS faid,
+        a.aliasEnabled                        AS aliasEnabled,
+        a.budgetStartDate                     AS budgetStartDate,
+        a.budgetEndDate                       AS budgetEndDate,
+        a.projectStatus                       AS projectStatus,
+        COALESCE(a.investigatorId,
+                 bl.project.investigatorId)   AS investigatorId
+    FROM base_lease bl
+    LEFT JOIN onprc_billing_public.projectAccountHistory pah
+           ON pah.project = bl.project
+          AND pah.startDate <= CAST(bl.date AS DATE)
+          AND pah.endDate   >= CAST(bl.date AS DATE)
+    LEFT JOIN onprc_billing_public.aliases a
+           ON pah.account = a.alias
+),
+
+-- =====================================================================================
+-- 3) Rates / Exemptions / Multipliers / Indirect Rate
+-- =====================================================================================
+rate_context AS (
+    SELECT
+        ac.*,
+
+        cr.unitCost   AS nihRate1,
+        cr.subsidy    AS subsidy1,
+        e.unitCost    AS exemptRate1,
+        e.rowid       AS exemptionId1,
+
+        cr2.unitCost  AS nihRate2,
+        cr2.subsidy   AS subsidy2,
+        e2.unitCost   AS exemptRate2,
+        e2.rowid      AS exemptionId2,
+
+        cr3.unitCost  AS nihRate3,
+        cr3.subsidy   AS subsidy3,
+        e3.unitCost   AS exemptRate3,
+        e3.rowid      AS exemptionId3,
+
+        pm.multiplier AS multiplier,
+        ir.indirectRate AS indirectRate
+    FROM alias_context ac
+
+    LEFT JOIN onprc_billing_public.chargeRates cr
+           ON ac.assignmentStart >= cr.startDate
+          AND (ac.assignmentStart <= cr.enddateCoalesced OR cr.enddate IS NULL)
+          AND ac.chargeId = cr.chargeId
+
+    LEFT JOIN onprc_billing_public.chargeRateExemptions e
+           ON ac.assignmentStart >= e.startDate
+          AND (ac.assignmentStart <= e.enddateCoalesced OR e.enddate IS NULL)
+          AND ac.chargeId = e.chargeId
+          AND ac.project = e.project
+
+    LEFT JOIN onprc_billing_public.chargeRates cr2
+           ON ac.assignmentStart >= cr2.startDate
+          AND (ac.assignmentStart <= cr2.enddateCoalesced OR cr2.enddate IS NULL)
+          AND ac.leaseCharge1 = cr2.chargeId
+
+    LEFT JOIN onprc_billing_public.chargeRateExemptions e2
+           ON ac.assignmentStart >= e2.startDate
+          AND (ac.assignmentStart <= e2.enddateCoalesced OR e2.enddate IS NULL)
+          AND ac.leaseCharge1 = e2.chargeId
+          AND ac.project = e2.project
+
+    LEFT JOIN onprc_billing_public.chargeRates cr3
+           ON ac.assignmentStart >= cr3.startDate
+          AND (ac.assignmentStart <= cr3.enddateCoalesced OR cr3.enddate IS NULL)
+          AND ac.leaseCharge2 = cr3.chargeId
+
+    LEFT JOIN onprc_billing_public.chargeRateExemptions e3
+           ON ac.assignmentStart >= e3.startDate
+          AND (ac.assignmentStart <= e3.enddateCoalesced OR e3.enddate IS NULL)
+          AND ac.leaseCharge2 = e3.chargeId
+          AND ac.project = e3.project
+
+    LEFT JOIN onprc_billing_public.projectMultipliers pm
+           ON ac.date >= pm.startDate
+          AND (ac.date <= pm.enddateCoalesced OR pm.enddate IS NULL)
+          AND ac.account = pm.account
+
+    LEFT JOIN onprc_billing.ogaSynchIR ir
+           ON ir.alias = ac.account
+),
+
+-- =====================================================================================
+-- 4) Unit Cost Calculation (Parser-Safe)
+-- =====================================================================================
+calculated_costs AS (
+    SELECT
+        rc.*,
+
+        CAST(
+            CASE
+                WHEN exemptRate1 IS NOT NULL THEN exemptRate1
+
+                WHEN multiplier IS NOT NULL AND nihRate1 IS NOT NULL
+                    THEN (CAST(nihRate1 AS DOUBLE) * CAST(multiplier AS DOUBLE))
+
+                WHEN nihRate1 IS NULL THEN NULL
+
+                WHEN aliasCategory IS NOT NULL AND aliasCategory <> 'OGA'
+                    THEN nihRate1
+
+                WHEN aliasType IS NULL THEN NULL
+
+                WHEN removeSubsidy = TRUE
+                     AND canRaiseFA = TRUE
+                     AND chargeId.canRaiseFA = TRUE
+                    THEN (
+                        (CAST(nihRate1 AS DOUBLE) / NULLIF((1 - COALESCE(subsidy1, 0)), 0))
+                        *
+                        (
+                            1 +
+                            CASE
+                                WHEN faRate IS NOT NULL
+                                     AND indirectRate IS NOT NULL
+                                     AND faRate < CAST(indirectRate AS DOUBLE)
+                                    THEN (CAST(indirectRate AS DOUBLE) / (1 + faRate))
+                                ELSE 0
+                            END
+                        )
+                    )
+
+                WHEN removeSubsidy = TRUE AND canRaiseFA = FALSE
+                    THEN (CAST(nihRate1 AS DOUBLE) / NULLIF((1 - COALESCE(subsidy1, 0)), 0))
+
+                WHEN removeSubsidy = FALSE
+                     AND canRaiseFA = TRUE
+                     AND chargeId.canRaiseFA = TRUE
+                    THEN (
+                        CAST(nihRate1 AS DOUBLE)
+                        *
+                        (
+                            1 +
+                            CASE
+                                WHEN faRate IS NOT NULL
+                                     AND indirectRate IS NOT NULL
+                                     AND faRate < CAST(indirectRate AS DOUBLE)
+                                    THEN ((CAST(indirectRate AS DOUBLE) - faRate) / (1 + faRate))
+                                ELSE 0
+                            END
+                        )
+                    )
+
+                ELSE nihRate1
+            END
+        AS DOUBLE) AS unitCost
+    FROM rate_context rc
+),
+
+-- =====================================================================================
+-- 5) Final Lease Charges (Billing Review)
+-- =====================================================================================
+lease_final AS (
+    SELECT
+        id,
+        date                      AS assignmentStartDate,
+        enddate                   AS assignmentEndDate,
+        datefinalized             AS dateFinalized,
+        project,
+        account,
+        projectedReleaseCondition,
+        releaseCondition,
+        assignCondition,
+        releaseType,
+        ageAtTime,
+        category,
+        chargeId,
+        chargeId.departmentCode   AS serviceCenter,
+        chargeId.name             AS item,
+        leaseCharge1.name as InitialLEaseType,
+        leaseCharge2.name as FinalLEaseType,
+        sourceRecord,
+        chargeCategory,
+
+        unitCost,
+        quantity,
+
+        CASE
+            WHEN isAdjustment IS NOT NULL
+                THEN ROUND(CAST(unitCost AS DOUBLE), 2)
+            ELSE
+                ROUND(
+                    CAST(unitCost AS DOUBLE)
+                    * COALESCE(CAST(quantity AS DOUBLE), 1),
+                    2
+                )
+        END AS totalCost,
+
+        investigatorId,
+
+        NULL AS isMiscCharge,
+        isAdjustment
+    FROM calculated_costs
+    WHERE id.demographics.species NOT IN ('Rabbit','Guinea Pig')
+),
+
+-- =====================================================================================
+-- 6) Misc Charges (Billing Review)
+-- =====================================================================================
+misc_charges AS (
+    SELECT
+        mc.id,
+        mc.billingDate            AS assignmentStartDate,
+        NULL                      AS assignmentEndDate,
+        NULL                      AS dateFinalized,
+        mc.project,
+        mc.account,
+        NULL AS projectedReleaseCondition,
+        NULL AS releaseCondition,
+        NULL AS assignCondition,
+        NULL AS releaseType,
+        NULL AS ageAtTime,
+        mc.category,
+        mc.chargeId,
+        mc.serviceCenter,
+        mc.item,
+        NULL AS leaseCharge1,
+        NULL AS leaseCharge2,
+        mc.sourceRecord,
+        mc.chargeCategory,
+
+        mc.unitCost,
+        mc.quantity,
+
+        ROUND(
+            CAST(mc.unitCost AS DOUBLE)
+            * COALESCE(CAST(mc.quantity AS DOUBLE), 1),
+            2
+        ) AS totalCost,
+
+        mc.investigatorId,
+
+        'Y' AS isMiscCharge,
+        mc.isAdjustment
+    FROM onprc_billing.miscChargesFeeRateData mc
+    WHERE CAST(mc.billingDate AS DATE)
+          BETWEEN CAST(StartDate AS DATE) AND CAST(EndDate AS DATE)
+      AND mc.category = 'Lease Fees'
+)
+
+-- =====================================================================================
+-- FINAL BILLING-REVIEW OUTPUT
+-- =====================================================================================
+SELECT * FROM lease_final
+UNION ALL
+SELECT * FROM misc_charges;
