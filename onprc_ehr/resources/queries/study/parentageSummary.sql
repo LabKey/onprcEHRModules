2a6bce12f32fcdeb07b8c148b9fa3d6394023959
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2025-12-23 08:42:36 -0800

    Modified Parentage report on both the Clinical Snapshot, and Parentage tab

--- onprc_ehr/resources/queries/study/parentageSummary.sql
+++ onprc_ehr/resources/queries/study/parentageSummary.sql
@@ -34,7 +34,7 @@ SELECT
 
 FROM study.birth b
 WHERE b.dam is not null and b.qcstate.publicdata = true
-And 'dam' not in (select k.relationship from study.parentage k where k.Id = b.Id and k.enddate is null)
+And b.dam not in (select k.parent from study.parentage k where k.Id = b.Id and k.enddate is null and k.relationship = 'Dam' and k.method = 'Observed')
 UNION
 
 SELECT
@@ -46,4 +46,4 @@ SELECT
 
 FROM study.birth a
 WHERE a.sire is not null and a.qcstate.publicdata = true
- And 'sire' not in (select k.relationship from study.parentage k where k.Id = a.Id and k.enddate is null)
+ And a.sire not in (select k.parent from study.parentage k where k.Id = a.Id and k.enddate is null and k.relationship = 'Sire' and k.method = 'Observed')
