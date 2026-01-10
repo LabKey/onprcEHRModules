6620784c8f4eabdc6e3e0b9554b374fc66bff4cd
ohsudev <blasar@ohsu.edu>
2026-01-09 21:58:24 -0800

    Modified Compliance Training/Summary report to use the same report as Overdue/Soon report version.

--- ONPRC_EHR_ComplianceDB/src/org/labkey/ONPRCEHR_ComplianceDB/ONPRC_EHR_ComplianceDBModule.java
+++ ONPRC_EHR_ComplianceDB/src/org/labkey/ONPRCEHR_ComplianceDB/ONPRC_EHR_ComplianceDBModule.java
@@ -110,7 +110,8 @@ public class ONPRC_EHR_ComplianceDBModule extends ExtendedSimpleModule
 //               Added: 10-24-2022  R.Blasa
         EHRService.get().registerMoreActionsButton(new ShowEditUIButton(this, "ehr_compliancedb", "completiondates", ONPRC_ComplianceDBEntryPermission.class), "ehr_compliancedb", "completiondates");
 
-
+      // Added 1-8-2026
+        EHRService.get().registerActionOverride("employeeDetails", this, "views/empoyeeDetails.html");
 
 
     }
