be2ac8c6e794461561b58f67e1973b66606f612a
loganb <loganb@ohsu.edu>
2026-01-23 12:15:34 -0800

    Delete excluded projects table in favor of a list

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-25.003-25.003.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ehr_lookups.roomUtilizationHistoricalExcludedProjects (
-    rowid [int] IDENTITY(100,1) NOT NULL,
-    name nvarchar(200),
-
-    CONSTRAINT PK_EHR_LOOKUPS_ROOMTUILIZATIONHISTORYICALEXCLUDEDPROJECTS PRIMARY KEY (rowid),
-
-);
-GO
