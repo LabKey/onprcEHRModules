c1f72796b92fc389733a8a7645c58866fcd73b15
kollil <kollil@ohsu.edu>
2026-02-24 15:00:13 -0800

    Added a new report in the Colony alert: Parentage Dam mismatch records

---
+++ onprc_ehr/resources/queries/study/ParentageDamMismatch.sql
@@ -0,0 +1,53 @@
+/*
+ Added by Kolli, Feb 2026
+ Refer tkt# 14114
+ Get data where genetic and observed dam do not match, AND foster dam ISBLANK AND Observed Dam IS NOT BLANK
+*/
+
+SELECT
+    d.Id,
+    d.Id.curLocation.area AS Area,
+    coalesce(p2.parent, '') as geneticDam,
+    coalesce(b.dam, '') as observedDam
+
+FROM study.demographics d
+
+         LEFT JOIN (
+    SELECT
+        p2.Id,
+        MAX(p2.parent) AS parent
+    FROM study.parentage p2
+    WHERE (p2.method = 'Genetic' OR p2.method = 'Provisional Genetic')
+      AND p2.relationship = 'Dam'
+      AND p2.enddate IS NULL
+    GROUP BY p2.Id
+) p2 ON d.Id = p2.Id
+
+         LEFT JOIN (
+    SELECT
+        p3.Id,
+        MAX(p3.parent) AS parent
+    FROM study.parentage p3
+    WHERE p3.relationship = 'Foster Dam'
+      AND p3.enddate IS NULL
+    GROUP BY p3.Id
+) p3 ON d.Id = p3.Id
+
+         LEFT JOIN study.birth b
+                   ON b.Id = d.Id
+
+WHERE d.calculated_status.code IN ('Alive', 'Dead') AND d.qcstate = 18
+    /* exclude foster-dam cases (NULL or blank only) */
+  AND COALESCE(RTRIM(LTRIM(CAST(p3.parent AS VARCHAR(50)))), '') = ''
+
+    /* exclude blank observed dam */
+  AND COALESCE(RTRIM(LTRIM(CAST(b.dam AS VARCHAR(50)))), '') <> ''
+
+    /* exclude blank genetic dam */
+  AND COALESCE(RTRIM(LTRIM(CAST(p2.parent AS VARCHAR(50)))), '') <> ''
+
+    /* mismatch observed vs genetic */
+  AND COALESCE(RTRIM(LTRIM(CAST(b.dam AS VARCHAR(50)))), '') <>
+      COALESCE(RTRIM(LTRIM(CAST(p2.parent AS VARCHAR(50)))), '')
+
+
