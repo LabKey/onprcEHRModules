914507bc153471f974f6a77f875ba44d3d6e7ed7
blasar <blasar@ohsu.eddu>
2026-04-23 08:09:57 -0700

    Modified Room Layout web page to accommodate "no cage" entries..

--- onprc_ehr/resources/web/onprc_ehr/panel/RoomLayoutPanel.js
+++ onprc_ehr/resources/web/onprc_ehr/panel/RoomLayoutPanel.js
@@ -185,7 +185,7 @@ Ext4.define('ONPRC.panel.RoomLayoutPanel', {
                 var rooms = Ext4.Object.getKeys(roomMap).sort();
                 var dividerWidth = 3;
                 var height = 75;
-                var cageWidth = 90;
+                var cageWidth = 60;
                 var hasCages = false;
 
                 Ext4.each(rooms, function(room, roomIdx){
