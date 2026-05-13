2625f08bd68be0b7569c99fbc3622f4c92db674c
blasar <blasar@ohsu.eddu>
2026-05-13 09:51:58 -0700

    Modified Compliance Public reports

--- ONPRC_EHR_ComplianceDB/resources/web/EHR_ComplianceDB/panel/EmployeeDetailsPanel.js
+++ ONPRC_EHR_ComplianceDB/resources/web/EHR_ComplianceDB/panel/EmployeeDetailsPanel.js
@@ -32,7 +32,7 @@ Ext4.define('EHR_ComplianceDB.panel.EmployeeDetailsPanel', {
             }, {
                 xtype: 'ldk-querypanel',
                 queryConfig: {
-                    title: 'Training / Requirement Summary',
+                    title: 'Training / Requirement SummaryX',
                     schemaName: this.schemaName,
                     queryName: 'ComplianceProcedureRecentTests',  //exclude employee training exemptions
                     filterArray: filterArray,
