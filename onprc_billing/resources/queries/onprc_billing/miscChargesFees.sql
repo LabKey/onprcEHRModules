48f30a9e45ac1e91022c38b8ee30cc7bfc466281
Adam Rauch <adam@labkey.com>
2026-05-28 15:57:46 -0700

    Fix double license

--- onprc_billing/resources/queries/onprc_billing/miscChargesFees.sql
+++ onprc_billing/resources/queries/onprc_billing/miscChargesFees.sql
@@ -4,12 +4,6 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 
-/*
- * Copyright (c) 2014-2026 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
-
 SELECT
   mc.Id,
   mc.date,
