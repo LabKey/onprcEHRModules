3b5c44fab1b7ab38bc65d4ab974c5b943a025aba
kollil <kollil@ohsu.edu>
2026-07-02 16:06:04 -0700

    New ETL process created to update investigatorId in sla census table.

---
+++ sla/resources/schemas/dbscripts/sqlserver/sla-26.001-26.002.sql
@@ -0,0 +1,31 @@
+
+/****** Object:  StoredProcedure  sla.SLACensusInvestigatorUpdate   Script Date: June, 2026 *****/
+-- ==========================================================================================
+-- Author: Lakshmi Kolli
+-- Create date: June 2026
+-- A new stored procedure is created to update sla.census.investigatorId directly from ehr.project
+-- and onprc_ehr.investigators
+-- ==========================================================================================
+
+--Drop Stored proc if exists
+EXEC core.fn_dropifexists 'SLACensusInvestigatorUpdate', 'onprc_ehr', 'PROCEDURE';
+GO
+
+CREATE PROCEDURE [onprc_ehr].[SLACensusInvestigatorUpdate]
+AS
+
+BEGIN
+
+    UPDATE c
+    SET c.investigatorId = i.objectid
+        FROM sla.census c
+        INNER JOIN ehr.project p
+    ON p.project = c.project
+        INNER JOIN onprc_ehr.investigators i
+        ON i.rowId = p.investigatorId
+    WHERE c.date >= '2016/09/01'
+      AND c.investigatorId IS NULL
+      AND i.objectid IS NOT NULL
+
+END
+Go
