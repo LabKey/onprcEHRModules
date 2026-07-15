4bbe4381b20712954941d7e95d689a274ac5f6bb
blasar <blasar@ohsu.edu>
2026-07-15 16:58:49 -0700

    Removed stored procedures that are no longer needed.

--- onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-26.001-26.002.sql
+++ onprc_ehr/resources/schemas/dbscripts/sqlserver/onprc_ehr-26.001-26.002.sql
@@ -10,54 +10,48 @@
 ---- ComplianceRecentTestsorg  --> directory and xml files
 
 
-
 ----Drop corresponding temp tables
-
-
-
-----Drop corresponding temp tables
-
+  ---- No temp tables to include here
 --Drop stored procedures
 EXEC core.fn_dropifexists 'sp_Compliance_requirementname_Update_Process','onprc_ehr','PROCEDURE';
-
+GO
 ----Drop corresponding temp tables
-
+EXEC core.fn_dropifexists 'Rpt_TempJmacDate','onprc_ehr','TABLE';
+GO
 --Drop stored procedures
 EXEC core.fn_dropifexists 's_JmacRemovalDateProcess','onprc_ehr','PROCEDURE';
-
+GO
 ----Drop corresponding temp tables
-
+  ---- No temp tables to include here
 --Drop stored procedures
 EXEC core.fn_dropifexists 'p_EnvironmentalHistoricalUpdates','onprc_ehr','PROCEDURE';
-
+GO
 ----Drop corresponding temp tables
-
+  ---- No temp tables to include here
 --Drop stored procedures
 EXEC core.fn_dropifexists 'p_Environmental_Update_Process','onprc_ehr','PROCEDURE';
-
+GO
 ----Drop corresponding temp tables
-
+EXEC core.fn_dropifexists 'Rpt_SLaCensus','dbo','TABLE';
+GO
 --Drop stored procedures
 EXEC core.fn_dropifexists 'sp_RpSLASummaryCensus','dbo','PROCEDURE';
+GO
 
 ----Drop corresponding temp tables
-
+EXEC core.fn_dropifexists 'Rpt_Labwork_MergeUpdate','dbo','TABLE';
+GO
+EXEC core.fn_dropifexists 'Rpt_Labwork_MergeUpdatelog','dbo','TABLE';
+GO
 --Drop stored procedures
 EXEC core.fn_dropifexists 'RptMergeChargetypeUpdateSP','dbo','PROCEDURE';
+GO
+
 
 ----Drop corresponding temp tables
-
---Drop stored procedures
-EXEC core.fn_dropifexists 'RptMergeChargetypeUpdateSP','dbo','PROCEDURE';
-
-----Drop corresponding temp tables
-
+---- No temp tables to include here
 --Drop stored procedures
 EXEC core.fn_dropifexists 'p_ComplianceTranslatestringUpdate','onprc_ehr_compliancedb','PROCEDURE';
-
-
-
-
 GO
 
 
