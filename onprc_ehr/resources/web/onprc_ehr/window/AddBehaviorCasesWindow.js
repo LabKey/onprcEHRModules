152fe10b0f977164f9a4d62c25b4ed38c37befaa
blasar <blasar@ohsu.edu>
2026-07-17 17:25:37 -0700

    Modified BSU Rounds process.

--- onprc_ehr/resources/web/onprc_ehr/window/AddBehaviorCasesWindow.js
+++ onprc_ehr/resources/web/onprc_ehr/window/AddBehaviorCasesWindow.js
@@ -98,7 +98,7 @@ Ext4.define('ONPRC_EHR.window.AddBehaviorCasesWindow', {
 
                 //note: this has been changed to ensure 1 row per case
                 var key = row.getValue('caseid');
-                if ( row.getValue('category') == 'Alopecia Regrowth' && (tempcaseid != key || key == null) ) {
+                if ( row.getValue('category') == 'Alopecia Regrowth' && (tempcaseid != key || tempcaseid == '') ) {
                     newobservation = row.getValue('category');  //load Alopecia Regrowth
                     tempcaseid = row.getValue('caseid');
                 }
