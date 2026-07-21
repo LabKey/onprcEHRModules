fa0ce8fb5e0d1f908c71e885ef9d1d9fbc9a50dd
blasar <blasar@ohsu.edu>
2026-07-20 21:56:24 -0700

    Modified Radiology scheduler program

--- extscheduler/resources/web/extscheduler/App/view/Scheduler_Radiology.js
+++ extscheduler/resources/web/extscheduler/App/view/Scheduler_Radiology.js
@@ -1,7 +1,7 @@
 Ext.define('App.view.Scheduler', {
     extend        : 'Sch.panel.SchedulerGrid',
-    alias         : 'widget.scheduler',
-    reference     : 'scheduler',
+    alias         : 'widget.scheduler_Radiology',
+    reference     : 'scheduler_Radiology',
     startDate     : new Date(),
     //endDate       : new Date(),
     startTime     : 6,
@@ -28,14 +28,14 @@ Ext.define('App.view.Scheduler', {
             text : 'Previous',
             iconCls: 'x-fa fa-arrow-circle-left',
             handler: function (btn) {
-                var scheduler = btn.up('scheduler');
+                var scheduler = btn.up('scheduler_Radiology');
                 scheduler.timeAxis.shift(-7, Sch.util.Date.DAY);
             }
         },
         {
             text : 'Today',
             handler: function (btn) {
-                var scheduler = btn.up('scheduler');
+                var scheduler = btn.up('scheduler_Radiology');
                 // Clear time here so date adjustment wouldn't result in 2 days span
                 scheduler.setStart(Sch.util.Date.clearTime(new Date()));
             }
@@ -45,7 +45,7 @@ Ext.define('App.view.Scheduler', {
             iconCls: 'x-fa fa-arrow-circle-right',
             iconAlign: 'right',
             handler: function (btn) {
-                var scheduler = btn.up('scheduler');
+                var scheduler = btn.up('scheduler_Radiology');
                 scheduler.timeAxis.shift(7, Sch.util.Date.DAY);
             }
         },
@@ -55,7 +55,7 @@ Ext.define('App.view.Scheduler', {
             scope        : this,
             menu         : Ext.create('Ext.menu.DatePicker', {
                 handler : function (dp, date) {
-                    var scheduler = dp.up('scheduler');
+                    var scheduler = dp.up('scheduler_Radiology');
                     scheduler.setStart(Sch.util.Date.clearTime(date));
                 }
             })
@@ -66,7 +66,7 @@ Ext.define('App.view.Scheduler', {
             hidden: !LABKEY.user.canInsert,
             scope: this,
             handler: function (btn) {
-                var scheduler = btn.up('scheduler');
+                var scheduler = btn.up('scheduler_Radiology');
 
                 Ext.create('Ext.window.Window', {
                     title : 'Create New Event',
