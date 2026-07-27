357a7e46af5038324d7e7db32a51dc3fede5f9b6
blasar <blasar@ohsu.edu>
2026-07-27 11:04:01 -0700

    Modified scripts so that script numbering in ascending order.

--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -124,7 +124,7 @@ public class ONPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 26.005;
+        return 26.002;
     }
 
     @Override
