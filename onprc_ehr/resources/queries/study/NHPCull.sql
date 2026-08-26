0d73fffa5dffb8715345a17a7df7fbfc31c33c01
ohsudev-jonesga <jonesga@ohsu.edu>
2026-08-26 07:51:40 -0700

    Insertt of UserDefined QUery into CodeBase

--- onprc_ehr/resources/queries/study/NHPCull.sql
+++ onprc_ehr/resources/queries/study/NHPCull.sql
@@ -1,13 +1,13 @@
-SELECT flags.Id,
-flags.date,
-flags.flag,
-flags.performedby,
-flags.remark,
-flags.taskid,
-flags.description,
-flags.RequestedBY,
-flags.TargetEndDate,
-flags.ScheduleNecropsy,
-flags.history
-FROM flags join demographics d on flags.id = d.id
-where flags.flag.value like '%Cull%' and d.calculated_Status = 'alive' and flags.enddate is null
+SELECT f.Id,
+f.date,
+f.flag,
+f.performedby,
+f.remark,
+f.taskid,
+f.description,
+f.RequestedBY,
+f.TargetEndDate,
+f.ScheduleNecropsy,
+f.history
+FROM flags f join demographics d on flags.id = d.id
+where f.flag.value like '%Cull%' and d.calculated_Status = 'alive' and f.enddate is null
--- onprc_ehr/resources/queries/study/NHPUUL.sql
+++ onprc_ehr/resources/queries/study/NHPUUL.sql
@@ -1,14 +1,13 @@
-SELECT flags.Id,
-flags.date,
-flags.enddate,
-flags.flag,
-flags.performedby,
-flags.remark,
-flags.taskid,
-flags.description,
-flags.RequestedBY,
-flags.ScheduleNecropsy,
-flags.TargetEndDate,
-flags.history
-FROM flags join demographics d on flags.id = d.id
-where flags.flag.value like '%Underutilized%' and d.calculated_status = 'alive' and flags.enddate is null
+SELECT f.Id,
+f.date,
+f.enddate,
+f.flag,
+f.performedby,
+f.remark,
+f.taskid,
+f.description,
+f.RequestedBY,
+f.ScheduleNecropsy,
+f.history
+FROM flags f join demographics d on f.id = d.id
+where f.flag.value like '%Underutilized%' and d.calculated_status = 'alive' and f.enddate is null
