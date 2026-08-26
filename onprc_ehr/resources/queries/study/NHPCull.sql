cdcb2a4b7fb6e303c7a70bdfde280a67a8591ca5
ohsudev-jonesga <jonesga@ohsu.edu>
2026-08-26 13:27:35 -0700

    Repir of query error and resubmit

--- onprc_ehr/resources/queries/study/NHPCull.sql
+++ onprc_ehr/resources/queries/study/NHPCull.sql
@@ -9,5 +9,5 @@ f.RequestedBY,
 f.TargetEndDate,
 f.ScheduleNecropsy,
 f.history
-FROM flags f join demographics d on flags.id = d.id
+FROM flags f join demographics d on f.id = d.id
 where f.flag.value like '%Cull%' and d.calculated_Status = 'alive' and f.enddate is null
