1c4fc58c98ed9a0bc30fb7ee4efa4598d7bdbef5
loganb <loganb@ohsu.edu>
2026-08-31 09:01:05 -0700

    Initial commit

---
+++ onprc_ehr/resources/queries/study/vetAssignment_multipleVets.sql
@@ -0,0 +1,19 @@
+WITH assignmentData AS (
+    SELECT
+        Id,
+        Area,
+        Room,
+        CASE
+            WHEN AssignmentType = 'Open Case' THEN (AssignedVet || ', Open Case: ' || COALESCE(MasterProblems, ''))
+            ELSE (AssignedVet || ', ' || AssignmentType)
+            END AS assignment
+    FROM demographicsAssignedVet
+)
+SELECT
+    Id,
+    Area,
+    Room,
+    GROUP_CONCAT(assignment, chr(10)) AS Vets
+FROM assignmentData
+GROUP BY Id, Area, Room
+HAVING COUNT(*) > 1
