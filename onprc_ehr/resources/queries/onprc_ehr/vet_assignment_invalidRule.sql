5ce30b4e90e5d1853dcf730268cfc0cef9baba16
loganb <loganb@ohsu.edu>
2026-09-10 09:16:32 -0700

    Add copyright notices

--- onprc_ehr/resources/queries/onprc_ehr/vet_assignment_invalidRule.sql
+++ onprc_ehr/resources/queries/onprc_ehr/vet_assignment_invalidRule.sql
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2014-2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 WITH ruleData AS (
     SELECT
         rowid,
