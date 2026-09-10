b3fc3cd1fee3d52b748486cc4977ec2136b060c5
blasar <blasar@ohsu.edu>
2026-09-10 13:37:41 -0700

    Modified drop downlistings to remove the option that allows Prime users an option to enter their own preferred values.

--- onprc_ehr/resources/web/onprc_ehr/data/sources/ONPRCDefaults.js
+++ onprc_ehr/resources/web/onprc_ehr/data/sources/ONPRCDefaults.js
@@ -265,6 +265,16 @@ EHR.model.DataModelManager.registerMetadata('Default', {
             }
          }
       },
+        //Modified: 9-10-2026 R. Blasa
+        'study.housing': {
+            room: {
+                editorConfig: {
+                    plugins: [Ext4.create('LDK.plugin.UserEditableCombo', {
+                        allowChooseOther: false
+                    })]
+                }
+            }
+        },
         //Added: 12-27-2017   R.blasa
 
         'study.flags': {
