4ca894132bc8d90c9c0255897ec19b3efc456ee6
loganb <loganb@ohsu.edu>
2026-09-10 08:57:43 -0700

    Remove demographicsAssignedVet.sql from PR

--- onprc_ehr/resources/queries/study/demographicsAssignedVet.sql
+++ onprc_ehr/resources/queries/study/demographicsAssignedVet.sql
@@ -1,28 +1,30 @@
-WITH MinRule AS (
-    SELECT Id, MIN(matchedRule) AS MinMatchedRule
-    FROM vetAssignment_filter
-    GROUP BY Id
-),
-    VetCaseData AS (
-        SELECT
-            Id,
-            AssignedVet,
-            GROUP_CONCAT(DISTINCT ActiveMasterProblems, ', ') AS GroupedMasterProblems -- DISTINCT prevents duplicate master problems if dual-assigned
-        FROM vetAssignment_filter
-        WHERE matchedRule = 0
-        GROUP BY Id, AssignedVet
-    )
-SELECT DISTINCT -- DISTINCT prevents duplicate rows
+/*
+ * Copyright (c) 2014-2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+/*
+study.demographicsAssignedVet
+
+* Returns one or more assigned vets per animal ID
+* Note to future self: be very careful when adding more fields to this view. It can easily
+  result in additional rows per animal if they're distinct.
+ */
+
+SELECT
     f.Id,
     f.AssignedVet,
     f.AssignmentType,
-    v.GroupedMasterProblems AS MasterProblems,
+    GROUP_CONCAT(
+            CASE WHEN f.matchedRule = 0 THEN f.ActiveMasterProblems ELSE NULL END,
+            ', '
+    ) AS MasterProblems,
     f.Area,
     f.Room
 FROM vetAssignment_filter f
-     JOIN MinRule m
-          ON m.Id = f.Id
-              AND m.MinMatchedRule = f.matchedRule
-     LEFT JOIN VetCaseData v
-               ON v.Id = f.Id
-                   AND v.AssignedVet = f.AssignedVet
+WHERE f.matchedRule = (
+    SELECT min(matchedRule)
+    FROM vetAssignment_filter sub
+    WHERE sub.Id = f.Id
+)
+GROUP BY f.Id, f.AssignedVet, f.AssignmentType, f.Area, f.Room
