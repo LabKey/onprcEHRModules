4f147ba4e976b0341c9b9af0ff1b2d905f09efd9
blasar <blasar@ohsu.edu>
2026-09-14 23:56:28 -0700

    Modified Room's Drop Downlistings to prevent users from entering their own preferred values.

---
+++ onprc_ehr/resources/web/onprc_ehr/form/field/RoomFieldSingle.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2013-2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+Ext4.define('EHR.form.field.RoomFieldSingle', {
+    extend: 'Ext.form.field.ComboBox',
+    alias: 'widget.onprc_ehr-roomfieldsingle',
+
+    caseSensitive: false,
+    anyMatch: true,
+    displayField: 'room',
+    forceSelection: true,
+    columns: 'room,area',
+
+    initComponent: function(){
+        var ctx = EHR.Utils.getEHRContext();
+
+        Ext4.apply(this, {
+            valueField: 'room',
+            queryMode: 'local',
+            store: {
+                type: 'labkey-store',
+                containerPath: ctx ? ctx['EHRStudyContainer'] : null,
+                schemaName: 'ehr_lookups',
+                queryName: 'rooms',
+                columns: this.columns,
+                sort: 'room_sortValue',
+                filterArray: [LABKEY.Filter.create('datedisabled', null, LABKEY.Filter.Types.ISBLANK)],
+                autoLoad: true
+            },
+            // plugins: ['ldk-usereditablecombo']
+        });
+
+        this.callParent(arguments);
+    }
+});
--- onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
+++ onprc_ehr/src/org/labkey/onprc_ehr/ONPRC_EHRModule.java
@@ -333,6 +333,8 @@ public class ONPRC_EHRModule extends ExtendedSimpleModule
         //Added: 10-12-2023  R.Blasa
         EHRService.get().registerClientDependency(ClientDependency.supplierFromPath("onprc_ehr/form/field/EnvironmentalField.js"), this);
 
+        //Added: 9-14-2026  R.Blasa
+        EHRService.get().registerClientDependency(ClientDependency.supplierFromPath("onprc_ehr/form/field/RoomFieldSingle.js"), this);
 
         EHRService.get().registerReportLink(EHRService.REPORT_LINK_TYPE.housing, "List Single Housed Animals", this, DetailsURL.fromString("/query/executeQuery.view?schemaName=study&query.queryName=demographicsPaired&query.viewName=Single Housed"), "Commonly Used Queries");
         EHRService.get().registerReportLink(EHRService.REPORT_LINK_TYPE.housing, "Find Animals Housed In A Given Room/Cage At A Specific Time", this, DetailsURL.fromString("/ehr/housingOverlaps.view?groupById=1"), "Commonly Used Queries");
