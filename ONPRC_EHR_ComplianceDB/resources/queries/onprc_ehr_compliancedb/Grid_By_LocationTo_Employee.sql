e0c8b260e0e16a41ec3655952f3e5c44675071ff
blasar <blasar@ohsu.edu>
2026-09-21 14:39:01 -0700

    Modified Compliance Grid queries.

--- ONPRC_EHR_ComplianceDB/resources/queries/onprc_ehr_compliancedb/Grid_By_LocationTo_Employee.sql
+++ ONPRC_EHR_ComplianceDB/resources/queries/onprc_ehr_compliancedb/Grid_By_LocationTo_Employee.sql
@@ -1,7 +1,8 @@
 
 select
     coalesce(jj.requirementname, '') as requirementname,
-    jj.employeeid as employeeid
+    jj.employeeid as employeeid,
+    jj.grid_type as grid_type
 
 from onprc_ehr_compliancedb.employeeTraining_details  jj where
     jj.requirementname in (select distinct ss.requirementname from ehr_compliancedb.requirements ss where ss.requirementname like 'Area Training%' and ss.requirementname <> '' )
