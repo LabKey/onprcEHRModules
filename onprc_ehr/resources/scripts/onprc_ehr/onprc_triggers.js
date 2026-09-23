559eb15ba437f2fe8ae375d908eecdda6f92cff8
Lakshmi Kolli <69940873+kollil@users.noreply.github.com>
2026-09-23 13:50:36 -0700

    comment updated

--- onprc_ehr/resources/scripts/onprc_ehr/onprc_triggers.js
+++ onprc_ehr/resources/scripts/onprc_ehr/onprc_triggers.js
@@ -840,7 +840,7 @@ exports.init = function(EHR){
         }
 
         // Added by Kollil - Aug 2026
-        // Refer to tkt # 13777
+        // Refer to old tkt # 13777, new ticket #
         // Check whether the animal's current/latest housing record already exists, and if that record has an enddate, throw an error
         // Only check when creating a new active housing record:
         // IF this is a NEW housing record, AND, the new record has no end date, AND, the animal already has a housing record with no end date, THEN WARN the user
